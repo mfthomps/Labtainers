8da82bfa0eb690114eed87b273bfe969a6238433
jkhosali <jkhosali@ed162e55-3153-4343-a18d-e086fa355ed4>
2016-10-13 20:20:02 +0000

    Modified build script - DO NOT USE PWD - change to ORIG_PWD as variable.
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@6 ed162e55-3153-4343-a18d-e086fa355ed4

--- scripts/MyInstructorDocker/instructor.build.sh
+++ scripts/MyInstructorDocker/instructor.build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-PWD=`pwd`
-echo $PWD
-PARAMLABS_DIR=${PWD}/paramlabs/
-PARAMLABS_TAR=${PWD}/paramlabs.instructor.tar.gz
+ORIG_PWD=`pwd`
+echo $ORIG_PWD
+PARAMLABS_DIR=${ORIG_PWD}/paramlabs/
+PARAMLABS_TAR=${ORIG_PWD}/paramlabs.instructor.tar.gz
 
 cd $PARAMLABS_DIR
 tar -zcvf $PARAMLABS_TAR .local *
 
-cd $PWD
+cd $ORIG_PWD
 
-docker build -f ./Dockerfile.paramlabs.instructor -t paramlabs:instructor .
-#docker build -f ./Dockerfile.bufoverflow.instructor -t bufoverflow:instructor .
+docker build -f ${ORIG_PWD}/Dockerfile.paramlabs.instructor -t paramlabs:instructor .
+#docker build -f ${ORIG_PWD}/Dockerfile.bufoverflow.instructor -t bufoverflow:instructor .
--- scripts/MyStudentDocker/student.build.sh
+++ scripts/MyStudentDocker/student.build.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-PWD=`pwd`
-echo $PWD
-PARAMLABS_DIR=${PWD}/paramlabs/
-PARAMLABS_TAR=${PWD}/paramlabs.student.tar.gz
-BUFOVERFLOW_DIR=${PWD}/bufoverflow/
-BUFOVERFLOW_TAR=${PWD}/bufoverflow.student.tar.gz
+ORIG_PWD=`pwd`
+echo $ORIG_PWD
+PARAMLABS_DIR=${ORIG_PWD}/paramlabs/
+PARAMLABS_TAR=${ORIG_PWD}/paramlabs.student.tar.gz
+BUFOVERFLOW_DIR=${ORIG_PWD}/bufoverflow/
+BUFOVERFLOW_TAR=${ORIG_PWD}/bufoverflow.student.tar.gz
 
 cd $PARAMLABS_DIR
 tar -zcvf $PARAMLABS_TAR .local *
 cd $BUFOVERFLOW_DIR
 tar -zcvf $BUFOVERFLOW_TAR .local *
 
-cd $PWD
+cd $ORIG_PWD
 
-docker build -f ./Dockerfile.paramlabs.student -t paramlabs:student .
-#docker build -f ./Dockerfile.bufoverflow.student -t bufoverflow:student .
+docker build -f ${ORIG_PWD}/Dockerfile.paramlabs.student -t paramlabs:student .
+#docker build -f ${ORIG_PWD}/Dockerfile.bufoverflow.student -t bufoverflow:student .
