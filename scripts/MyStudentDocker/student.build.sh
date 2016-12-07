595dcfc504f4304c6a0a004e041484d23462628b
jkhosali <jkhosali@ed162e55-3153-4343-a18d-e086fa355ed4>
2016-12-07 00:29:14 +0000

    Add One-Way Hash Function Lab container for student
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@72 ed162e55-3153-4343-a18d-e086fa355ed4

--- scripts/MyStudentDocker/student.build.sh
+++ scripts/MyStudentDocker/student.build.sh
@@ -6,15 +6,21 @@ PARAMLABS_DIR=${ORIG_PWD}/paramlabs/
 PARAMLABS_TAR=${ORIG_PWD}/paramlabs.student.tar.gz
 BUFOVERFLOW_DIR=${ORIG_PWD}/bufoverflow/
 BUFOVERFLOW_TAR=${ORIG_PWD}/bufoverflow.student.tar.gz
+ONEWAYHASH_DIR=${ORIG_PWD}/onewayhash/
+ONEWAYHASH_TAR=${ORIG_PWD}/onewayhash.student.tar.gz
 
 cp -r bin $PARAMLABS_DIR/.local/
 cp -r bin $BUFOVERFLOW_DIR/.local/
+cp -r bin $ONEWAYHASH_DIR/.local/
 cd $PARAMLABS_DIR
 tar -zcvf $PARAMLABS_TAR .local *
 cd $BUFOVERFLOW_DIR
 tar -zcvf $BUFOVERFLOW_TAR .local *
+cd $ONEWAYHASH_DIR
+tar -zcvf $ONEWAYHASH_TAR .local *
 
 cd $ORIG_PWD
 
 docker build -f ${ORIG_PWD}/Dockerfile.paramlabs.student -t paramlabs:student .
 docker build -f ${ORIG_PWD}/Dockerfile.bufoverflow.student -t bufoverflow:student .
+docker build -f ${ORIG_PWD}/Dockerfile.onewayhash.student -t onewayhash:student .
