3dc440316e01fd20ac505e5e627cceb315a59003
jkhosali <jkhosali@ed162e55-3153-4343-a18d-e086fa355ed4>
2017-02-02 18:18:25 +0000

    Added trunk/labs/telnetlab (the new Telnet Lab). Modified buildImage.sh to take optional image name (i.e., can differentiate telnet client image vs telnet server image). Modified student.build.sh to have the new telnetlab and add the corresponding telnetlab.network
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@146 ed162e55-3153-4343-a18d-e086fa355ed4

--- scripts/MyStudentDocker/buildImage.sh
+++ scripts/MyStudentDocker/buildImage.sh
@@ -1,19 +1,47 @@
 #!/bin/bash
+
+# Usage: buildImage.sh <labname> [<imagename>]
+#        <imagename> is optional for lab that only has one image
 lab=$1
+has_image=0
+if [ "$#" -eq 2 ]; then
+    imagename=$2
+    labimage=$lab.$imagename
+    has_image=1
+else
+    labimage=$lab
+    has_image=0
+fi
+
+if [ $has_image == 1 ]; then
+    echo "Labname is $lab with image name $imagename"
+else
+    echo "Labname is $lab using default image"
+fi
+
 LAB_DIR=`realpath ../../labs/$lab/`
 if [ ! -d $LAB_DIR ]; then
     echo "$LAB_DIR not found as a lab directory"
     exit
 fi
+if [ $has_image == 1 ]; then
+    LABIMAGE_DIR=`realpath ../../labs/$lab/$imagename/`
+    if [ ! -d $LABIMAGE_DIR ]; then
+        echo "$LABIMAGE_DIR not found"
+        exit
+    fi
+else
+    LABIMAGE_DIR=`realpath ../../labs/$lab/`
+fi
+
 ORIG_PWD=`pwd`
 echo $ORIG_PWD
-#LAB_DIR=${ORIG_PWD}/$lab/
-LAB_TAR=${ORIG_PWD}/$lab.student.tar.gz
-TMP_DIR=/tmp/$lab
+LAB_TAR=${ORIG_PWD}/$labimage.student.tar.gz
+TMP_DIR=/tmp/$labimage
 rm -rf $TMP_DIR
 mkdir $TMP_DIR
 mkdir $TMP_DIR/.local
-cp $LAB_DIR/* $TMP_DIR 2>>/dev/null
+cp $LABIMAGE_DIR/* $TMP_DIR 2>>/dev/null
 cp -r $LAB_DIR/config $TMP_DIR/.local/ 2>>/dev/null
 cp  -r bin/ $TMP_DIR/.local/  2>>/dev/null
 cp  $LAB_DIR/bin/* $TMP_DIR/.local/bin 2>>/dev/null
@@ -21,8 +49,8 @@ mkdir $TMP_DIR/.local/result
 cd $TMP_DIR
 tar --atime-preserve -zcvf $LAB_TAR .local *
 cd $ORIG_PWD
-dfile=Dockerfile.$lab.student
+dfile=Dockerfile.$labimage.student
 cp $LAB_DIR/dockerfiles/$dfile .
-docker build --build-arg lab=$lab -f ./$dfile -t $lab:student .
+docker build --build-arg lab=$labimage -f ./$dfile -t $labimage:student .
 echo "removing temporary $dfile, reference original in $LAB_DIR/dockerfiles/$dfile"
 rm ./$dfile
--- scripts/MyStudentDocker/student.build.sh
+++ scripts/MyStudentDocker/student.build.sh
@@ -3,3 +3,5 @@
 ./buildImage.sh formatstring
 ./buildImage.sh bufoverflow
 ./buildImage.sh onewayhash
+./buildImage.sh telnetlab client
+./buildImage.sh telnetlab server
