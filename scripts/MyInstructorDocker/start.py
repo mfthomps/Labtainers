1b4a840002216ae3bd421443c3051886071f5425
jkhosali <jkhosali@ed162e55-3153-4343-a18d-e086fa355ed4>
2017-02-07 19:16:29 +0000

    Added start.py and stop.py for instructor (new start and stop scripts). Also added telnetlab.network - the networks/containers for telnetlab lab exercise for instructor
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@156 ed162e55-3153-4343-a18d-e086fa355ed4

---
+++ scripts/MyInstructorDocker/start.py
@@ -0,0 +1,321 @@
+#!/usr/bin/env python
+
+# Filename: start.py
+# Description:
+# This is the start script to be run by the instructor.
+# Note:
+# 1. It needs 'start.config' file, where
+#    <labname> is given as a parameter to the script.
+# 2. If the lab has multiple containers and/or multi-home
+#    networking, then <labname>.network file is necessary
+#
+
+import getpass
+import glob
+import json
+import md5
+import os
+import re
+import subprocess
+import sys
+import time
+import zipfile
+from netaddr import *
+import ParseInstMulti
+import ParseInstConfig
+
+# Error code returned by docker inspect
+SUCCESS=0
+FAILURE=1
+
+# get docker0 IP address
+def getDocker0IPAddr():
+    command="ifconfig docker0 | awk '/inet addr:/ {print $2}' | sed 's/addr://'"
+    #print "Command to execute is (%s)" % command
+    child = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+    result = child.stdout.read().strip()
+    #print "Result of subprocess.Popen getDocket0IPAddr is %s" % result
+    return result
+
+# Copy Students' Artifacts from host to instructor's lab container
+def CopyStudentArtifacts(start_config, mycontainer_name, labname):
+    container_user = start_config.container_user
+    host_home_xfer = start_config.host_home_xfer
+    # Set the lab name 
+    command = 'docker exec -it %s script -q -c "echo %s > /home/%s/.local/.labname" /dev/null' % (mycontainer_name, labname, container_user)
+    #print "Command to execute is (%s)" % command
+    result = subprocess.call(command, shell=True)
+    #print "Result of subprocess.call CopyStudentArtifacts set labname is %s" % result
+    if result == FAILURE:
+        sys.stderr.write("ERROR: Failed to set labname in container %s!\n" % mycontainer_name)
+        sys.exit(1)
+
+    username = getpass.getuser()
+    # Copy zip files from 'Shared' folder to 'home/$CONTAINER_USER'
+    zip_filelist = glob.glob('/home/%s/%s/*.zip' % (username, host_home_xfer))
+    #print "filenames is (%s)" % zip_filelist
+    for fname in zip_filelist:
+        #print "name is %s" % fname
+        base_fname = os.path.basename(fname)
+        # Copy zip file and chown it
+        command = 'docker cp %s %s:/home/%s/' % (fname, mycontainer_name, container_user)
+        #print "Command to execute is (%s)" % command
+        result = subprocess.call(command, shell=True)
+        #print "Result of subprocess.call CopyStudentArtifacts copy zipfile (%s) is %s" % (fname, result)
+        if result == FAILURE:
+            sys.stderr.write("ERROR: Failed to set labname in container %s!\n" % mycontainer_name)
+            sys.exit(1)
+        command = 'docker exec -it %s sudo chown %s:%s /home/%s/%s' % (mycontainer_name, container_user, container_user, container_user, base_fname)
+        #print "Command to execute is (%s)" % command
+        result = subprocess.call(command, shell=True)
+        #print "Result of subprocess.call CopyStudentArtifacts copy zipfile (%s) is %s" % (fname, result)
+        if result == FAILURE:
+            sys.stderr.write("ERROR: Failed to set labname in container %s!\n" % mycontainer_name)
+            sys.exit(1)
+
+# Start my_container_name container
+def StartMyContainer(mycontainer_name):
+    command = "docker start %s 2> /dev/null" % mycontainer_name
+    #print "Command to execute is (%s)" % command
+    result = subprocess.call(command, shell=True)
+    #print "Result of subprocess.call StartMyContainer is %s" % result
+    return result
+
+# Check to see if my_container_name container has been created or not
+def IsContainerCreated(mycontainer_name):
+    command = "docker inspect -f {{.Created}} %s 2> /dev/null" % mycontainer_name
+    #print "Command to execute is (%s)" % command
+    result = subprocess.call(command, shell=True)
+    #print "Result of subprocess.call IsContainerCreated is %s" % result
+    return result
+
+def ConnectNetworkToContainer(mycontainer_name, mysubnet_name, mysubnet_ip):
+    #print "Connecting more network subnet to container %s" % mycontainer_name
+    command = "docker network connect --ip=%s %s %s 2> /dev/null" % (mysubnet_ip, mysubnet_name, mycontainer_name)
+    #print "Command to execute is (%s)" % command
+    result = subprocess.call(command, shell=True)
+    #print "Result of subprocess.call ConnectNetworkToContainer is %s" % result
+    return result
+
+def CreateSingleContainerNonDefault(mycontainer_name, mycontainer_image_name, mysubnet_name, mysubnet_ip):
+    #print "Create Single Container with non-default networking"
+    docker0_IPAddr = getDocker0IPAddr()
+    #print "getDockerIPAddr result (%s)" % docker0_IPAddr
+    createsinglecommand = "docker create -t --network=%s --ip=%s --privileged --add-host my_host:%s --name=%s %s bash" % (mysubnet_name, mysubnet_ip, docker0_IPAddr, mycontainer_name, mycontainer_image_name)
+    #print "Command to execute is (%s)" % createsinglecommand
+    result = subprocess.call(createsinglecommand, shell=True)
+    #print "Result of subprocess.call CreateSingleContainerNonDefault is %s" % result
+    return result
+
+def CreateSingleContainerDefault(mycontainer_name, mycontainer_image_name):
+    #print "Create Single Container with default networking"
+    docker0_IPAddr = getDocker0IPAddr()
+    #print "getDockerIPAddr result (%s)" % docker0_IPAddr
+    createsinglecommand = "docker create -t --privileged --add-host my_host:%s --name=%s %s bash" % (docker0_IPAddr, mycontainer_name, mycontainer_image_name)
+    #print "Command to execute is (%s)" % createsinglecommand
+    result = subprocess.call(createsinglecommand, shell=True)
+    #print "Result of subprocess.call CreateSingleContainerDefault is %s" % result
+    return result
+
+def DoStartSingle(start_config, mycwd, labname):
+    #print "Do: START Single Container with default networking"
+    container_name = start_config.container_name
+    container_image = start_config.container_image
+    container_user = start_config.container_user
+    host_home_xfer = start_config.host_home_xfer
+    haveContainer = IsContainerCreated(container_name)
+    #print "IsContainerCreated result (%s)" % haveContainer
+
+    # IsContainerCreated returned FAILURE if container does not exists
+    if haveContainer == FAILURE:
+        # Container does not exist, create the container
+        containerCreated = CreateSingleContainerDefault(container_name, container_image)
+        #print "CreateSingleContainerDefault result (%s)" % containerCreated
+        # Give the container some time -- just in case
+        time.sleep(3)
+
+    # Check again - 
+    haveContainer = IsContainerCreated(container_name)
+    #print "IsContainerCreated result (%s)" % haveContainer
+
+    # IsContainerCreated returned FAILURE if container does not exists
+    if haveContainer == FAILURE:
+        sys.stderr.write("ERROR: DoStartSingle Container %s still not created!\n" % container_name)
+        sys.exit(1)
+    else:
+        # Start the container
+        start_result = StartMyContainer(container_name)
+        if start_result == FAILURE:
+            sys.stderr.write("ERROR: DoStartSingle Container %s failed to start!\n" % container_name)
+            sys.exit(1)
+
+    copy_result = CopyStudentArtifacts(start_config, container_name, labname)
+    if copy_result == FAILURE:
+        sys.stderr.write("ERROR: DoStartSingle Failed to copy students' artifacts to container %s!\n" % container_name)
+        sys.exit(1)
+
+    # Reach here - Everything is OK - spawn two terminals by default
+    spawn_command = "gnome-terminal -x docker exec -it %s bash -l &" % container_name
+    os.system(spawn_command)
+    os.system(spawn_command)
+
+    return 0
+
+# Create SUBNETS
+def CreateSubnets(subnets):
+    #print "Inside CreateSubnets"
+    #for (subnet_name, subnet_network_mask) in networklist.iteritems():
+    for subnet_name in subnets:
+        subnet_network_mask = subnets[subnet_name].subnet_mask
+        #print "subnet_name is %s" % subnet_name
+        #print "subnet_network_mask is %s" % subnet_network_mask
+
+        command = "docker network inspect %s > /dev/null" % subnet_name
+        #print "Command to execute is (%s)" % command
+        inspect_result = subprocess.call(command, shell=True)
+        #print "Result of subprocess.call CreateSubnets docker network inspect is %s" % inspect_result
+        if inspect_result == FAILURE:
+            # Fail means does not exist - then we can create
+            if subnets[subnet_name].subnet_gateway != None:
+                subnet_gateway = subnets[subnet_name].subnet_gateway
+                command = "docker network create -d bridge --gateway=%s --subnet %s %s 2> /dev/null" % (subnet_gateway, subnet_network_mask, subnet_name)
+            else:
+                command = "docker network create -d bridge --subnet %s %s 2> /dev/null" % (subnet_network_mask, subnet_name)
+            #print "Command to execute is (%s)" % command
+            create_result = subprocess.call(command, shell=True)
+            #print "Result of subprocess.call CreateSubnets docker network create is %s" % create_result
+            if create_result == FAILURE:
+                sys.stderr.write("ERROR: Failed to create %s subnet at %s!\n" % (subnet_name, subnet_network_mask))
+                sys.exit(1)
+        else:
+            print "Already exists! Not creating %s subnet at %s!\n" % (subnet_name, subnet_network_mask)
+        
+
+def DoStartMultiple(start_config, mycwd, labname):
+    container_user = start_config.container_user
+    host_home_xfer = start_config.host_home_xfer
+    #print "Do: START Multiple Containers and/or multi-home networking"
+    docker0_IPAddr = getDocker0IPAddr()
+    #print "getDockerIPAddr result (%s)" % docker0_IPAddr
+
+    networkfilename = '%s/%s.network' % (mycwd, labname)
+    multi_config = ParseInstMulti.ParseInstMulti(networkfilename)
+
+    # Create SUBNETS
+    CreateSubnets(multi_config.subnets)
+
+    for mycontainer_name in multi_config.containers:
+        mycontainer_image_name = multi_config.containers[mycontainer_name].container_image
+
+        haveContainer = IsContainerCreated(mycontainer_name)
+        #print "IsContainerCreated result (%s)" % haveContainer
+
+        # IsContainerCreated returned FAILURE if container does not exists
+        if haveContainer == FAILURE:
+            first_subnet_for_container = True
+            for mysubnet_name in multi_config.containers[mycontainer_name].container_nets:
+                mysubnet_ip = multi_config.containers[mycontainer_name].container_nets[mysubnet_name].ipaddr
+                # First subnet must be part of docker create
+                if first_subnet_for_container:
+                    first_subnet_for_container = False
+                    # Container does not exist, create the container
+                    # Use CreateSingleContainerNonDefault()
+                    #print "My subnet name is %s" % mysubnet_name
+                    #print "My subnet ip is %s" % mysubnet_ip
+                    containerCreated = CreateSingleContainerNonDefault(mycontainer_name, mycontainer_image_name,
+                                             mysubnet_name, mysubnet_ip)
+                    #print "CreateSingleContainerNonDefault result (%s)" % containerCreated
+                    # Give the container some time -- just in case
+                    time.sleep(3)
+                    first_subnet_for_container = False
+                else:
+                # Subsequent subnet must use docker network connect
+                    #print "My subnet name is %s" % mysubnet_name
+                    #print "My subnet ip is %s" % mysubnet_ip
+                    connectNetworkResult = ConnectNetworkToContainer(mycontainer_name, mysubnet_name, mysubnet_ip)
+
+        # Check again - 
+        haveContainer = IsContainerCreated(mycontainer_name)
+        #print "IsContainerCreated result (%s)" % haveContainer
+
+        # IsContainerCreated returned FAILURE if container does not exists
+        if haveContainer == FAILURE:
+            sys.stderr.write("ERROR: DoStartMultiple Container %s still not created!\n" % mycontainer_name)
+            sys.exit(1)
+        else:
+            # Start the container
+            start_result = StartMyContainer(mycontainer_name)
+            if start_result == FAILURE:
+                sys.stderr.write("ERROR: DoStartMultiple Container %s failed to start!\n" % container_name)
+                sys.exit(1)
+
+        copy_result = CopyStudentArtifacts(start_config, mycontainer_name, labname)
+        if copy_result == FAILURE:
+            sys.stderr.write("ERROR: DoStartMultiple Failed to copy students' artifacts to container %s!\n" % mycontainer_name)
+            sys.exit(1)
+    
+    # Reach here - Everything is OK - spawn terminal for each container based on num_terminal
+    for mycontainer_name in multi_config.containers:
+        num_terminal = multi_config.containers[mycontainer_name].term
+        #print "Number of terminal is %d" % num_terminal
+        # If the number of terminal is zero -- do not spawn
+        if num_terminal != 0:
+            for x in range(0, num_terminal):
+                spawn_command = "gnome-terminal -x docker exec -it %s bash -l &" % mycontainer_name
+                os.system(spawn_command)
+
+    return 0
+
+
+# Check existence of /home/$USER/$HOST_HOME_XFER directory - create if necessary
+def CreateHostHomeXfer(host_xfer_dir):
+    # remove trailing '/'
+    host_xfer_dir = host_xfer_dir.rstrip('/')
+    #print "host_home_xfer directory (%s)" % host_xfer_dir
+    if os.path.exists(host_xfer_dir):
+        # exists but is not a directory
+        if not os.path.isdir(host_xfer_dir):
+            # remove file then create directory
+            os.remove(host_xfer_dir)
+            os.makedirs(host_xfer_dir)
+        #else:
+        #    print "host_home_xfer directory (%s) exists" % host_xfer_dir
+    else:
+        # does not exists, create directory
+        os.makedirs(host_xfer_dir)
+
+# Usage: start.py <labname>
+# Arguments:
+#    <labname> - the lab to start
+def main():
+    #print "start.py -- main"
+    if len(sys.argv) != 2:
+        sys.stderr.write("Usage: start.py <labname>\n")
+        sys.exit(1)
+    
+    labname = sys.argv[1]
+    mycwd = os.getcwd()
+    myhomedir = os.environ['HOME']
+    #print "current working directory for %s" % mycwd
+    #print "current user's home directory for %s" % myhomedir
+    #print "ParseInstConfig for %s" % labname
+    startconfigfilename = '%s/start.config' % mycwd
+    start_config = ParseInstConfig.ParseInstructorStartConfig(startconfigfilename, labname)
+
+    # Check existence of /home/$USER/$HOST_HOME_XFER directory - create if necessary
+    host_xfer_dir = '%s/%s' % (myhomedir, start_config.host_home_xfer)
+    CreateHostHomeXfer(host_xfer_dir)
+
+    networkfilename = '%s/%s.network' % (mycwd, labname)
+    # If <labname>.network exists, do multi-containers/multi-home networking
+    # else do single container with default networking
+    if not os.path.exists(networkfilename):
+        DoStartSingle(start_config, mycwd, labname)
+    else:
+        DoStartMultiple(start_config, mycwd, labname)
+
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
+
---
+++ scripts/MyInstructorDocker/stop.py
@@ -0,0 +1,172 @@
+#!/usr/bin/env python
+
+# Filename: stop.py
+# Description:
+# This is the stop script to be run by the instructor.
+# Note:
+# 1. It needs 'start.config' file, where
+#    <labname> is given as a parameter to the script.
+# 2. If the lab has multiple containers and/or multi-home
+#    networking, then <labname>.network file is necessary
+#
+
+import getpass
+import os
+import re
+import subprocess
+import sys
+import zipfile
+import ParseInstMulti
+import ParseInstConfig
+
+# Error code returned by docker inspect
+SUCCESS=0
+FAILURE=1
+
+# CopyChownGradesFile
+def CopyChownGradesFile(mycwd, start_config, container_name, container_image):
+    container_user = start_config.container_user
+    host_home_xfer = start_config.host_home_xfer
+
+    username = getpass.getuser()
+    grade_filename = '/home/%s/grades.txt' % container_user
+    command = "docker cp %s:%s /home/%s/%s" % (container_name, grade_filename, username, host_home_xfer)
+    #print "Command to execute is (%s)" % command
+    result = subprocess.call(command, shell=True)
+    #print "CopyChownGradesFile: Result of subprocess.Popen exec cp grades.txt file is %s" % result
+    if result == FAILURE:
+        sys.stderr.write("ERROR: CopyChownGradesFile Container %s fail on executing cp zip file!\n" % container_name)
+        sys.exit(1)
+
+    # Change ownership to defined user $USER
+    command = "sudo chown %s:%s /home/%s/%s/grades.txt" % (username, username, username, host_home_xfer)
+    #print "Command to execute is (%s)" % command
+    result = subprocess.call(command, shell=True)
+    #print "CopyChownGradesFile: Result of subprocess.Popen exec chown zip file is %s" % result
+    if result == FAILURE:
+        sys.stderr.write("ERROR: CopyChownGradesFile Container %s fail on executing chown zip file!\n" % container_name)
+        sys.exit(1)
+
+
+# Stop my_container_name container
+def StopMyContainer(mycwd, start_config, container_name):
+    command = "docker stop %s 2> /dev/null" % container_name
+    #print "Command to execute is (%s)" % command
+    result = subprocess.call(command, shell=True)
+    #print "Result of subprocess.call StopMyContainer stop is %s" % result
+    return result
+
+# Check to see if my_container_name container has been created or not
+def IsContainerCreated(mycontainer_name):
+    command = "docker inspect -f {{.Created}} %s 2> /dev/null" % mycontainer_name
+    #print "Command to execute is (%s)" % command
+    result = subprocess.call(command, shell=True)
+    #print "Result of subprocess.call IsContainerCreated is %s" % result
+    return result
+
+def DoStopSingle(start_config, mycwd, labname):
+    #print "Do: STOP Single Container with default networking"
+    container_name = start_config.container_name
+    container_image = start_config.container_image
+    haveContainer = IsContainerCreated(container_name)
+    #print "IsContainerCreated result (%s)" % haveContainer
+
+    # IsContainerCreated returned FAILURE if container does not exists
+    # error: can't stop non-existent container
+    if haveContainer == FAILURE:
+        sys.stderr.write("ERROR: DoStopSingle Container %s does not exist!\n" % container_name)
+        sys.exit(1)
+    else:
+        # For single container, the grades.txt can be found in that container
+        # The 'Instructor.py' should have been run inside that container already
+        # and that will create grades.txt file of the result
+        CopyChownGradesFile(mycwd, start_config, container_name, container_image)
+        # Stop the container
+        StopMyContainer(mycwd, start_config, container_name)
+
+    return 0
+
+def DoStopMultiple(start_config, mycwd, labname):
+    container_user = start_config.container_user
+    host_home_xfer = start_config.host_home_xfer
+    #print "Do: STOP Multiple Containers and/or multi-home networking"
+
+    networkfilename = '%s/%s.network' % (mycwd, labname)
+    multi_config = ParseInstMulti.ParseInstMulti(networkfilename)
+
+    for mycontainer_name in multi_config.containers:
+        mycontainer_image = multi_config.containers[mycontainer_name].container_image
+        haveContainer = IsContainerCreated(mycontainer_name)
+        #print "IsContainerCreated result (%s)" % haveContainer
+
+        # IsContainerCreated returned FAILURE if container does not exists
+        # error: can't stop non-existent container
+        if haveContainer == FAILURE:
+            sys.stderr.write("ERROR: DoStopMultiple Container %s does not exist!\n" % container_name)
+            sys.exit(1)
+        else:
+            # The grades.txt can be found in container named multi_config.grade_containername
+            # The 'Instructor.py' should have been run inside that container already
+            # and that will create grades.txt file of the result
+            if mycontainer_name == multi_config.grade_containername:
+                CopyChownGradesFile(mycwd, start_config, mycontainer_name, mycontainer_image)
+            # Stop the container
+            StopMyContainer(mycwd, start_config, mycontainer_name)
+
+    return 0
+
+# Check existence of /home/$USER/$HOST_HOME_XFER directory - create if necessary
+def CreateHostHomeXfer(host_xfer_dir):
+    # remove trailing '/'
+    host_xfer_dir = host_xfer_dir.rstrip('/')
+    #print "host_home_xfer directory (%s)" % host_xfer_dir
+    if os.path.exists(host_xfer_dir):
+        # exists but is not a directory
+        if not os.path.isdir(host_xfer_dir):
+            # remove file then create directory
+            os.remove(host_xfer_dir)
+            os.makedirs(host_xfer_dir)
+        #else:
+        #    print "host_home_xfer directory (%s) exists" % host_xfer_dir
+    else:
+        # does not exists, create directory
+        os.makedirs(host_xfer_dir)
+
+# Usage: stop.py <labname>
+# Arguments:
+#    <labname> - the lab to stop
+def main():
+    #print "stop.py -- main"
+    if len(sys.argv) != 2:
+        sys.stderr.write("Usage: stop.py <labname>\n")
+        sys.exit(1)
+    
+    labname = sys.argv[1]
+    mycwd = os.getcwd()
+    myhomedir = os.environ['HOME']
+    #print "current working directory for %s" % mycwd
+    #print "current user's home directory for %s" % myhomedir
+    #print "ParseInstConfig for %s" % labname
+    startconfigfilename = '%s/start.config' % mycwd
+    start_config = ParseInstConfig.ParseInstructorStartConfig(startconfigfilename, labname)
+
+    # Check existence of /home/$USER/$HOST_HOME_XFER directory - create if necessary
+    host_xfer_dir = '%s/%s' % (myhomedir, start_config.host_home_xfer)
+    CreateHostHomeXfer(host_xfer_dir)
+
+    networkfilename = '%s/%s.network' % (mycwd, labname)
+    # If <labname>.network exists, do multi-containers/multi-home networking
+    # else do single container with default networking
+    if not os.path.exists(networkfilename):
+        DoStopSingle(start_config, mycwd, labname)
+    else:
+        DoStopMultiple(start_config, mycwd, labname)
+
+    # Inform user where results are stored
+    print "Results (grades.txt) stored in directory: %s" % host_xfer_dir
+
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
+
