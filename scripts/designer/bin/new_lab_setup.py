03e72497bbfb3947aa5674b2b584443502abb7ee
mfthomps <mfthomps@ed162e55-3153-4343-a18d-e086fa355ed4>
2017-03-28 16:18:38 +0000

    customize start.config for automated seed and default container names
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@214 ed162e55-3153-4343-a18d-e086fa355ed4

--- scripts/designer/bin/new_lab_setup.py
+++ scripts/designer/bin/new_lab_setup.py
@@ -2,6 +2,11 @@
 import os
 import shutil
 import glob
+import pwd
+'''
+Copy a set of initial lab configuration files into a new lab and
+adjust their names and content to reflect the lab name.
+'''
 SEED_DIR=None
 try:
     SEED_DIR = os.environ['SEED_DIR']
@@ -21,7 +26,26 @@ for source in template_dirs:
         print('error copying %s to %s, expected %s to be empty' % (source, here, here))
         exit(1)
 
+''' alter template file names, except those that will have altered content '''
+start_config_template = 'config/start.config.template'
+start_config_file = 'config/start.config'
 adapt_list = glob.glob(here+'/*/*template*')
 for a in adapt_list:
-    new = a.replace('template', labname)
-    shutil.move(a, new)
+    if not a.endswith(start_config_template):
+        new = a.replace('template', labname)
+        shutil.move(a, new)
+
+default_string = 'default'
+seed_label = 'LAB_MASTER_SEED'
+myname = pwd.getpwuid(os.getuid()).pw_name
+with open(start_config_template) as fh:
+  with open(start_config_file, 'w') as out_fh:
+    for line in fh:
+        if line.strip().startswith(seed_label):
+            out_fh.write('\t%s %s_%s_master_seed\n' % (seed_label, labname, myname))
+        elif not line.strip().startswith('#') and default_string in line:
+            new_line = line.replace(default_string, labname)
+            out_fh.write(new_line)
+        else:
+            out_fh.write(line)
+ 
