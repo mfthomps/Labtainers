28b59d3f7cad16f913287340c3bca802620c0c99
mfthomps <mfthomps@ed162e55-3153-4343-a18d-e086fa355ed4>
2017-03-28 23:13:38 +0000

    revised container naming, rebuild image if needed
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@218 ed162e55-3153-4343-a18d-e086fa355ed4

--- scripts/MyStudentDocker/buildImage.sh
+++ scripts/MyStudentDocker/buildImage.sh
@@ -3,36 +3,25 @@
 # Usage: buildImage.sh <labname> [<imagename>]
 #        <imagename> is optional for lab that only has one image
 lab=$1
-has_image=0
 if [ "$#" -eq 2 ]; then
     imagename=$2
     labimage=$lab.$imagename
-    has_image=1
 else
-    labimage=$lab
+    imagename=$lab
     labimage=$lab.$lab
-    has_image=1
 fi
 
-if [ $has_image == 1 ]; then
-    echo "Labname is $lab with image name $imagename"
-else
-    echo "Labname is $lab using default image"
-fi
+echo "Labname is $lab with image name $imagename"
 
 LAB_DIR=`realpath ../../labs/$lab/`
 if [ ! -d $LAB_DIR ]; then
     echo "$LAB_DIR not found as a lab directory"
     exit
 fi
-if [ $has_image == 1 ]; then
-    LABIMAGE_DIR=`realpath ../../labs/$lab/$imagename/`
-    if [ ! -d $LABIMAGE_DIR ]; then
-        echo "$LABIMAGE_DIR not found"
-        exit
-    fi
-else
-    LABIMAGE_DIR=`realpath ../../labs/$lab/`
+LABIMAGE_DIR=`realpath ../../labs/$lab/$imagename/`
+if [ ! -d $LABIMAGE_DIR ]; then
+    echo "$LABIMAGE_DIR not found"
+    exit
 fi
 
 ORIG_PWD=`pwd`
@@ -53,6 +42,7 @@ tar --atime-preserve -zcvf $LAB_TAR .local *
 cd $ORIG_PWD
 dfile=Dockerfile.$labimage.student
 cp $LAB_DIR/dockerfiles/$dfile .
+
 docker build --build-arg lab=$labimage -f ./$dfile -t $labimage:student .
 echo "removing temporary $dfile, reference original in $LAB_DIR/dockerfiles/$dfile"
 rm ./$dfile
--- scripts/MyStudentDocker/redo.py
+++ scripts/MyStudentDocker/redo.py
@@ -2,7 +2,8 @@
 
 # Filename: redo.py
 # Description:
-# For lab development testing workflow
+# For lab development testing workflow.  This will stop containers of a lab, create or update lab images
+# and start the containers.
 #
 
 import os
@@ -11,8 +12,64 @@ import sys
 import ParseStartConfig
 import start
 import stop
+import time
+import datetime
 
 LABS_ROOT = os.path.abspath("../../labs/")
+
+def FileModLater(ts, fname):
+    ''' is the given file later than the timestamp (which is in UTC)? '''
+    df_time = os.path.getmtime(fname)
+    #print('df ts %s' % df_time)
+
+    df_string = datetime.datetime.fromtimestamp(df_time)
+    #print('df_local time is %s' % df_string)
+
+    df_utc_string = str(datetime.datetime.utcfromtimestamp(df_time))
+    parts = df_utc_string.split('.')
+    df_ts = time.mktime(time.strptime(parts[0], "%Y-%m-%d %H:%M:%S"))
+
+    #print('df_utc time is %s' % df_utc_string)
+    #print('df_utc ts is %s' % df_ts)
+    if df_ts > ts:
+        return True
+    else:
+        return False
+
+def CheckBuild(labname, image_name, container_name, name):
+    '''
+    Determine if a container image needs to be rebuilt.
+    '''
+    retval = False
+    print('check build for container %s image %s' % (container_name, image_name))
+    cmd = "docker inspect -f '{{.Created}}' %s" % image_name
+    child = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+    result = child.stdout.read().strip()
+    if 'Error:' in result or len(result.strip()) == 0:
+        return True
+    print('result is %s' % result)
+    parts = result.strip().split('.')
+    time_string = parts[0]
+    #print('image time string %s' % time_string)
+    ts = time.mktime(time.strptime(time_string, "%Y-%m-%dT%H:%M:%S"))
+    #print('image ts %s' % ts)
+    lab_path = os.path.join(LABS_ROOT,labname)
+    df_name = 'Dockerfile.%s' % container_name
+    df = os.path.join(lab_path, 'dockerfiles', df_name)
+    if FileModLater(ts, df):
+        print('dockerfile changed, would build')
+        retval = True
+    else:
+        container_dir = os.path.join(lab_path, name)
+        #print('container dir %s' % container_dir)
+        for folder, subs, files in os.walk(container_dir):
+            for f in files:
+               f_path = os.path.join(folder, f)
+               if FileModLater(ts, f_path):
+                   print('%s is later, would build' % f_path)
+                   retval = True
+                   break
+    return retval
 
 def RedoLab(labname):
     mycwd = os.getcwd()
@@ -27,9 +84,14 @@ def RedoLab(labname):
     stop.StopLab(labname)
     for name, container in start_config.containers.items():
         mycontainer_name       = container.full_name
+        mycontainer_image_name = container.image_name
         cmd = 'docker rm %s' % mycontainer_name
         os.system(cmd)
+        if CheckBuild(labname, mycontainer_image_name, mycontainer_name, name):
+            cmd = './buildImage.sh %s' % labname 
+            os.system(cmd)
     start.StartLab(labname)
+
 
 # Usage: redo.py <labname>
 # Arguments:
