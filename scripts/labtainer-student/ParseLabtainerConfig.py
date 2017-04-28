378975e85e765f6ece3bea8a6889265bb9f17f5e
jkhosali <jkhosali@ed162e55-3153-4343-a18d-e086fa355ed4>
2017-04-28 17:14:29 +0000

    Modified labtainer.config and added ParseLabtainerConfig.py
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@367 ed162e55-3153-4343-a18d-e086fa355ed4

---
+++ scripts/labtainer-student/ParseLabtainerConfig.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+class ParseLabtainerConfig():
+    def __init__(self, fname, labname):
+        self.labname = labname
+        self.host_home_xfer= "" # HOST_HOME_XFER - directory to transfer artifact to/from containers
+        self.testsets_root= None # TESTSETS_ROOT - regression test root
+
+        if not os.path.exists(fname):
+            sys.stderr.write("ParseLabtainerConfig, Config file %s does not exists!\n" % fname)
+            sys.exit(1)
+
+        self.get_configs(fname)
+        self.finalize()
+        self.validate()
+
+    def get_configs(self, fname):
+        """Reads the new config format. There is basically no format validation so 
+           this accidentally supports a much more flexible format right now, which 
+           is bad. It does check for unknown config options, which is good. The main
+           advantage is that I think the parsing method should be easy to extend."""
+        active      = None
+        defaults_ok = {"global_settings"}
+        with open(fname, "r") as f:
+            for line in f:
+                linestrip = line.strip()
+                if not linestrip or linestrip.startswith("#"):
+                    continue
+                keyval = linestrip.split()    
+                key = keyval[0].lower()
+                if len(keyval) > 1:
+                    val = keyval[1].lower()
+                elif key in defaults_ok:
+                    val = "default"
+                else:
+                    sys.stderr.write("ParseLabtainerConfig, Fatal. Missing value for: %s" % line)
+                    exit(1)
+
+                if key == "global_settings":
+                    active = self
+                elif hasattr(active, key):
+                    setattr(active, key, val) 
+                else:
+                    sys.stderr.write("ParseLabtainerConfig, Fatal. Can't understand config setting: %s" % line)
+                    exit(1)
+
+    def validate(self):
+        """ Checks to make sure we have all the info we need from the user."""
+        if not self.host_home_xfer:
+            sys.stderr.write("ERROR: Missing host_home_xfer in labtainer.config!\n")
+            exit(1)
+        if not self.testsets_root:
+            sys.stderr.write("ERROR: Missing testsets_root in labtainer.config!\n")
+            exit(1)
+        
+    def finalize(self):
+        """Combines info provided by user with what we already know about the
+           lab to get the final settings we want."""
+        # fixing up global parameters
+        self.host_home_xfer = os.path.join(self.host_home_xfer,self.labname)
+        self.testsets_root = os.path.join(os.path.abspath(self.testsets_root), self.labname)
+
+    def show_current_settings(self):
+        bar = "="*80
+        print bar
+        print("Global configuration settings:")
+        print bar
+        for key, val in self.__dict__.items():
+            if type(val) == type({}): val = len(val)
+            print "\t" + str(key) + ": " + str(val)
+        print "\n"+bar
+             
+if __name__ == '__main__':
+    labtainer_config = ParseLabtainerConfig(*sys.argv[1:])
+    labtainer_config.show_current_settings()
