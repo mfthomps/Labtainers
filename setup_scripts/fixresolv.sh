60677791aef3f1d2d50548479a6cf6a1955b004d
mfthomps <mfthomps@ed162e55-3153-4343-a18d-e086fa355ed4>
2017-05-01 16:33:24 +0000

    add header / copyright
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@384 ed162e55-3153-4343-a18d-e086fa355ed4

--- setup_scripts/fixresolv.sh
+++ setup_scripts/fixresolv.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-
+: <<'END'
+This software was created by United States Government employees at 
+The Center for the Information Systems Studies and Research (CISR) 
+at the Naval Postgraduate School NPS.  Please note that within the 
+United States, copyright protection is not available for any works 
+created  by United States Government employees, pursuant to Title 17 
+United States Code Section 105.   This software is in the public 
+domain and is not subject to copyright. 
+END
 sudo rm -f /tmp/fixresolv.tmp
 echo "search ern.nps.edu" > /tmp/fixresolv.tmp
 echo "nameserver 172.20.20.12" >> /tmp/fixresolv.tmp
--- setup_scripts/installDocker.sh
+++ setup_scripts/installDocker.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+: <<'END'
+This software was created by United States Government employees at 
+The Center for the Information Systems Studies and Research (CISR) 
+at the Naval Postgraduate School NPS.  Please note that within the 
+United States, copyright protection is not available for any works 
+created  by United States Government employees, pursuant to Title 17 
+United States Code Section 105.   This software is in the public 
+domain and is not subject to copyright. 
+END
 #
 # Install Docker on an Ubuntu system, along with other packages required by Labtainers
 #
