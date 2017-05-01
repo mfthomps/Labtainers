76b225d25c66df66809d6da520ebfbcd4670be13
mfthomps <mfthomps@ed162e55-3153-4343-a18d-e086fa355ed4>
2017-05-01 16:29:50 +0000

    add header / copyright
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@381 ed162e55-3153-4343-a18d-e086fa355ed4

--- scripts/labtainer-student/bin/bash-pre-capinout.sh
+++ scripts/labtainer-student/bin/bash-pre-capinout.sh
@@ -1,3 +1,12 @@
+: <<'END'
+This software was created by United States Government employees at 
+The Center for the Information Systems Studies and Research (CISR) 
+at the Naval Postgraduate School NPS.  Please note that within the 
+United States, copyright protection is not available for any works 
+created  by United States Government employees, pursuant to Title 17 
+United States Code Section 105.   This software is in the public 
+domain and is not subject to copyright. 
+END
 #
 # Invoke the command in $1 using the capinout.sh script, 
 # but only if it is not a system command.  Checks the
