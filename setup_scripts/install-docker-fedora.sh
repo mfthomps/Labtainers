8201f500a8581563e88f2348b5a1e7722bcd5443
jkhosali <jkhosali@ed162e55-3153-4343-a18d-e086fa355ed4>
2017-05-02 19:01:56 +0000

    Added install-docker-fedora.sh and rename installDocker.sh to install-docker-ubuntu.sh
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@397 ed162e55-3153-4343-a18d-e086fa355ed4

---
+++ setup_scripts/install-docker-fedora.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+: <<'END'
+This software was created by United States Government employees at 
+The Center for the Information Systems Studies and Research (CISR) 
+at the Naval Postgraduate School NPS.  Please note that within the 
+United States, copyright protection is not available for any works 
+created  by United States Government employees, pursuant to Title 17 
+United States Code Section 105.   This software is in the public 
+domain and is not subject to copyright. 
+END
+#
+# Install Docker on a Fedora system, along with other packages required by Labtainers
+#
+read -p "This script will reboot the system when done, press enter to continue"
+
+sudo dnf upgrade
+sudo dnf install mod_dav_svn subversion python docker
+sudo pip install netaddr
+sudo systemctl start docker
+sudo systemctl enable docker
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+sudo reboot
