0060e9e65085a707081d9b3f9c816d3aeca17bdf
mfthomps <mfthomps@ed162e55-3153-4343-a18d-e086fa355ed4>
2017-05-03 00:04:31 +0000

    descibe purpose of script
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@405 ed162e55-3153-4343-a18d-e086fa355ed4

--- setup_scripts/fixresolv.sh
+++ setup_scripts/fixresolv.sh
@@ -8,6 +8,13 @@ created  by United States Government employees, pursuant to Title 17
 United States Code Section 105.   This software is in the public 
 domain and is not subject to copyright. 
 END
+: <<'END'
+This script modifies the resolv.conf file for use with 
+Dockers installed on a VM.  The resolv.conf file is given
+entries to use the NPS name servers to resolve addresses.
+It should be modified to reflect your local domain and DNS servers.
+END
+
 sudo rm -f /tmp/fixresolv.tmp
 echo "search ern.nps.edu" > /tmp/fixresolv.tmp
 echo "nameserver 172.20.20.12" >> /tmp/fixresolv.tmp
