0e66d1283fb7b126a642a8738ebbed49dc4fad5e
mfthomps <mfthomps@ed162e55-3153-4343-a18d-e086fa355ed4>
2017-05-11 21:23:16 +0000

    was looping into next treataslocal
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@480 ed162e55-3153-4343-a18d-e086fa355ed4

--- scripts/labtainer-student/bin/bash-pre-capinout.sh
+++ scripts/labtainer-student/bin/bash-pre-capinout.sh
@@ -7,6 +7,22 @@ created  by United States Government employees, pursuant to Title 17
 United States Code Section 105.   This software is in the public 
 domain and is not subject to copyright. 
 END
+treatlocal(){
+   local cmd_path=$1
+   local TAS=/home/ubuntu/.local/bin/treataslocal
+   if [ -f $TAS ]
+   then
+       # Get the list of commands from treataslocal
+       while read cmdlocal; do
+           if [[ "$cmd_path" == "$cmdlocal" ]]; then
+               return 1
+           else
+               continue
+           fi
+       done <$TAS
+    fi
+    return 0
+}
 #
 # Invoke the command in $1 using the capinout.sh script, 
 # but only if it is not a system command.  Checks the
@@ -29,20 +45,11 @@ preexec() {
        else
           cmd_path=`which ${stringarray[0]}`
        fi
-       # If file /home/ubuntu/.local/bin/treataslocal exist, run it
-       if [ -f /home/ubuntu/.local/bin/treataslocal ]
-       then
-           # Get the list of commands from treataslocal
-           cmdlocallist=`cat /home/ubuntu/.local/bin/treataslocal`
-           for cmdlocal in $cmdlocallist; do
-               if [[ "$cmd_path" == "$cmdlocal" ]]; then
-                   #echo "Treat as local command (specified in treataslocal)"
-                   capinout.sh "$1"
-                   return 1
-               else
-                   continue
-               fi
-           done
+       treatlocal $cmd_path
+       result=$?
+       if [ $result == 1 ]; then
+           capinout.sh "$1"
+           return 1
        fi
        if [[ ! -z $cmd_path ]] && [[ "$cmd_path" != /usr/* ]] && \
           [[ "$cmd_path" != /bin/* ]] && [[ "$cmd_path" != /sbin/* ]]; then
