a7c622ee6ddcaaefca6c2d3f4bf0184b6eab6865
mfthomps <mfthomps@ed162e55-3153-4343-a18d-e086fa355ed4>
2017-05-12 00:14:02 +0000

    pass logger to ParseStartConfig
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@483 ed162e55-3153-4343-a18d-e086fa355ed4

--- scripts/labtainer-instructor/moreterm.py
+++ scripts/labtainer-instructor/moreterm.py
@@ -96,7 +96,7 @@ def main():
     config_path       = os.path.join(lab_path,"config")
     start_config_path = os.path.join(config_path,"start.config")
 
-    start_config = ParseStartConfig.ParseStartConfig(start_config_path, labname, "instructor")
+    start_config = ParseStartConfig.ParseStartConfig(start_config_path, labname, "instructor", labutils.logger)
 
     DoMoreterm(start_config, mycwd, labname, container, requested_term)
 
--- scripts/labtainer-instructor/unpause.py
+++ scripts/labtainer-instructor/unpause.py
@@ -95,7 +95,7 @@ def main():
     config_path       = os.path.join(lab_path,"config")
     start_config_path = os.path.join(config_path,"start.config")
 
-    start_config = ParseStartConfig.ParseStartConfig(start_config_path, labname, "instructor")
+    start_config = ParseStartConfig.ParseStartConfig(start_config_path, labname, "instructor", labutils.logger)
 
     DoUnpause(start_config, mycwd, labname)
 
