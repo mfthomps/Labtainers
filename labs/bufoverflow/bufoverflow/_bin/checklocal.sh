2eb3c2d63add6d1477e1a614343a7c6cc2df6d57
mfthomps <mfthomps@ed162e55-3153-4343-a18d-e086fa355ed4>
2017-08-02 23:58:51 +0000

    checklocal no longer takes parameters, capinout handles output file
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@572 ed162e55-3153-4343-a18d-e086fa355ed4

--- labs/bufoverflow/bufoverflow/_bin/checklocal.sh
+++ labs/bufoverflow/bufoverflow/_bin/checklocal.sh
@@ -2,23 +2,9 @@
 
 # checklocal.sh
 # Description:
-#     This file should contain checks for local setting (such as sysctl)
-#     specific for each lab
+#     This file should contain checks for local settings (such as sysctl)
+#     specific for each lab.  The resulting output will go into the
+#     checklocal.stdout.timestamp file
 #
-# Usage: checklocal.sh <outputfile>
-# Arguments:
-#     <outputfile> - where to store output
-
-if [ $# -ne 1 ]
-then
-    echo "Usage: checklocal.sh <outputfile>"
-    echo "       <outputfile> - where to store output"
-    exit 1
-fi
-
-OUTPUTFILE=$1
-echo "" > $OUTPUTFILE
-
 # Get status of kernel.randomize_va_space
-sudo sysctl -a | grep kernel.randomize_va_space >> $OUTPUTFILE
-
+sudo sysctl -a | grep kernel.randomize_va_space
