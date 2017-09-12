2936531eac1abede8840abf46792c60f74f285de
mfthomps <mfthomps@ed162e55-3153-4343-a18d-e086fa355ed4>
2017-09-12 18:01:10 +0000

    build the executables in fixlocal
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@663 ed162e55-3153-4343-a18d-e086fa355ed4

--- labs/bufoverflow/bufoverflow/_bin/fixlocal.sh
+++ labs/bufoverflow/bufoverflow/_bin/fixlocal.sh
@@ -16,12 +16,5 @@ sudo rm -f /bin/sh
 # 2. Re-create /bin/sh (soft-linked to /bin/zsh instead)
 sudo ln -s /bin/zsh /bin/sh
 
-# These are no longer needed as executable stack is no longer provided by default
-## 3. Change 'stack' executable found in /home/ubuntu ownership
-#sudo chown root:root /home/ubuntu/stack
-#
-## 4. Change 'stack' executable found in /home/ubuntu permission
-##    Note the stick bit
-#sudo chmod 4755 /home/ubuntu/stack
-
-
+cd $HOME
+./compile.sh
