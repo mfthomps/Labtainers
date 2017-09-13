74ca3b603d16d38ac8144cf351792316cd39121e
mfthomps <mfthomps@ed162e55-3153-4343-a18d-e086fa355ed4>
2017-09-13 15:50:38 +0000

    initial cut at one install script
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@668 ed162e55-3153-4343-a18d-e086fa355ed4

---
+++ setup_scripts/install-Labtainers.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+$here=`pwd`
+cd trunk/setup_scripts
+found_distrib=`cat /etc/*-release | grep "^DISTRIB_ID" | awk -F "=" '{print $2}'`
+if [[ -z "$1" ]]; then
+    distrib=$found_distrib
+else
+    distrib=$1
+fi
+case "$distrib" in
+    Ubuntu)
+        echo is ubuntu
+        ./install-docker-ubuntu.sh
+        ;;
+    Debian)
+        echo is debian
+        ./install-docker-debian.sh
+        ;;
+    Fedora)
+        echo is fedora
+        ./install-docker-fedora.sh
+        ;;
+    Centos)
+        echo is centos
+        ./install-docker-centos.sh
+        ;;
+    *)
+        if [[ -z "$1" ]]; then
+            echo "Did not recognize distribution: $found_distrib"
+            echo "Try providing distribution as argument, either Ubuntu|Debian|Fedora|Centos"
+        else
+            echo $"Usage: $0 Ubuntu|Debian|Fedora|Centos"
+        fi
+        exit 1
+esac
+
