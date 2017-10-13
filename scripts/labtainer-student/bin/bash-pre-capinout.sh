e20a1654d1051bb4ce2daeb4d2f6ff71a7fcdd71
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2017-10-13 18:29:23 +0000

    support program output identifiers in the treataslocal file
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@825 ed162e55-3153-4343-a18d-e086fa355ed4

--- scripts/labtainer-student/bin/bash-pre-capinout.sh
+++ scripts/labtainer-student/bin/bash-pre-capinout.sh
@@ -7,14 +7,68 @@ created  by United States Government employees, pursuant to Title 17
 United States Code Section 105.   This software is in the public 
 domain and is not subject to copyright. 
 END
+local_output=""
+getlocaloutput(){
+   # find command line paramters that match requested treataslocal
+   # token. or just return filename if that is what the token reflects.
+   local in_param=$1
+   #echo "in_param is $in_param"
+   IFS=':' read -r -a array <<< "$in_param"
+   #read -r -a array <<< "$in_param"
+   if [[ ${#array[@]} == "2" ]];then
+       delim_type=${array[0]}
+       delim_value=${array[1]}
+       index=0
+       for field in "${cmd_line_array[@]}";do
+           #echo "check field $field indix is $index"
+           case $delim_type in
+               starts)
+                  if [[ $field == $delim_value* ]]; then
+                      plen=${#delim_value}
+                      local_output=${field:$plen}
+                      return 0 
+                  fi
+                  ;;
+               file)
+                  local_output=$delim_value
+                  return 0 
+                  ;;
+               follows)
+                  if [[ $field == $delim_value ]]; then
+                      index=$(expr $index + 1)
+                      local_output=${cmd_line_array[$index]}
+                      return 0 
+                  fi
+                  ;;
+               *)
+                  echo "unknown delimiter type $delim_type"
+                  exit 1
+           esac
+           index=$(expr $index + 1)
+       done 
+   fi
+}
 treatlocal(){
    local cmd_path=$1
    local TAS=$HOME/.local/bin/treataslocal
    if [ -f $TAS ]
    then
+       local_output=""
        # Get the list of commands from treataslocal
        while read cmdlocal; do
-           if [[ "$cmd_path" == "$cmdlocal" ]]; then
+           if [[ $cmdlocal == #* ]]; then
+               continue
+           fi
+           read -r -a cmd_array <<< "$cmdlocal"
+           the_command=${cmd_array[0]}
+           #echo "the command $the_command"
+           if [[ "$cmd_path" == "$the_command" ]]; then
+               if [[ ${#cmd_array[@]} == "2" ]];then
+                  the_param=${cmd_array[1]}
+                  #echo "the_param is $the_param"
+                  getlocaloutput $the_param 
+                  #echo "set local to $local_output"
+               fi
                return 1
            else
                continue
@@ -64,6 +118,7 @@ forcecheck(){
 #
 preexec() {
    #echo "just typed $1";
+   timestamp=$(date +"%Y%m%d%H%M%S")
    if [[ "$1" == "exit" ]]; then
        return 0
    fi
@@ -84,11 +139,11 @@ preexec() {
        # TBD test for only one pipe
        # 
        counter=$[$counter +1]
-       stringarray=($command)
-       if [ ${stringarray[0]} == "sudo" ]; then
-          cmd_path=`which ${stringarray[1]}`
+       cmd_line_array=($command)
+       if [ ${cmd_line_array[0]} == "sudo" ]; then
+          cmd_path=`which ${cmd_line_array[1]}`
        else
-          cmd_path=`which ${stringarray[0]}`
+          cmd_path=`which ${cmd_line_array[0]}`
        fi
        # do we want to run checklocal on this command, though it is not otherwise tracked?
        forcecheck $cmd_path
@@ -109,7 +164,13 @@ preexec() {
        result=$?
        if [ $result == 1 ]; then
            #echo "will treat as local"
-           capinout.sh "$1" $counter
+           capinout.sh "$1" $counter $timestamp
+           if [[ ! -z "$local_output" ]]; then
+               # we are to timestamp a program output file 
+               #echo "local output is $local_output"
+               just_command=$(basename $cmd_path)
+               cp $local_output $HOME/.local/result/$just_command.prgout.$timestamp
+           fi
            return 1
        fi
        # do we ignore a non-system command?
@@ -121,7 +182,7 @@ preexec() {
        if [[ ! -z $cmd_path ]] && [[ "$cmd_path" != /usr/* ]] && \
           [[ "$cmd_path" != /bin/* ]] && [[ "$cmd_path" != /sbin/* ]]; then
            #echo "would do this command $1"
-           capinout.sh "$1" $counter
+           capinout.sh "$1" $counter $timestamp
            return 1
        fi
    done
