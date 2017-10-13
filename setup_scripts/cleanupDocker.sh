288f6bf5c5b80c5d50ceeae7b3d53f3bd548c32c
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2017-10-13 19:04:30 +0000

    do not remove exited containers, only dead ones
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@828 ed162e55-3153-4343-a18d-e086fa355ed4

--- setup_scripts/cleanupDocker.sh
+++ setup_scripts/cleanupDocker.sh
@@ -10,7 +10,7 @@ domain and is not subject to copyright.
 END
 
 # remove exited containers:
-docker ps --filter status=dead --filter status=exited -aq | xargs -r docker rm -v
+docker ps --filter status=dead -aq | xargs -r docker rm -v
     
 # remove unused images:
 docker images --no-trunc | grep '<none>' | awk '{ print $3 }' | xargs -r docker rmi
