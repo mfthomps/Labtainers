4f218bb1e0f28e603fd7f00d9cd017960c31ad1b
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2017-10-16 19:50:27 +0000

    use echo, not print
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@853 ed162e55-3153-4343-a18d-e086fa355ed4

--- setup_scripts/dependent.sh
+++ setup_scripts/dependent.sh
@@ -1,4 +1,6 @@
-#docker inspect --format='{{.Id}} {{.Parent}}' $(docker images --filter since=$1 -q) 
+#
+# delete all images that are dependent on a given image
+#
 list=$(docker inspect --format='{{.Id}} {{.Parent}}' $(docker images --filter since=$1 -q) | awk '{print substr($1,8,12)}')
 echo $list
 docker rmi -f $list
