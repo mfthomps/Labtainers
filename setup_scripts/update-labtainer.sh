30271738c7b852bf3221b5321bd4a1d38447a551
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2017-10-30 17:20:54 +0000

    pull network baseline and centos image as part of install and update
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@953 ed162e55-3153-4343-a18d-e086fa355ed4

--- setup_scripts/update-labtainer.sh
+++ setup_scripts/update-labtainer.sh
@@ -2,3 +2,6 @@
 wget https://my.nps.edu/documents/107523844/109121513/labtainer.tar/6fc80410-e87d-4e47-ae24-cbb60c7619fa -O labtainer.tar
 cd ..
 tar xf labtainer/labtainer.tar
+docker pull mfthomps/labtainer.base
+docker pull mfthomps/labtainer.network
+docker pull mfthomps/labtainer.centos
