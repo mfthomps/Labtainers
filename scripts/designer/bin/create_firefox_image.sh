fb880ee06323d0e252e1d82d3852cfd7fa46d8e8
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2017-11-03 22:56:43 +0000

    lamp and firefox images, login and funbuffer for centos
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@986 ed162e55-3153-4343-a18d-e086fa355ed4

---
+++ scripts/designer/bin/create_firefox_image.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+: <<'END'
+This software was created by United States Government employees at 
+The Center for the Information Systems Studies and Research (CISR) 
+at the Naval Postgraduate School NPS.  Please note that within the 
+United States, copyright protection is not available for any works 
+created  by United States Government employees, pursuant to Title 17 
+United States Code Section 105.   This software is in the public 
+domain and is not subject to copyright. 
+END
+read -p "This buid will pull from the docker hub.  Have you published the base?"
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    echo exiting
+    exit
+fi
+here=`pwd`
+cd ../
+docker build -f base_dockerfiles/Dockerfile.labtainer.firefox -t labtainer.firefox:latest .
+cd $here
