8b24e66330f5592b049cc2a512730802aa7ea46e
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2017-11-10 01:34:42 +0000

    Modified new_lab_setup.py : if cloning a container, use the template configuration files (parameter.config, results.config and goals.config) instead. When adding a container, comment out XTERM line by default.
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@1035 ed162e55-3153-4343-a18d-e086fa355ed4

--- scripts/designer/bin/new_lab_setup.py
+++ scripts/designer/bin/new_lab_setup.py
@@ -74,6 +74,8 @@ def handle_add_container(tdir, newcontainer):
             container_default_line_found = True
         if container_default_line_found:
             new_line = line.replace(default_string, newcontainer)
+            if "XTERM" in line:
+                new_line = new_line.replace("XTERM", "#XTERM")
             start_config_file.write(new_line)
     start_config_file.close()
 
@@ -155,6 +157,21 @@ def handle_clone_lab(tdir, newlabname):
         for line in config_filelines:
             newline = line.replace(oldlabname, newlabname)
             start_config_file.write(newline)
+
+    # If we are cloning - fix the 'parameter.config', 'results.config' and 'goals.config'
+    # i.e., use the templates configuration files instead
+    parameter_config = os.path.join(newlabpath, 'config', 'parameter.config')
+    results_config = os.path.join(newlabpath, 'instr_config', 'results.config')
+    goals_config = os.path.join(newlabpath, 'instr_config', 'goals.config')
+    template_parameter_config = os.path.join(tdir, 'config', 'parameter.config')
+    template_results_config = os.path.join(tdir, 'instr_config', 'results.config')
+    template_goals_config = os.path.join(tdir, 'instr_config', 'goals.config')
+    os.remove(parameter_config)
+    os.remove(results_config)
+    os.remove(goals_config)
+    shutil.copy(template_parameter_config, parameter_config)
+    shutil.copy(template_results_config, results_config)
+    shutil.copy(template_goals_config, goals_config)
 
 def handle_replace_container(tdir, oldcontainer, newcontainer):
     # This assumes directories 'config', 'dockerfiles' and 'instr_config'
