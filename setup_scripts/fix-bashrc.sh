07df3216bf68c58a945928a685078635fda19574
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2017-11-15 00:19:25 +0000

    separate pull script so update need not be run twice.
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@1056 ed162e55-3153-4343-a18d-e086fa355ed4

---
+++ setup_scripts/fix-bashrc.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+target=~/.bashrc
+grep ":./bin:" $target >>/dev/null
+result=$?
+if [[ result -ne 0 ]];then
+   cat <<EOT >>$target
+   if [[ ":\$PATH:" != *":./bin:"* ]]; then 
+       export PATH="\${PATH}:./bin"
+   fi
+EOT
+fi
