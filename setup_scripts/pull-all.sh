d0871a54de2d221bb603b0a824b69aa8715b598e
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2017-11-15 00:19:25 +0000

    separate pull script so update need not be run twice.
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@1056 ed162e55-3153-4343-a18d-e086fa355ed4

---
+++ setup_scripts/pull-all.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+    docker pull mfthomps/labtainer.base
+    docker pull mfthomps/labtainer.network
+    docker pull mfthomps/labtainer.firefox
+    docker pull mfthomps/labtainer.java
+    docker pull mfthomps/labtainer.centos
+    docker pull mfthomps/labtainer.lamp
--- setup_scripts/update-labtainer.sh
+++ setup_scripts/update-labtainer.sh
@@ -2,11 +2,7 @@
 wget https://my.nps.edu/documents/107523844/109121513/labtainer.tar/6fc80410-e87d-4e47-ae24-cbb60c7619fa -O labtainer.tar
 cd ..
 tar xf labtainer/labtainer.tar
-docker pull mfthomps/labtainer.base
-docker pull mfthomps/labtainer.network
-docker pull mfthomps/labtainer.firefox
-docker pull mfthomps/labtainer.centos
-docker pull mfthomps/labtainer.lamp
+labtainer/trunk/setup_scripts/pull-all.sh
 #
 # ensure labtainer paths in .bashrc
 #
