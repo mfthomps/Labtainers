1315a547d89009a5f8cf508d3dff43738d8a2d7d
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2017-11-15 21:10:01 +0000

    use tmp directory, remove debug
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@1066 ed162e55-3153-4343-a18d-e086fa355ed4

--- scripts/labtainer-student/bin/checkTars.py
+++ scripts/labtainer-student/bin/checkTars.py
@@ -10,6 +10,7 @@ domain and is not subject to copyright.
 '''
 import sys
 import os
+import shutil
 '''
 Look at _tar directories for the given labs/[lab]/[image] and
 create or update tar files to reflect recent changes.  Uses
@@ -17,17 +18,18 @@ an 'external-manifest' file to identify tars from other labs
 that should be part of this one.
 '''
 external = 'external-manifest'
+tmp_loc = '/tmp/check_tar'
 def expandManifest(full, tar_name):
-    print('expand for %s' % full)
+    #print('expand for %s' % full)
     mf = os.path.join(full, external)
     labdir = os.path.dirname(os.path.dirname(os.path.dirname(full)))
-    print('labdir is %s' % labdir)
+    #print('labdir is %s' % labdir)
     with open(mf) as fh:
         for line in fh:
             lab, image = line.strip().split(':')
             ref_tar = os.path.join(labdir, lab, image, os.path.basename(full), tar_name)
-            print('external ref is %s' % ref_tar)
-            cmd = 'tar xf %s' % ref_tar
+            #print('external ref is %s' % ref_tar)
+            cmd = 'tar xf %s -C %s' % (ref_tar, tmp_loc)
             os.system(cmd)
 
 def newest_file_in_tree(rootfolder):
@@ -42,48 +44,60 @@ image_name = sys.argv[2]
 tar_list = os.listdir(lab_dir)
 manifest_name = '%s-home_tar.list' % image_name
 manifest = os.path.join(os.path.dirname(lab_dir), 'config', manifest_name)
+try:
+    shutil.rmtree(tmp_loc)
+except:
+    pass
+os.mkdir(tmp_loc)
 for f in tar_list:
     full = os.path.join(lab_dir, f)
     if os.path.isdir(full) and f.endswith('_tar'):
         os.chdir(full)
         tmp_name = f[:-4]
         tar_name = tmp_name+'.tar'
-        print('check for %s' % tar_name)
+        #print('check for %s' % tar_name)
         if not os.path.isfile(tar_name):
             ''' no tar, make one '''
-            print('no tar %s, make one' % tar_name)
+            #print('no tar %s, make one' % tar_name)
             f_list = os.listdir('./')
             if len(f_list) == 0:
-                print('no files, make empty')
+                #print('no files, make empty')
                 ''' no files at all, create empty archive '''
                 cmd = 'tar cvf %s --files-from /dev/null' % tar_name
                 os.system(cmd)
-                print('did %s' % cmd)
+                #print('did %s' % cmd)
             else:
                 if external in f_list:
                     ''' external manifest, expand that '''
                     expandManifest(full, tar_name)
+                for cfile in f_list:
+                    if cfile != external:
+                        shutil.copytree(cfile, tmp_loc)
+                os.chdir(tmp_loc)
+                full_tar = os.path.join(full, tar_name)
                 if f == 'home_tar':
-                    cmd = 'tar czf %s --exclude=external-manifest `ls -A -1` > %s' % (tar_name, manifest)
+                    cmd = 'tar czf %s `ls -A -1` > %s' % (full_tar, manifest)
                 else:
-                    cmd = 'tar czf %s --exclude=external-manifest `ls -A -1`' % (tar_name)
+                    cmd = 'tar czf %s `ls -A -1`' % (full_tar)
                 os.system(cmd)
-                print('did %s' % cmd)
+                #print('did %s' % cmd)
         else:
             ''' is a tar file, should it be updated? '''
             os.chdir(full)
             newest = newest_file_in_tree('./') 
-            print('newest is %s' % newest)
+            #print('newest is %s' % newest)
             if not newest.endswith(tar_name):
                 os.remove(tar_name)
                 ''' something is newer, need to update tar '''
                 if os.path.isfile(os.path.join('./', external)):
                     expandManifest(full, tar_name)
+                os.chdir(tmp_loc)
+                full_tar = os.path.join(full, tar_name)
                 if f == 'home_tar':
-                    cmd = 'tar czf %s --exclude=external-manifest `ls -A -1` > %s' % (tar_name, manifest)
+                    cmd = 'tar czf %s `ls -A -1` > %s' % (full_tar, manifest)
                 else:
-                    cmd = 'tar czf %s --exclude=external-manifest `ls -A -1`' % (tar_name)
+                    cmd = 'tar czf %s `ls -A -1`' % (full_tar)
                 os.system(cmd)
-                print('did %s' % cmd)
+                #print('did %s' % cmd)
                  
                 
