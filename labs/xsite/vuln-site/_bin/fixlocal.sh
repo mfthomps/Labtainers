ea5abd8f882ff44ba39ae6b79dd4506d053627ba
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2017-11-17 16:17:26 +0000

    move sql into separate script and run in background -- though this saves no time
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@1090 ed162e55-3153-4343-a18d-e086fa355ed4

--- labs/xsite/vuln-site/_bin/fixlocal.sh
+++ labs/xsite/vuln-site/_bin/fixlocal.sh
@@ -7,16 +7,6 @@
 #  not not permit nopassword, then use:
 #  echo $1 | sudo -S the-command
 #
-while [ 1 ]; do
-    mysql -u root -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('seedubuntu')"
-    result=$?
-    if [[ $result == 0 ]]; then
-        break
-    else
-        echo no server, sleep
-        sleep 1
-    fi
-done
-mysql -u root -pseedubuntu -e "CREATE DATABASE if not exists myelgg; "
-mysql -u root -pseedubuntu myelgg < $HOME/myelgg.sql
-
+date
+$HOME/.local/bin/dosql.sh &
+date
