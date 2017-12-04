41cffec7153e9eb8185952b01edef38a6d3874b2
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2017-12-04 23:55:24 +0000

    use nping vice netwox
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@1153 ed162e55-3153-4343-a18d-e086fa355ed4

--- labs/tcpip/attacker/hexify.py
+++ labs/tcpip/attacker/hexify.py
@@ -5,4 +5,4 @@ if len(sys.argv) != 2:
     exit(0)
 s = sys.argv[1]
 print('The hex encoding of  %s is:\n%s' % (s, s.encode("hex")))
-print('Do not forget to add a "0d" if a newline is needed')
+print('Do not forget to add a "0d00" if a newline and null termination is needed')
