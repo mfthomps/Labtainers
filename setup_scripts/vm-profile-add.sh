9b7c8f97a773ff55a57c836b7e98569b1162d38c
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2017-12-15 01:34:00 +0000

    also modify gnome to shutdown on acpi power button
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@1198 ed162e55-3153-4343-a18d-e086fa355ed4

--- setup_scripts/vm-profile-add.sh
+++ setup_scripts/vm-profile-add.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+#
+# Modify the user profile to create a terminal on login that
+# starts in the labtainer workspace.  The profile will also, run the Labtainer
+# update script if the labtainer/.doupdate file exists.
+# This script also creates the .doupdate file and modifieds gnmoe
+# to shutdown when the virtual powerbutton is pressed.
+#
 cat >>~/.profile <<EOL
 gnome-terminal --geometry 120x31+150+300 --working-directory=/home/student/labtainer/labtainer-student -e "bash -c \"/bin/cat README; exec bash\"" &
 if [[ -f /home/student/labtainer/.doupdate ]]; then
@@ -5,3 +13,4 @@ if [[ -f /home/student/labtainer/.doupdate ]]; then
 fi
 EOL
 touch /home/student/labtainer/.doupdate 
+gsettings set org.gnome.settings-daemon.plugins.power button-power 'shutdown'
