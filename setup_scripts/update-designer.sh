4ff73b1bd5262c0b030f864f7dcc83404859c489
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2017-12-18 22:45:13 +0000

    script to update to the designer distribution
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@1211 ed162e55-3153-4343-a18d-e086fa355ed4

---
+++ setup_scripts/update-designer.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget https://my.nps.edu/documents/107523844/109121513/labtainer-developer.tar/f377285e-23b5-4cd4-a578-c879b0200fff -O labtainer-developer.tar
+cd ..
+tar xf labtainer/labtainer-developer.tar
