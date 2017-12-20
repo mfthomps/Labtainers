d48b8831797615128f43091471bb57f15a1549d5
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2017-12-20 00:19:16 +0000

    seed remote dns cache poisoning lab
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@1219 ed162e55-3153-4343-a18d-e086fa355ed4

---
+++ labs/remote-dns/attacker/_bin/fixlocal.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+#  Script will be run after parameterization has completed, e.g., 
+#  use this to compile source code that has been parameterized.
+#  The container user password will be passed as the first argument.
+#  Thus, if this script is to use sudo and the sudoers for the lab
+#  not not permit nopassword, then use:
+#  echo $1 | sudo -S the-command
+#
+sudo tee -a /etc/bind/named.conf.local << EOL
+zone "example.com" {
+type master;
+file "/etc/bind/example.com.db";
+};
+EOL
+echo "nameserver 192.168.0.10" | sudo tee /etc/resolv.conf
+
