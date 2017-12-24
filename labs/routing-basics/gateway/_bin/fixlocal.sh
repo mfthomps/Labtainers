4df1e30f14062b87cc24001dcb5c509e0439032c
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2017-12-24 19:19:37 +0000

    remove unused routes, adjust iptables rather than replace
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@1227 ed162e55-3153-4343-a18d-e086fa355ed4

--- labs/routing-basics/gateway/_bin/fixlocal.sh
+++ labs/routing-basics/gateway/_bin/fixlocal.sh
@@ -7,3 +7,19 @@
 #  not not permit nopassword, then use:
 #  echo $1 | sudo -S the-command
 #
+#
+# no predicting docker network assignments, use ifconfig fu
+#
+# Set up NAT on gateway
+#
+lan1=$(ifconfig | grep -B1 "inet addr:192.168.1.10" | awk '$1!="inet" && $1!="--" {print $1}')
+lan2=$(ifconfig | grep -B1 "inet addr:192.168.2.10" | awk '$1!="inet" && $1!="--" {print $1}')
+wan=$(ifconfig | grep -B1 "inet addr:192.170.0.10" | awk '$1!="inet" && $1!="--" {print $1}')
+#iptables --flush
+#iptables -t nat --flush
+#iptables --delete-chain
+#iptables -t nat --delete-chain
+sudo iptables --table nat -I POSTROUTING 1 --out-interface $wan -j MASQUERADE
+sudo iptables --append FORWARD --in-interface $lan1 -j ACCEPT
+sudo iptables --append FORWARD --in-interface $lan2 -j ACCEPT
+
