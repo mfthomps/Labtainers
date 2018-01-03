406eeafa8513ed79af11cb361c92f9759e1eb628
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-01-03 21:43:41 +0000

    add third ws, ISP gateway that is hidden
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@1239 ed162e55-3153-4343-a18d-e086fa355ed4

--- labs/routing-basics/ws3/_bin/fixlocal.sh
+++ labs/routing-basics/ws3/_bin/fixlocal.sh
@@ -7,8 +7,6 @@
 #  not not permit nopassword, then use:
 #  echo $1 | sudo -S the-command
 #
-#
-echo "nameserver 203.0.113.1" | sudo tee /etc/resolv.conf
-# remove the dns route so as to not confuse student
+echo "" | sudo tee /etc/resolv.conf
 sudo route del -host 172.17.0.1
-
+sudo route delete default gw 192.168.2.101
