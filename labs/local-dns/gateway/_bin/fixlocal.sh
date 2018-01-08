6a807384d586fd08370f28d4db5fc5e1973a682c
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-01-08 16:27:57 +0000

    put NAT setup in fixlocal vice rc.local, 3 attacher terminals
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@1251 ed162e55-3153-4343-a18d-e086fa355ed4

--- labs/local-dns/gateway/_bin/fixlocal.sh
+++ labs/local-dns/gateway/_bin/fixlocal.sh
@@ -7,3 +7,13 @@
 #  not not permit nopassword, then use:
 #  echo $1 | sudo -S the-command
 #
+#
+# no predicting docker network assignments, use ifconfig fu
+#
+# Set up NAT on gateway
+#
+lan1=$(ifconfig | grep -B1 "inet addr:192.168.0.1" | awk '$1!="inet" && $1!="--" {print $1}')
+wan=$(ifconfig | grep -B1 "inet addr:10.10.0.1" | awk '$1!="inet" && $1!="--" {print $1}')
+sudo iptables --table nat -I POSTROUTING 1 --out-interface $wan -j MASQUERADE
+sudo iptables --append FORWARD --in-interface $lan1 -j ACCEPT
+
