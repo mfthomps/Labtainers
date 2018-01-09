b4901a10a8cc31af4fe2507991168ae633fd91fd
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-01-09 22:07:17 +0000

    seed lab setuid environment variable lab
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@1266 ed162e55-3153-4343-a18d-e086fa355ed4

---
+++ labs/setuid-env/setuid-env/execve.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+extern char ** environ;
+int main()
+{
+   char * argv[2];
+   argv[0] = "/usr/bin/env";
+   argv[1] = NULL;
+   execve("/usr/bin/env", argv, NULL);
+   return 0 ;
+}
