cdd0f60af0edb4a3faa4c94cd3f752434ec94218
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-01-09 01:32:10 +0000

    fix routing, move attacker outside domain
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@1258 ed162e55-3153-4343-a18d-e086fa355ed4

--- labs/remote-dns/gateway/_bin/fixlocal.sh
+++ labs/remote-dns/gateway/_bin/fixlocal.sh
@@ -7,12 +7,18 @@
 #  not not permit nopassword, then use:
 #  echo $1 | sudo -S the-command
 #
+echo "nameserver 203.0.113.1" | sudo tee /etc/resolv.conf
+# remove the dns route so as to not confuse student
+sudo route del -host 172.17.0.1
+
 # no predicting docker network assignments, use ifconfig fu
 #
 # Set up NAT on gateway
 #
 lan1=$(ifconfig | grep -B1 "inet addr:192.168.0.1" | awk '$1!="inet" && $1!="--" {print $1}')
-wan=$(ifconfig | grep -B1 "inet addr:10.10.0.1" | awk '$1!="inet" && $1!="--" {print $1}')
+wan=$(ifconfig | grep -B1 "inet addr:203.0.113.1" | awk '$1!="inet" && $1!="--" {print $1}')
 sudo iptables --table nat -I POSTROUTING 1 --out-interface $wan -j MASQUERADE
 sudo iptables --append FORWARD --in-interface $lan1 -j ACCEPT
-
+sudo iptables -t nat -A PREROUTING -i $wan -p udp --dport 53 -j DNAT --to-destination 192.168.0.10
+sudo iptables -t nat -A POSTROUTING -o $lan1 -p udp --dport 53 -d 192.168.0.10 -j SNAT --to-source 192.168.0.1
+sudo /etc/init.d/dnsmasq restart
