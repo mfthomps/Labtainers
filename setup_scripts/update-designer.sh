57767a167f36441301c6155b68d5838c48cee491
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-01-10 18:37:34 +0000

    do not make copy of update scripts, use links and logic to know where we execute from
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@1272 ed162e55-3153-4343-a18d-e086fa355ed4

--- setup_scripts/update-designer.sh
+++ setup_scripts/update-designer.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
+#
+# figure out where we are executing from and go to the labtainer directory
+#
+here=`pwd`
+if [[ $here == */labtainer ]]; then
+   echo is at top >> /dev/null
+elif [[ $here == */labtainer-student ]]; then
+   #echo is in student
+   real=`realpath ./`
+   cd $real
+   cd ../../..
+elif [[ $here == */setup_scripts ]]; then
+   cd ../../
+else
+   echo "Please run this script from the labtainer or labtainer-student directory"
+   exit
+fi
+rm -f update-designer.sh
+ln -s trunk/setup_scripts/update-designer.sh
+full=`realpath trunk/setup_scripts/update-designer.sh`
+ln -sf $full trunk/scripts/labtainer-student/bin/update-designer.sh
 wget https://my.nps.edu/documents/107523844/109121513/labtainer-developer.tar/f377285e-23b5-4cd4-a578-c879b0200fff -O labtainer-developer.tar
 cd ..
 tar xf labtainer/labtainer-developer.tar
--- setup_scripts/update-labtainer.sh
+++ setup_scripts/update-labtainer.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
+#
+# figure out where we are executing from and go to the labtainer directory
+#
+here=`pwd`
+if [[ $here == */labtainer ]]; then
+   echo is at top >> /dev/null
+elif [[ $here == */labtainer-student ]]; then
+   #echo is in student
+   real=`realpath ./`
+   cd $real
+   cd ../../..
+elif [[ $here == */setup_scripts ]]; then
+   cd ../../
+else
+   echo "Please run this script from the labtainer or labtainer-student directory"
+   exit
+fi
+rm -f update-labtainer.sh
+ln -s trunk/setup_scripts/update-labtainer.sh
+full=`realpath trunk/setup_scripts/update-labtainer.sh`
+ln -sf $full trunk/scripts/labtainer-student/bin/update-labtainer.sh
+
 wget https://my.nps.edu/documents/107523844/109121513/labtainer.tar/6fc80410-e87d-4e47-ae24-cbb60c7619fa -O labtainer.tar
 cd ..
 tar xf labtainer/labtainer.tar
