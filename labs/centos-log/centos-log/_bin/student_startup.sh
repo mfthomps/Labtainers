37fe32e3c80d83b6775e871a3864a13d5d985b12
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-01-17 19:01:28 +0000

    hack around /var/run/no_login bugs
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@1313 ed162e55-3153-4343-a18d-e086fa355ed4

--- labs/centos-log/centos-log/_bin/student_startup.sh
+++ labs/centos-log/centos-log/_bin/student_startup.sh
@@ -6,6 +6,7 @@ id | grep root >>/dev/null
 result=$?
 if [[ $result -eq 0 ]]; then
     #echo "is root"
+    rm -f /var/run/nologin
     while [ 1 ]; do
         trap login SIGINT
         /sbin/login
