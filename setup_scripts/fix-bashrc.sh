5be2bdf98a83b553c296743dc4c9ee7856008291
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-01-18 18:33:25 +0000

    add designer/bin to path
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@1328 ed162e55-3153-4343-a18d-e086fa355ed4

--- setup_scripts/fix-bashrc.sh
+++ setup_scripts/fix-bashrc.sh
@@ -9,3 +9,12 @@ if [[ result -ne 0 ]];then
    fi
 EOT
 fi
+grep ":scripts/designer/bin:" $target | grep PATH >>/dev/null
+result=$?
+if [[ result -ne 0 ]];then
+   cat <<EOT >>$target
+   if [[ ":\$PATH:" != *":scripts/designer/bin:"* ]]; then 
+       export PATH="\${PATH}:$here/trunk/scripts/designer/bin"
+   fi
+EOT
+fi
