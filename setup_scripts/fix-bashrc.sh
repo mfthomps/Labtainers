7fc429e5b6afa3843da6ce1e9be2400f99ac3da6
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-01-21 16:07:12 +0000

    add LABTAINERS_DIR and path to designer
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@1346 ed162e55-3153-4343-a18d-e086fa355ed4

--- setup_scripts/fix-bashrc.sh
+++ setup_scripts/fix-bashrc.sh
@@ -12,9 +12,11 @@ fi
 grep ":scripts/designer/bin:" $target | grep PATH >>/dev/null
 result=$?
 if [[ result -ne 0 ]];then
+   here=`realpath ../`
    cat <<EOT >>$target
    if [[ ":\$PATH:" != *":scripts/designer/bin:"* ]]; then 
-       export PATH="\${PATH}:$here/trunk/scripts/designer/bin"
+       export PATH="\${PATH}:$here/scripts/designer/bin"
+       export LABTAINER_DIR=$here
    fi
 EOT
 fi
