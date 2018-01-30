ac605a66e0ad31f95d3013c78baf80622e1ccc18
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-01-30 23:41:52 +0000

    Modified httpserver.sh for attacker-site: change DIR to poit to /home/attacker instead of /home/ubuntu directory
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@1392 ed162e55-3153-4343-a18d-e086fa355ed4

--- labs/xforge/attacker-site/_system/etc/init.d/httpserver.sh
+++ labs/xforge/attacker-site/_system/etc/init.d/httpserver.sh
@@ -11,7 +11,7 @@
 ### END INIT INFO
 
 # Change the next 3 lines to suit where you install your script and what you want to call it
-DIR=/home/ubuntu
+DIR=/home/attacker
 DAEMON=$DIR/MyHTTPServer.py
 DAEMON_NAME=httpserver
 
