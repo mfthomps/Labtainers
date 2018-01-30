0d2e38d1bfea8b6e975bd79e372eb0caaa809561
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-01-30 18:47:26 +0000

    feedback on use of LABTAINER_TESTING
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@1391 ed162e55-3153-4343-a18d-e086fa355ed4

--- setup_scripts/update-labtainer.sh
+++ setup_scripts/update-labtainer.sh
@@ -21,8 +21,10 @@ ln -s trunk/setup_scripts/update-labtainer.sh
 full=`realpath trunk/setup_scripts/update-labtainer.sh`
 ln -sf $full trunk/scripts/labtainer-student/bin/update-labtainer.sh
 
-if [[ -z "$LABTAINER_TESTING" ]]; then
+if [[ "$LABTAINER_TESTING" != TRUE ]]; then
     wget https://my.nps.edu/documents/107523844/109121513/labtainer.tar/6fc80410-e87d-4e47-ae24-cbb60c7619fa -O labtainer.tar
+else
+    echo "USING LOCAL TAR, NOT PULLING FROM WEB"
 fi
 cd ..
 tar xf labtainer/labtainer.tar
