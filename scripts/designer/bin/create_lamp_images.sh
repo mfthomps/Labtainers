f60a2ce7392afa8eda0761c56792cd3663326dbb
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-02-06 17:59:55 +0000

    more user warnings for creation of baseline images
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@1424 ed162e55-3153-4343-a18d-e086fa355ed4

--- scripts/designer/bin/create_lamp_images.sh
+++ scripts/designer/bin/create_lamp_images.sh
@@ -8,14 +8,21 @@ created  by United States Government employees, pursuant to Title 17
 United States Code Section 105.   This software is in the public 
 domain and is not subject to copyright. 
 END
-read -p "This buid will pull from the docker hub.  Have you published the centos base?"
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
-    echo exiting
-    exit
+source ./set_reg.sh
+if [[ "$1" != -f ]]; then
+   echo "This will build the labtainer lamp image.  "
+   echo "Confirm that the labtainer.centos has been published."
+   echo "registry is $LABTAINER_REGISTRY"
+   read -p "Continue? (y/n)"
+   if [[ ! $REPLY =~ ^[Yy]$ ]]
+   then
+       echo exiting
+       exit
+   fi
+else
+   echo "registry is $LABTAINER_REGISTRY"
 fi
 here=`pwd`
-source ./set_reg.sh
 cd ../
 docker build --build-arg registry=$LABTAINER_REGISTRY -f base_dockerfiles/Dockerfile.labtainer.lamp -t labtainer.lamp:latest .
 cd $here
