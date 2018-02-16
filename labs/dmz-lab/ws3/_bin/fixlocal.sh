6404669a23b406cfd96a842ddccf39d0a8908faf
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-02-16 01:50:39 +0000

    first cut at a DYI DMZ lab
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@1459 ed162e55-3153-4343-a18d-e086fa355ed4

---
+++ labs/dmz-lab/ws3/_bin/fixlocal.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#  Script will be run after parameterization has completed, e.g., 
+#  use this to compile source code that has been parameterized.
+#  The container user password will be passed as the first argument.
+#  Thus, if this script is to use sudo and the sudoers for the lab
+#  not not permit nopassword, then use:
+#  echo $1 | sudo -S the-command
+#
+echo "" | sudo tee /etc/resolv.conf
+echo "nameserver 198.18.1.3" | sudo tee /etc/resolv.conf
+sudo route del -host 172.17.0.1
+sudo route del default
