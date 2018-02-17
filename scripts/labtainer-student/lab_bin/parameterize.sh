18fc062ab03c4224ed48fe17ee46c17b86759776
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-02-17 01:26:14 +0000

    run hook as sudo
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@1462 ed162e55-3153-4343-a18d-e086fa355ed4

--- scripts/labtainer-student/lab_bin/parameterize.sh
+++ scripts/labtainer-student/lab_bin/parameterize.sh
@@ -90,9 +90,9 @@ if [ -f /var/tmp/home.tar ]; then
    echo "expanded /var/tmp/home.tar to $HOME" >>/tmp/parameterize.out 2>&1
 fi
 
-$HOME/.local/bin/hookBash.sh 2>>/tmp/hookBash.output
+echo $CONTAINER_PASSWORD | sudo -S $HOME/.local/bin/hookBash.sh 2>>/tmp/hookBash.output
 rmdir $LOCKDIR
 # Added a permanent 'did_param' lock directory
 PERMLOCKDIR=/var/labtainer/did_param
-sudo mkdir -p "$PERMLOCKDIR" >/dev/null 2>&1
+echo $CONTAINER_PASSWORD | sudo -S mkdir -p "$PERMLOCKDIR" >/dev/null 2>&1
 
