29a44060f8d513d3ba069e1902ff162473bfd5c9
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-02-19 20:38:51 +0000

    change checklocal to precheck, reflecting script runs prior to command
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@1464 ed162e55-3153-4343-a18d-e086fa355ed4

--- labs/routing-basics2/isp/_bin/fixlocal.sh
+++ labs/routing-basics2/isp/_bin/fixlocal.sh
@@ -7,8 +7,8 @@
 #  not not permit nopassword, then use:
 #  echo $1 | sudo -S the-command
 #
-lan1=$(ifconfig | grep -B1 "inet addr:203.113.0.1" | awk '$1!="inet" && $1!="--" {print $1}')
-wan=$(ifconfig | grep -B1 "inet addr:10.10.0.1" | awk '$1!="inet" && $1!="--" {print $1}')
-sudo iptables --table nat -I POSTROUTING 1 --out-interface $wan -j MASQUERADE
-sudo iptables --append FORWARD --in-interface $lan1 -j ACCEPT
+#lan1=$(ifconfig | grep -B1 "inet addr:203.113.0.1" | awk '$1!="inet" && $1!="--" {print $1}')
+#wan=$(ifconfig | grep -B1 "inet addr:10.10.0.1" | awk '$1!="inet" && $1!="--" {print $1}')
+#sudo iptables --table nat -I POSTROUTING 1 --out-interface $wan -j MASQUERADE
+#sudo iptables --append FORWARD --in-interface $lan1 -j ACCEPT
 
