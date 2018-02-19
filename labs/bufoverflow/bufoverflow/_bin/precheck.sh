446eb90d2322219c8cdf0adc1269d0db6fa0db0e
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-02-19 20:38:51 +0000

    change checklocal to precheck, reflecting script runs prior to command
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@1464 ed162e55-3153-4343-a18d-e086fa355ed4

--- labs/bufoverflow/bufoverflow/_bin/precheck.sh
+++ labs/bufoverflow/bufoverflow/_bin/precheck.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-# checklocal.sh
+# precheck.sh
 # Description:
 #     This file should contain checks for local settings (such as sysctl)
 #     specific for each lab.  The resulting output will go into the
-#     checklocal.stdout.timestamp file
+#     precheck.stdout.timestamp file
 #
-
+# Get status of kernel.randomize_va_space
+sudo sysctl -a | grep kernel.randomize_va_space
