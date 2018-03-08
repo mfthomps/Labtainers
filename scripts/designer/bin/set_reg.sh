32e24ad2d78236072e7abac3725a4cdfc99484e8
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-03-08 18:20:31 +0000

    ongoing automation of base image creation
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@1514 ed162e55-3153-4343-a18d-e086fa355ed4

--- scripts/designer/bin/set_reg.sh
+++ scripts/designer/bin/set_reg.sh
@@ -1,6 +1,10 @@
-if [[ "${TEST_REGISTRY}" != YES ]]; then
-    export LABTAINER_REGISTRY="mfthomps"
-else
+if [ "$1" == -t ] || [ "$2" == -t ]; then
     export LABTAINER_REGISTRY="testregistry:5000"
+else
+    if [[ "${TEST_REGISTRY}" != YES ]]; then
+        export LABTAINER_REGISTRY="mfthomps"
+    else
+        export LABTAINER_REGISTRY="testregistry:5000"
+    fi
 fi
 
