05dc812f45ca8a421938c753c41d4697cf8aa2aa
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-03-08 18:20:31 +0000

    ongoing automation of base image creation
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@1514 ed162e55-3153-4343-a18d-e086fa355ed4

--- scripts/designer/bin/create_all.sh
+++ scripts/designer/bin/create_all.sh
@@ -1,22 +1,23 @@
-export TEST_REGISTRY=NO
+TEST_ARG=-t
+echo "Using test registry"
 ./create_base_image.sh
-./publish_image.sh labtainer.base
+./publish_image.sh labtainer.base -f $TEST_ARG
 
-./create_network_image.sh
-./publish_image.sh labtainer.network
+./create_network_image.sh -f $TEST_ARG
+./publish_image.sh labtainer.network $TEST_ARG
 
-./create_wireshark_image.sh
-./publish_image.sh labtainer.wireshark
+./create_wireshark_image.sh -f $TEST_ARG
+./publish_image.sh labtainer.wireshark $TEST_ARG
 
-./create_firefox_image.sh
-./publish_image.sh labtainer.firefox
+./create_firefox_image.sh -f $TEST_ARG
+./publish_image.sh labtainer.firefox $TEST_ARG
 
-./create_java_image.sh
-./publish_image.sh labtainer.java
+./create_java_image.sh -f $TEST_ARG
+./publish_image.sh labtainer.java $TEST_ARG
 
-#./create_centos_image.sh
-#./publish_image.sh labtainer.centos
+./create_centos_image.sh $TEST_ARG
+./publish_image.sh labtainer.centos $TEST_ARG
 
-#./create_lamp_images.sh
-#./publish_image.sh labtainer.lamp
+./create_lamp_images.sh -f $TEST_ARG
+./publish_image.sh labtainer.lamp $TEST_ARG
 
