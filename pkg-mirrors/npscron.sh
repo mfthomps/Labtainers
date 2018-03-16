922ea5de1e01deeb4ce82bb1fd26c783d239965d
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-03-16 23:56:47 +0000

    Added UpdatePackage.sh and modified npscron.sh to use it
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@1555 ed162e55-3153-4343-a18d-e086fa355ed4

--- pkg-mirrors/npscron.sh
+++ pkg-mirrors/npscron.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 echo "NPS cron jobs"
-NPS_PKG_ADDITIONS="/etc/nps-apt-additions"
 
 # Update CentOS repo
 sudo /usr/local/bin/UpdateCentosRepo.sh
@@ -12,10 +11,7 @@ sudo /usr/local/bin/apt-mirror
 # Update Ubuntu dists hierarchy - include universe and multiverse
 sudo /usr/local/bin/CheckHierarchy.sh
 
-while read pkgaddition;
-do
-    echo "Adding package $pkgaddition"
-    sudo /usr/local/bin/AddPackage.py $pkgaddition
-done < $NPS_PKG_ADDITIONS
+# Update Packages
+sudo /usr/local/bin/UpdatePackage.sh
 
 exit 0
