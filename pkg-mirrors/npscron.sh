b88b359bda80e31e9a656d80f4b17cc9088be884
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-03-16 23:24:01 +0000

    Modified npscron.sh - use /usr/local/bin instead. Added dnsmasq to nps-apt-additions.
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@1553 ed162e55-3153-4343-a18d-e086fa355ed4

--- pkg-mirrors/npscron.sh
+++ pkg-mirrors/npscron.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 echo "NPS cron jobs"
-NPS_PKG_ADDITIONS="/home/jkhosali/nps-apt-additions"
+NPS_PKG_ADDITIONS="/etc/nps-apt-additions"
 
 # Update CentOS repo
-sudo /home/jkhosali/UpdateCentosRepo.sh
+sudo /usr/local/bin/UpdateCentosRepo.sh
 
 # Update Ubuntu repo - only main and restricted
 sudo /usr/local/bin/apt-mirror
 
 # Update Ubuntu dists hierarchy - include universe and multiverse
-sudo /home/jkhosali/CheckHierarchy.sh
+sudo /usr/local/bin/CheckHierarchy.sh
 
 while read pkgaddition;
 do
     echo "Adding package $pkgaddition"
-    sudo /home/jkhosali/AddPackage.py $pkgaddition
+    sudo /usr/local/bin/AddPackage.py $pkgaddition
 done < $NPS_PKG_ADDITIONS
 
 exit 0
