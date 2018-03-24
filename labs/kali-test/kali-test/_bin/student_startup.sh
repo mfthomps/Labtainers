90ff8228e53b9f95f675852cfc3e39f6a223347c
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-03-24 00:58:59 +0000

    add dhcp, begin rework to for new multiuser scheme
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@1593 ed162e55-3153-4343-a18d-e086fa355ed4

--- labs/kali-test/kali-test/_bin/student_startup.sh
+++ labs/kali-test/kali-test/_bin/student_startup.sh
@@ -2,14 +2,14 @@
 #
 # If root, loop forever in login. 
 #
-id | grep root >>/dev/null
-result=$?
-if [[ $result -eq 0 ]]; then
-    #echo "is root"
-    rm -f /var/run/nologin
-    while [ 1 ]; do
-        trap login SIGINT
-        /bin/login
-    done
-fi
+#id | grep root >>/dev/null
+#result=$?
+#if [[ $result -eq 0 ]]; then
+#    #echo "is root"
+#    rm -f /var/run/nologin
+#    while [ 1 ]; do
+#        trap login SIGINT
+#        /bin/login
+#    done
+#fi
 
