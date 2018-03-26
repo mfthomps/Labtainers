ff80e79491ecf15e79eabafb759c45b706e720cb
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-03-26 17:51:32 +0000

    missed class for tracking clone counts
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@1596 ed162e55-3153-4343-a18d-e086fa355ed4

---
+++ scripts/labtainer-student/bin/CurrentLab.py
@@ -0,0 +1,24 @@
+import json
+import os
+clab_file = '.tmp/current.json'
+class CurrentLab():
+    def __init__(self):
+        self.data = {}
+        self.load()
+    def load(self):
+        if os.path.isfile(clab_file):
+            with open(clab_file) as fh: 
+                self.data = json.load(fh)
+    def save(self):
+        with open(clab_file, 'w') as fh:
+            json.dump(self.data, fh)
+    def add(self, key, value):
+        self.data[key] = value
+    def get(self, key):
+        if key in self.data:
+            return self.data[key]
+        else:
+            return None
+    def clear(self):
+        self.data = {}
+        self.save()
