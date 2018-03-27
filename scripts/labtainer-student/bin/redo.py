3eda62f5a54d7aac2414c56ac3b622103a81f7cc
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-03-27 15:58:44 +0000

    clean up parameter names and usage
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@1609 ed162e55-3153-4343-a18d-e086fa355ed4

--- scripts/labtainer-student/bin/redo.py
+++ scripts/labtainer-student/bin/redo.py
@@ -29,40 +29,18 @@ def main():
     dir_path = dir_path[:dir_path.index("scripts/labtainer-student")]	
     path = dir_path + "labs/"
     dirs = os.listdir(path)
-    parser = argparse.ArgumentParser(description='Start a Labtainers lab.  Provide no arguments see a list of labs.')
+    parser = argparse.ArgumentParser(description='Restart a fresh instance of aLabtainers lab.  Previous work on this lab will be lost. The lab will be stopped if it is running.')
     parser.add_argument('labname', help='The lab to run')
-    parser.add_argument('-c', '--run_container', action='store', help='run just this container')
     parser.add_argument('-q', '--quiet', action='store_true', help='Do not prompt for email, use previoulsy supplied email.')
     parser.add_argument('-s', '--servers', action='store_true', help='Intended for distributed Labtainers, start the containers that are not clients.')
     parser.add_argument('-w', '--workstation', action='store_true', help='Intended for distributed Labtainers, start the client workstation.')
-    parser.add_argument('-n', '--clone_count', action='store', help='Number of clones of client containers to create, itended for multi-user labs')
-    try:
-        args = parser.parse_args()
-    except SystemExit:
-        num_args = len(sys.argv)
-        if num_args < 2: 
-            description = ''
-            description += 'Start a Labtainers lab\n'
-    	    description+="List of available labs:\n\n"
-	    for loc in sorted(dirs):
-                description = description+'\n  '+loc
-		aboutFile = path + loc + "/config/about.txt"
-		if(os.path.isfile(aboutFile)):
-                    description += ' - '
-		    with open(aboutFile) as fh:
-		        for line in fh:
-                            description += line
-                else:
-                    description += "\n"
-            #sys.stderr.write(description)
-            pydoc.pager(description)
-        sys.exit(1)
-    
+    parser.add_argument('-n', '--client_count', action='store', help='Number of clones of client components to create, itended for multi-user labs')
+    parser.add_argument('-o', '--only_container', action='store', help='run only the named container')
+    args = parser.parse_args()
     labname = args.labname
     if labname not in dirs:
         sys.stderr.write("ERROR: Lab named %s was not found!\n" % labname)
         sys.exit(1)
-
     
     labutils.logger = LabtainerLogging.LabtainerLogging("labtainer.log", labname, "../../config/labtainer.config")
     labutils.logger.INFO("Begin logging start.py for %s lab" % labname)
@@ -80,14 +58,14 @@ def main():
         distributed = 'server' 
     elif args.workstation:
         distributed = 'client'
-    if distributed is not None and args.clone_count is not None:
-        print('Cannot specify --server or --client if a --clone_count is provided')
+    if distributed is not None and args.client_count is not None:
+        print('Cannot specify --server or --client if a --client_count is provided')
         exit(1)
     labutils.RedoLab(lab_path, "student", quiet_start=args.quiet, 
-                     run_container=args.run_container, servers=distributed, clone_count=args.clone_count)
+                     run_container=args.only_container, servers=distributed, clone_count=args.client_count)
     current_lab = CurrentLab.CurrentLab()
     current_lab.add('lab_name', args.labname)
-    current_lab.add('clone_count', args.clone_count)
+    current_lab.add('clone_count', args.client_count)
     current_lab.add('servers', distributed)
     current_lab.save()
 
