f0bcafcdadc9b0df44cd2b45d54fecafcccfcdc2
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-03-28 23:37:29 +0000

    use alternate file system, fix manual
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@1614 ed162e55-3153-4343-a18d-e086fa355ed4

--- labs/backups/backups/_bin/fixlocal.sh
+++ labs/backups/backups/_bin/fixlocal.sh
@@ -3,17 +3,17 @@
 #  Script will be run after parameterization has completed, e.g., 
 #  use this to compile source code that has been parameterized.
 #
+#sudo cp /usr/bin/who /usr/bin/cheese
 img_dir=/vfs
 img_fid=/vfs/myfs.img
 sudo mkdir $img_dir
-sudo dd if=/dev/zero of=$img_fid bs=1k count=60k
-#sudo mkfs.ext2 -F $img_fid
+sudo dd if=/dev/zero of=$img_fid bs=1k count=100k
 sudo mkfs -t ext2 -F $img_fid
 sudo mkdir -p /lab_mnt
 sudo mount -o loop $img_fid /lab_mnt
 sudo chown student mnt
 sudo mkdir /lab_mnt/usr
 sudo cp -R /usr/bin /lab_mnt/usr/
-cp /lab_mnt/usr/bin/who /lab_mnt/sur/bin/cheese
-sudo umount mnt
+sudo cp /lab_mnt/usr/bin/who /lab_mnt/usr/bin/cheese
+#sudo umount /lab_mnt
 
