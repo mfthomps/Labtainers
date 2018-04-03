e586c11957f211075d3a2215de520481e5babbf7
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-04-03 18:16:33 +0000

    move test utilities to testsets/bin
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@1654 ed162e55-3153-4343-a18d-e086fa355ed4

--- scripts/designer/bin/create_all.sh
+++ scripts/designer/bin/create_all.sh
@@ -1,7 +1,7 @@
-#TEST_ARG=-t
-#echo "Using test registry"
-docker login
-export DOCKER_LOGIN=YES
+TEST_ARG=-t
+echo "Using test registry"
+#docker login
+#export DOCKER_LOGIN=YES
 ./create_base_image.sh
 ./publish_image.sh labtainer.base $TEST_ARG
 
@@ -17,9 +17,9 @@ export DOCKER_LOGIN=YES
 ./create_java_image.sh -f $TEST_ARG
 ./publish_image.sh labtainer.java $TEST_ARG
 
-./create_centos_image.sh $TEST_ARG
-./publish_image.sh labtainer.centos $TEST_ARG
-
-./create_lamp_images.sh -f $TEST_ARG
-./publish_image.sh labtainer.lamp $TEST_ARG
+#./create_centos_image.sh $TEST_ARG
+#./publish_image.sh labtainer.centos $TEST_ARG
+#
+#./create_lamp_images.sh -f $TEST_ARG
+#./publish_image.sh labtainer.lamp $TEST_ARG
 
--- setup_scripts/removelab.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cont_list=$(docker ps -a | grep $1 | awk '{print $1}')
-echo $cont_list
-docker rm $cont_list
-image_list=$(docker images | grep $1 | awk '{print $1}')
-docker rmi -f $image_list
