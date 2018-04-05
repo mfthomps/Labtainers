7dcf6216033710329253c51624b61bd4cb498078
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-04-05 21:56:19 +0000

    next round of testing of testing
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@1669 ed162e55-3153-4343-a18d-e086fa355ed4

--- setup_scripts/full-smoke-test.sh
+++ setup_scripts/full-smoke-test.sh
@@ -1,6 +1,10 @@
+#!/bin/bash
 # Assume running from setup_scripts/
 #Clear out docker.
-./destroy-docker.sh
+now=`date +"%s"`
+exec > /media/sf_SEED/smokelogs/log-$now.log
+exec 2>&1
+./destroy-docker.sh -f
 
 # Update baseline and framework
 ./update-labtainer.sh -t
