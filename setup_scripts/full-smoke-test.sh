1ba5dfaafcf9c0a626e28b74b2d05de91933e2c6
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-04-06 16:23:02 +0000

    next iteration testing of testing
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@1671 ed162e55-3153-4343-a18d-e086fa355ed4

--- setup_scripts/full-smoke-test.sh
+++ setup_scripts/full-smoke-test.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 # Assume running from setup_scripts/
-#Clear out docker.
+#
+#  Runs as bash -c argument to gnome-terminal.  unable to get it to inherit bashrc defined env.
+#
+export TEST_REGISTRY=TRUE
+export PATH="${PATH}:./bin:$HOME/labtainer/trunk/scripts/designer/bin:$HOME/labtainer/testsets/bin"
+
 now=`date +"%s"`
 exec > /media/sf_SEED/smokelogs/log-$now.log
 exec 2>&1
+
+#Clear out docker.
 ./destroy-docker.sh -f
 
 # Update baseline and framework
@@ -12,5 +19,6 @@ exec 2>&1
 # Update test sets
 ./update-testsets.sh
 cd ../scripts/labtainer-student
+echo "start smoke test"
 smoketest.py
 
