713d9ea7f93ac35df7438497846c093535ffde3b
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-04-09 21:32:06 +0000

    clean up, bring in line with centos-log, but ubuntu-specific
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@1682 ed162e55-3153-4343-a18d-e086fa355ed4

--- labs/sys-log/sys-log/_bin/fixlocal.sh
+++ labs/sys-log/sys-log/_bin/fixlocal.sh
@@ -4,4 +4,5 @@
 #  use this to compile source code that has been parameterized.
 #
 # avoid error message in syslog
+echo $1 | sudo -S touch /dev/xconsole
 echo $1 | sudo -S chown syslog:adm /dev/xconsole
