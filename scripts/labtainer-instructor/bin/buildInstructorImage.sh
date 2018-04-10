86817e76f38c77b1fdffa91b29fade54b7662475
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-04-10 18:17:09 +0000

    copy instr_config at run time
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@1684 ed162e55-3153-4343-a18d-e086fa355ed4

--- scripts/labtainer-instructor/bin/buildInstructorImage.sh
+++ scripts/labtainer-instructor/bin/buildInstructorImage.sh
@@ -80,9 +80,6 @@ else
     rm -fr $TMP_DIR/_system
     rm -fr $TMP_DIR/home_tar
     rm -fr $TMP_DIR/sys_tar
-    cp $LAB_DIR/instr_config/* $TMP_DIR/.local/instr_config/ 2>>/dev/null
-    cp $LAB_DIR/config/* $TMP_DIR/.local/config/ 2>>/dev/null
-    cp config/* $TMP_DIR/.local/instr_config/ 2>>/dev/null
     if [ -d $LABIMAGE_DIR/_system ]; then
         cd $LABIMAGE_DIR/_system
         tar --atime-preserve -zcvf $SYS_TAR . > $TMP_DIR/.local/sys_manifest.list
