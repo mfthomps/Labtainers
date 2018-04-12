3e170470d0123ade12367680db723886facc737b
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-04-12 23:05:24 +0000

    move distrib under trunk
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@1700 ed162e55-3153-4343-a18d-e086fa355ed4

---
+++ distrib/mkdist.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+function contains() {
+    local n=$#
+    local value=${!n}
+    for ((i=1;i < $#;i++)) {
+        if [ "${!i}" == "${value}" ]; then
+            echo "y"
+            return 0
+        fi
+    }
+    echo "n"
+    return 1
+}
+skip="skip-labs"
+skiplist=""
+lines=`cat $skip`
+for line in $lines; do
+    lab=$(basename $line)
+    hack=$lab/
+    skiplist+=($hack)
+    #echo "added $hack"
+done
+mkdir -p /tmp/labtainer_pdf
+#myshare=/home/mike/sf_SEED/
+myshare=/media/sf_SEED/
+here=`pwd`
+cd ../
+svn status | grep -E "^M|^D|^A" | less
+ddir=/tmp/labtainer-distrib
+ldir=/tmp/labtainer-distrib/labtainer
+rm -fr /$ddir
+mkdir $ddir
+mkdir $ldir
+cd $ldir
+mkdir trunk
+cd trunk
+svn export https://tor.ern.nps.edu/svn/proj/labtainer/trunk/README
+sed -i "s/mm\/dd\/yyyy/$(date '+%m\/%d\/%Y %H:%M')/" README
+svn export https://tor.ern.nps.edu/svn/proj/labtainer/trunk/config
+svn export https://tor.ern.nps.edu/svn/proj/labtainer/trunk/setup_scripts
+svn export https://tor.ern.nps.edu/svn/proj/labtainer/trunk/docs
+mkdir scripts
+cd scripts
+svn export https://tor.ern.nps.edu/svn/proj/labtainer/trunk/scripts/labtainer-student
+svn export https://tor.ern.nps.edu/svn/proj/labtainer/trunk/scripts/labtainer-instructor
+cd ../
+mkdir labs
+cd labs
+llist=$(svn ls https://tor.ern.nps.edu/svn/proj/labtainer/trunk/labs)
+for lab in $llist; do
+    if [ $(contains "${skiplist[@]}" $lab) != "y" ]; then
+        mkdir $lab
+        cd $lab
+        mkdir -p /tmp/labtainer_pdf/$lab
+        svn export https://tor.ern.nps.edu/svn/proj/labtainer/trunk/labs/$lab/config
+        svn export https://tor.ern.nps.edu/svn/proj/labtainer/trunk/labs/$lab/instr_config
+        svn export https://tor.ern.nps.edu/svn/proj/labtainer/trunk/labs/$lab/docs
+        if [[ -d docs ]]; then
+            cd docs
+            cp -p /tmp/labtainer_pdf/$lab/*.pdf .
+            if [[ -f Makefile ]]; then
+                make
+            else
+                doc=$lab.docx
+                if [[ -f $doc ]]; then
+                    soffice --convert-to pdf $doc --headless
+                fi
+            fi
+            cp -p *pdf /tmp/labtainer_pdf/$lab/
+            cd ../
+        fi
+        cd ../
+    fi
+done
+cd $ldir
+mv trunk/setup_scripts/install-labtainer.sh .
+ln -s trunk/setup_scripts/update-labtainer.sh .
+ln -s trunk/setup_scripts/update-designer.sh .
+
+cd $ldir/trunk/docs/student
+make
+cp labtainer-student.pdf ../../../
+cp labtainer-student.pdf $myshare
+
+cd $ldir/trunk/docs/instructor
+make
+cp labtainer-instructor.pdf ../../../
+cp labtainer-instructor.pdf $myshare
+cd $ddir
+tar -cz -X $here/skip-labs -f $here/labtainer.tar labtainer
+cd $here
+cp labtainer.tar $myshare
