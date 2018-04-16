8180bbc1f3a28b19ca0e2b017ef3de1de872777a
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-04-16 16:30:09 +0000

    add date in README.md
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@1729 ed162e55-3153-4343-a18d-e086fa355ed4

--- distrib/mkdist.sh
+++ distrib/mkdist.sh
@@ -35,7 +35,7 @@ cd $ldir
 mkdir trunk
 cd trunk
 svn export https://tor.ern.nps.edu/svn/proj/labtainer/trunk/README.md
-sed -i "s/mm\/dd\/yyyy/$(date '+%m\/%d\/%Y %H:%M')/" README
+sed -i "s/mm\/dd\/yyyy/$(date '+%m\/%d\/%Y %H:%M')/" README.md
 svn export https://tor.ern.nps.edu/svn/proj/labtainer/trunk/config
 svn export https://tor.ern.nps.edu/svn/proj/labtainer/trunk/setup_scripts
 svn export https://tor.ern.nps.edu/svn/proj/labtainer/trunk/docs
