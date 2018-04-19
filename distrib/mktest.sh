9eab139389aeb16779f3a0d1708ad10023a2af63
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-04-19 17:39:59 +0000

    make test set tar for testing VM to use
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@1739 ed162e55-3153-4343-a18d-e086fa355ed4

---
+++ distrib/mktest.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+tmp_dir=/tmp/labtainer_tests
+rm -fr $tmp_dir
+mkdir $tmp_dir
+cd ../
+svn status testsets | less
+cd $tmp_dir
+mkdir trunk
+cd trunk
+svn export https://tor.ern.nps.edu/svn/proj/labtainer/trunk/testsets
+cd ../
+svn export https://tor.ern.nps.edu/svn/proj/labtainer/simlab
+tar czf /tmp/labtainer-tests.tar trunk simlab
+mv /tmp/labtainer-tests.tar /media/sf_SEED/
