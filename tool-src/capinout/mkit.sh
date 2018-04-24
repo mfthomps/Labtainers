da968ac98ac23b75696be5fe14fb3293c6ea2577
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-04-24 01:06:20 +0000

    more broken bits
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@1757 ed162e55-3153-4343-a18d-e086fa355ed4

--- tool-src/capinout/mkit.sh
+++ tool-src/capinout/mkit.sh
@@ -1,3 +1,3 @@
 g++ mypty2.cpp -o capinout -static-libstdc++ 
-cp capinout /home/mike/svn/labtainer/trunk/scripts/labtainer-student/lab_sys/sbin/
+cp capinout ../../scripts/labtainer-student/lab_sys/sbin/
 
