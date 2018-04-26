94c34e405a29033d7bf82818cfcc6082f9f65dc2
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-04-26 00:44:32 +0000

    add extra layer of image to augment centos baseline
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@1766 ed162e55-3153-4343-a18d-e086fa355ed4

---
+++ scripts/designer/bin/create_image.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+: <<'END'
+This software was created by United States Government employees at 
+The Center for the Information Systems Studies and Research (CISR) 
+at the Naval Postgraduate School NPS.  Please note that within the 
+United States, copyright protection is not available for any works 
+created  by United States Government employees, pursuant to Title 17 
+United States Code Section 105.   This software is in the public 
+domain and is not subject to copyright. 
+END
+source ./set_reg.sh
+if [[ "$2" != -f ]]; then
+   echo "This will build the labtainer $1 image.  "
+   echo "Confirm that the labtainer.centos has been published."
+   echo "registry is $LABTAINER_REGISTRY"
+   read -p "Continue? (y/n)"
+   if [[ ! $REPLY =~ ^[Yy]$ ]]
+   then
+       echo exiting
+       exit
+   fi
+else
+   echo "registry is $LABTAINER_REGISTRY"
+fi
+here=`pwd`
+cd ../
+docker build --build-arg registry=$LABTAINER_REGISTRY -f base_dockerfiles/Dockerfile.labtainer.$1 -t labtainer.$1:latest .
+cd $here
