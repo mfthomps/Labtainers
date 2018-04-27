0ab9d3b2bbea08f433f1c0f5a07a40a41ac32644
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-04-27 23:07:21 +0000

    add version field to readme
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@1798 ed162e55-3153-4343-a18d-e086fa355ed4

--- distrib/mkdist.sh
+++ distrib/mkdist.sh
@@ -36,6 +36,7 @@ mkdir trunk
 cd trunk
 svn export https://tor.ern.nps.edu/svn/proj/labtainer/trunk/README.md
 sed -i "s/mm\/dd\/yyyy/$(date '+%m\/%d\/%Y %H:%M')/" README.md
+sed -i "s/^Version:/version: $(svn info --show-item revision)/" README.md
 svn export https://tor.ern.nps.edu/svn/proj/labtainer/trunk/config
 svn export https://tor.ern.nps.edu/svn/proj/labtainer/trunk/setup_scripts
 svn export https://tor.ern.nps.edu/svn/proj/labtainer/trunk/docs
