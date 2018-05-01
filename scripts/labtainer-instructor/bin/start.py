0254b88edf38423487cbc946530e0cfb77e89e1c
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-05-01 15:32:49 +0000

    add auto grading, change defaults for instructor to not run full lab
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@1817 ed162e55-3153-4343-a18d-e086fa355ed4

--- scripts/labtainer-instructor/bin/start.py
+++ scripts/labtainer-instructor/bin/start.py
@@ -54,6 +54,9 @@ def main():
     parser = argparse.ArgumentParser(description='Start a Labtainers lab.  Provide no arguments see a list of labs.')
     parser.add_argument('labname', help='The lab to run')
     parser.add_argument('-r', '--redo', action='store_true', help='Creates new instance of the lab, previous work will be lost.')
+    parser.add_argument('-a', '--all_containers', action='store_true', help='Create all containers as seen by the student.')
+    parser.add_argument('-d', '--debug_grade', action='store_true', help='Create the grading container leave it running with a terminal')
+    parser.add_argument('-q', '--quiet', action='store_true', help='Do not display results.')
     num_args = len(sys.argv)
     if num_args < 2: 
         showLabs(dirs, path)
@@ -66,14 +69,22 @@ def main():
 
     saki.checkBulkSaki(bulk_path=None, lab=labname)
     lab_path = os.path.join(os.path.abspath('../../labs'), labname)
-    summary = docgoals.getGoalInfo(os.path.join(lab_path,'instr_config'))
-    print summary
+    if args.quiet or args.all_containers:
+        summary = docgoals.getGoalInfo(os.path.join(lab_path,'instr_config'))
+        print summary
     labutils.logger = LabtainerLogging.LabtainerLogging("labtainer.log", labname, "../../config/labtainer.config")
     labutils.logger.INFO("Begin logging start.py for %s lab" % labname)
+    auto_grade = True
+    if args.all_containers:
+        if args.debug_grade:
+            print('--all_containers and --debug_grade are mutually exclusive')
+            parser.usage()
+            exit(1)
+        auto_grade = False
     if not args.redo:
-        labutils.StartLab(lab_path, "instructor")
+        labutils.StartLab(lab_path, "instructor", auto_grade=auto_grade, debug_grade=args.debug_grade, quiet_start=args.quiet)
     else:
-        labutils.RedoLab(lab_path, "instructor", force_build=force_build)
+        labutils.RedoLab(lab_path, "instructor", force_build=force_build, auto_grade=auto_grade, debug_grade=args.debug_grade, quiet_start=args.quiet)
 
     return 0
 if __name__ == '__main__':
