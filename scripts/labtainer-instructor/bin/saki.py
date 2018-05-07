0aac30f2bd1fe9fff45e11c968002550b2d0a859
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-05-07 14:52:48 +0000

    put student reports into reports directory
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@1854 ed162e55-3153-4343-a18d-e086fa355ed4

--- scripts/labtainer-instructor/bin/saki.py
+++ scripts/labtainer-instructor/bin/saki.py
@@ -4,6 +4,7 @@ import sys
 import time
 import zipfile
 import shutil
+from io import BytesIO
 instructor_cwd = os.getcwd()
 student_cwd = instructor_cwd.replace('labtainer-instructor', 'labtainer-student')
 # Append Student CWD to sys.path
@@ -40,20 +41,43 @@ def extract(zip_fname, xfer, expect_lab):
                 if not filename:
                     continue
 
-                # copy file (taken from zipfile's extract)
+                # copy file (taken from zipfile's extract) into xfer for lab
                 source = zip_file.open(member)
                 target = file(os.path.join(lab_xfer, filename), "wb")
                 with source, target:
                     shutil.copyfileobj(source, target)
+
+                # copy reports
+                parts = member.split('/')
+                student = parts[1]
+                target_dir = os.path.join(results_dir, student)
+                zip_file_data = BytesIO(zip_file.read(member))
+                with zipfile.ZipFile(zip_file_data) as zip_zips:
+                   for zi in zip_zips.namelist():
+                       if zi == 'docs.zip':
+                           docs_zip_data = BytesIO(zip_zips.read(zi))
+                           with zipfile.ZipFile(docs_zip_data) as zip_docs:
+                               for zdoc_info in zip_docs.infolist():
+                                   zdoc = zdoc_info.filename     
+                                   doc_date_time = time.mktime(zdoc_info.date_time + (0, 0, -1))
+                                   print('look for report in %s' % zdoc)
+                                   fname, ext = os.path.splitext(zdoc)
+                                   if (ext == '.docx' or ext == '.odg') and (fname+'.pdf' not in zip_docs.namelist()):
+                                       if not os.path.isfile(os.path.join(target_dir, zdoc)):
+                                           zip_docs.extract(zdoc, target_dir)
+                                           print('extracted report %s to %s' % (zdoc, target_dir))
+                                           os.utime(os.path.join(target_dir, zdoc), (doc_date_time, doc_date_time))
+                                       else:
+                                           print('found doc at %s, do not overwrite' % os.path.join(target_dir, zdoc))
+
             else:
                 fname, ext = os.path.splitext(filename)
                 if (ext == '.docx' or ext == '.odg'): 
                     source = zip_file.open(member)
                     parts = member.split('/')
                     student = parts[1]
-                
                     target_dir = os.path.join(results_dir, student)
-                    print('target_dir is %s' % target_dir)
+                    #print('target_dir is %s' % target_dir)
                     try:
                         os.makedirs(target_dir)
                     except OSError:
@@ -61,7 +85,7 @@ def extract(zip_fname, xfer, expect_lab):
                     #zip_file.extract(member, target_dir)
                     target = file(os.path.join(target_dir, filename), "wb")
       
-                    print('copied %s to %s' % (source, target))
+                    #print('copied %s to %s' % (source, target))
                     shutil.copyfileobj(source, target)
                     os.utime(os.path.join(target_dir, filename), (date_time, date_time))
     if count > 0:
