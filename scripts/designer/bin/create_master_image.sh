6e71f202a90b4f6f558cb135f1370c201e683b5c
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-05-29 16:18:03 +0000

    create master container as alternative to installing Labtainers
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@2047 ed162e55-3153-4343-a18d-e086fa355ed4

---
+++ scripts/designer/bin/create_master_image.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# Create a master Labtainer image for use in running Labtainers from a container
+# on any system that has Docker installed, withou having to install Labtainers.
+# Thanks for Olivier Berger for this contribution.
+#
+source ./set_reg.sh
+if [[ "$1" != -f ]]; then
+   echo "This will build the labtainer master controller image.  "
+   echo "registry is $LABTAINER_REGISTRY"
+   read -p "Continue? (y/n)"
+   if [[ ! $REPLY =~ ^[Yy]$ ]]
+   then
+       echo exiting
+       exit
+   fi
+else
+   echo "registry is $LABTAINER_REGISTRY"
+fi
+here=`pwd`
+cd ../
+cp ../../distrib/labtainer.tar ./
+
+cat <<EOT >bashrc.labtainer.master
+   if [[ ":\$PATH:" != *":./bin:"* ]]; then 
+       export PATH="\${PATH}:./bin:/home/labtainer/labtainer/trunk/scripts/designer/bin"
+       export LABTAINER_DIR=/home/labtainer/labtainer/trunk
+   fi
+EOT
+
+cp ../../scripts/labtainer-student/bin/labutils.py ./
+docker build --build-arg DOCKER_GROUP_ID="$(getent group docker | cut -d: -f3)" -f base_dockerfiles/Dockerfile.labtainer.master -t labtainer.master:latest .
+cd $here
