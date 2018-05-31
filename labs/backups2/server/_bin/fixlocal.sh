5cb3796262b4fda25a1de4b5de1e3f84b61f0bc2
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-05-31 20:15:52 +0000

    revision to backups lab to include backup to remote server
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@2063 ed162e55-3153-4343-a18d-e086fa355ed4

---
+++ labs/backups2/server/_bin/fixlocal.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+#  Script will be run after parameterization has completed, e.g., 
+#  use this to compile source code that has been parameterized.
+#
+img_dir=/vfs
+img_fid=/vfs/myfs.img
+sudo mkdir $img_dir
+sudo dd if=/dev/zero of=$img_fid bs=1k count=100k
+sudo parted -s /vfs/myfs.img mklabel bsd mkpart ext2 1 100
+sudo losetup -f /vfs/myfs.img -P --show
+sudo mkfs.ext2 /dev/loop2
+sudo mkdir -p /lab_mnt
+sudo mount /dev/loop2 /lab_mnt
+sudo mkdir /lab_mnt/usr
+sudo cp -R /usr/bin /lab_mnt/usr/
+sudo cp /lab_mnt/usr/bin/who /lab_mnt/usr/bin/cheese
+sudo route delete default
+sudo route add -host 172.25.3.3 gw 172.25.0.3
+
