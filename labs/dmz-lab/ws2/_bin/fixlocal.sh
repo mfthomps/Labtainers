b502ed1f7595822663e62b7e9a86f7f5ccaf8da9
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-06-05 00:23:11 +0000

    fixlocal.sh were done on things that don't persist across boots, e.g., routing
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@2078 ed162e55-3153-4343-a18d-e086fa355ed4

--- labs/dmz-lab/ws2/_bin/fixlocal.sh
+++ labs/dmz-lab/ws2/_bin/fixlocal.sh
@@ -7,9 +7,3 @@
 #  not not permit nopassword, then use:
 #  echo $1 | sudo -S the-command
 #
-# Use gateway as the name server
-#
-echo "nameserver 198.18.1.3" | sudo tee /etc/resolv.conf
-sudo /usr/bin/set_default_gw.sh 198.18.1.INNER_LAN2
-# remove the dns route so as to not confuse student
-sudo route del -host 172.17.0.1
