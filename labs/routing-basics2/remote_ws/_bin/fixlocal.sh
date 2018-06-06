51906d558f5ddc98930588dfef2c75ae0ca05676
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-06-06 22:54:06 +0000

    move routing to rc.local
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@2083 ed162e55-3153-4343-a18d-e086fa355ed4

--- labs/routing-basics2/remote_ws/_bin/fixlocal.sh
+++ labs/routing-basics2/remote_ws/_bin/fixlocal.sh
@@ -7,6 +7,3 @@
 #  not not permit nopassword, then use:
 #  echo $1 | sudo -S the-command
 #
-echo "nameserver 172.16.0.10" | sudo tee /etc/resolv.conf
-# remove the dns route so as to not confuse student
-sudo route del -host 172.17.0.1
