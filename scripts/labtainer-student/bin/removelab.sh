ef0fbd6629c94eb866ed55918326a6ca5592068e
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-06-07 23:15:21 +0000

    was removing any lab with the target as a substring
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@2095 ed162e55-3153-4343-a18d-e086fa355ed4

--- scripts/labtainer-student/bin/removelab.sh
+++ scripts/labtainer-student/bin/removelab.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-cont_list=$(docker ps -a | grep $1 | awk '{print $1}')
+cont_list=$(docker ps -a | grep " $1\." | awk '{print $1}')
 if [[ ! -z "$cont_list" ]]; then
     docker rm $cont_list
 fi
-image_list=$(docker images | grep "$1\." | awk '{print $1}')
+image_list=$(docker images | grep "^$1\." | awk '{print $1}')
 if [[ ! -z "$image_list" ]]; then
     docker rmi -f $image_list
 else
