87ef37193c95c81b5b4d5c8f8786da941964bcee
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-06-07 16:20:04 +0000

    do not replace target image if it exists
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@2090 ed162e55-3153-4343-a18d-e086fa355ed4

--- distrib/pull_tag_push.py
+++ distrib/pull_tag_push.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import os
 import sys
+sys.path.append('../scripts/labtainer-student/bin')
+import InspectLocalReg
 '''
 Pull all labtainer container images from the docker hub, retag them, and push to a 
 local registry.
@@ -20,15 +22,21 @@ def do_lab(lab_dir, lab, role, source_reg, dest_reg):
         except:
             print('could not get image from %s' % df);
             continue
-        cmd = 'docker pull %s/%s' % (source_reg, image)
-        #print cmd
-        os.system(cmd)
-        cmd = 'docker tag %s/%s %s/%s' % (source_reg, image, dest_reg, image)
-        os.system(cmd)
-        cmd = 'docker push %s/%s' % (dest_reg, image)
+        created, user = InspectLocalReg.inspectLocal(image, dest_reg)
+        print created
+        if created is None:
+            cmd = 'docker pull %s/%s' % (source_reg, image)
+            #print cmd
+            os.system(cmd)
+            cmd = 'docker tag %s/%s %s/%s' % (source_reg, image, dest_reg, image)
+            #print cmd
+            os.system(cmd)
+            cmd = 'docker push %s/%s' % (dest_reg, image)
+            #print cmd
+            os.system(cmd)
 
 skip = []
-with open(skip_labs) as fh:
+with open('skip-labs') as fh:
    for line in fh:
        f = os.path.basename(line).strip()
        print('will skip [%s]' % f)
@@ -39,8 +47,15 @@ lab_list = os.listdir(labdir)
 #
 # test with a single lab.  Then use loop below once it works.
 #
-do_lab(labdir, 'sql-inject', 'student', 'mfthomps', 'testregistry:5000')
-#for lab in lab_list:
-#    if lab not in skip:
-#        do_lab(labdir, lab, 'student', 'mfthomps', 'testregistry:5000')
-#        do_lab(labdir, lab, 'instructor', 'mfthomps', 'testregistry:5000')
+testregistry = 'testregistry:5000'
+if len(sys.argv) > 1:
+    lab = sys.argv[1]
+    do_lab(labdir, lab, 'student', 'mfthomps', testregistry)
+    do_lab(labdir, lab, 'instructor', 'mfthomps', testregistry)
+else:
+    #print('commented out for now')
+    testregistry = 'testregistry:5000'
+    for lab in lab_list:
+        if lab not in skip:
+            do_lab(labdir, lab, 'student', 'mfthomps', testregistry)
+            do_lab(labdir, lab, 'instructor', 'mfthomps', testregistry)
