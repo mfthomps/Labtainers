a524d52d942ce443f0de635958f59605a9f78705
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-06-07 16:52:46 +0000

    remount using the loop2 device
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@2092 ed162e55-3153-4343-a18d-e086fa355ed4

--- labs/backups2/server/_bin/student_startup.sh
+++ labs/backups2/server/_bin/student_startup.sh
@@ -4,7 +4,6 @@ if [[ $result -eq 0 ]]; then
    df | grep lab_mnt >>/dev/null
    mounted=$?
    if [[ $mounted -ne 0 ]]; then
-       img_fid=/vfs/myfs.img
-       sudo mount -o loop $img_fid /lab_mnt
+       sudo mount /dev/loop2 /lab_mnt
    fi
 fi
