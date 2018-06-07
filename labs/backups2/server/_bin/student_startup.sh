872d506fc7d6074db5b9a0a1fe9e8b9f37074a2a
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-06-07 17:56:51 +0000

    fix handling of loopback devices
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@2093 ed162e55-3153-4343-a18d-e086fa355ed4

--- labs/backups2/server/_bin/student_startup.sh
+++ labs/backups2/server/_bin/student_startup.sh
@@ -1,9 +1,15 @@
 id | grep root >>/dev/null
 result=$?
 if [[ $result -eq 0 ]]; then
+# 
+#  Mount the loopback device on /lab_mnt if not already mounted.
+#  Need to first find which loopback device is mapped to /vfs/myfs.img.
+#  Take most recent one, there may be many.
+#
    df | grep lab_mnt >>/dev/null
    mounted=$?
    if [[ $mounted -ne 0 ]]; then
-       sudo mount /dev/loop2 /lab_mnt
+       loopdev=$(losetup -l | grep /vfs/myfs.img | head -1 | awk '{print $1}')
+       sudo mount $loopdev /lab_mnt
    fi
 fi
