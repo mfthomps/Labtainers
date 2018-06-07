fdcff76787051ce2890e1193ca0d028701f17dec
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-06-07 16:12:43 +0000

    move routing and such from fixlocal to rc.local to persist across stop/start
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@2086 ed162e55-3153-4343-a18d-e086fa355ed4

--- labs/snort/snort/_bin/fixlocal.sh
+++ labs/snort/snort/_bin/fixlocal.sh
@@ -9,9 +9,6 @@
 #
 # Use gateway as the name server
 #
-echo "nameserver 192.168.3.10" | sudo tee /etc/resolv.conf
-# remove the dns route so as to not confuse student
-sudo route del -host 172.17.0.1
 
 # Avoid the circle of death -- no packet forwarding back to the gateway!
 sudo sysctl -w net.ipv4.ip_forward=0
