44209136a0b822cf27646acfcd86bc2af24455fd
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-06-15 22:15:45 +0000

    remove labs from whatever registry
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@2145 ed162e55-3153-4343-a18d-e086fa355ed4

--- scripts/labtainer-student/bin/removelab.sh
+++ scripts/labtainer-student/bin/removelab.sh
@@ -3,7 +3,7 @@ cont_list=$(docker ps -a | grep " $1\." | awk '{print $1}')
 if [[ ! -z "$cont_list" ]]; then
     docker rm $cont_list
 fi
-image_list=$(docker images | grep "^$1\." | awk '{print $1}')
+image_list=$(docker images | grep "^*./$1\." | awk '{print $1}')
 if [[ ! -z "$image_list" ]]; then
     docker rmi -f $image_list
 else
