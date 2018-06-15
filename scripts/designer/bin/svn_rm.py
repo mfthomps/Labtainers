4046f3f62d6f77114b480bb9d83086c624ce900f
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-06-15 17:45:10 +0000

    use this so containers get rebuilt
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@2131 ed162e55-3153-4343-a18d-e086fa355ed4

---
+++ scripts/designer/bin/svn_rm.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+'''
+Use svn to remove a file, and then modify the parent directory
+(using a dummy property), so that affected containers get rebuilt.
+'''
+import os
+import sys
+import time
+if len(sys.argv) != 2:
+    print('svn_rm.py path')
+
+path = sys.argv[1]
+cmd = 'svn rm %s' % path
+print cmd
+if '/' in path:
+    parent = os.path.dirname(path)
+else:
+    parent = '../'
+
+print('parent is %s' % parent)
+cmd = "svn propset file_rm_date '%s' %s" % (time.time(), parent)
+print cmd
