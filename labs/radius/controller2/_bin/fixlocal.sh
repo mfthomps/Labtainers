fbe656b384b9d965af8797684f870ba4ca8f537d
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-06-21 00:15:27 +0000

    change names, add second radius client
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@2174 ed162e55-3153-4343-a18d-e086fa355ed4

---
+++ labs/radius/controller2/_bin/fixlocal.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+#  Script will be run after parameterization has completed, e.g., 
+#  use this to compile source code that has been parameterized.
+#  The container user password will be passed as the first argument.
+#  Thus, if this script is to use sudo and the sudoers for the lab
+#  not not permit nopassword, then use:
+#  echo $1 | sudo -S the-command
+#
+sudo sed -i 's/@include common-auth/#@include common-auth/' /etc/pam.d/sshd
+sudo sed -i '/@include common-auth/a session    sufficient      /lib/security/pam_radius_auth.so debug conf=/etc/pam_radius_auth.conf' /etc/pam.d/sshd
+sudo sed -i '/@include common-auth/a auth       sufficient      /lib/security/pam_radius_auth.so debug' /etc/pam.d/sshd
+sudo /etc/init.d/xinetd restart
+
+me=$(hostname)
+cat >> $HOME/.profile << EOL
+echo "Welcome to the $me shambang controller."
+echo "Use 'exit' to terminate your session"
+EOL
--- labs/radius/radius/_bin/student_startup.sh
+++ labs/radius/radius/_bin/student_startup.sh
@@ -1,1 +1,13 @@
-sudo radiusd -X
+id | grep root >>/dev/null
+result=$?
+if [[ $result -ne 0 ]]; then
+cat << EOF
+Use this command:
+   radiusd -X
+to start the radius server in debug mode.
+Use ctrl-C to stop the server.
+
+Radius configuration files are in /etc/raddb
+EOF
+sudo su -
+fi
