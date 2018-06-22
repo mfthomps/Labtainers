9c4192cf52114df7ded4f17afaff01cd10b41395
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-06-22 22:21:43 +0000

    move sources-list to /var/tmp
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@2194 ed162e55-3153-4343-a18d-e086fa355ed4

--- scripts/designer/system/bin/yum-source.sh
+++ scripts/designer/system/bin/yum-source.sh
@@ -7,11 +7,12 @@
 #
 #
 if [[ "$APT_SOURCE" != NPS ]]; then
-    if [ -d /tmp/yum.repos.d ]; then
-        mv /tmp/yum.repos.d/* /etc/yum.repos.d/
-        rmdir /tmp/yum.repos.d
+    if [ -d /var/tmp/yum.repos.d ]; then
+        mv /var/tmp/yum.repos.d/* /etc/yum.repos.d/
+        rmdir /var/tmp/yum.repos.d
     fi
 else
-    mv /etc/yum.repos.d/epel* /tmp/yum.repos.d/
+    mkdir -p /var/tmp/yum.repos.d
+    mv /etc/yum.repos.d/epel* /var/tmp/yum.repos.d/
 fi
 
