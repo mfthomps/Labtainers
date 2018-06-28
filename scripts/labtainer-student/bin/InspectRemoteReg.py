b3b28d32cdc4e97cfa7e8086eedf8543912c8fc3
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-06-28 22:53:20 +0000

    steps toward tagging images with base id
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@2213 ed162e55-3153-4343-a18d-e086fa355ed4

--- scripts/labtainer-student/bin/InspectRemoteReg.py
+++ scripts/labtainer-student/bin/InspectRemoteReg.py
@@ -17,14 +17,58 @@ Return creation date and user of a given image from the Docker Hub
 without pulling the image.
 '''
 def inspectRemote(image):
+    use_tag = 'latest'
     token = getToken(image)
     if token is None or len(token.strip()) == 0:
         return None, None, None
     digest = getDigest(token, image, 'latest')
     if digest is None:
         return None, None, None
-    created, user, version = getCreated(token, image, digest)
-    return created, user, version
+    created, user, version, base = getCreated(token, image, digest)
+    print('base is %s' % base)
+    base_image, base_id = base.rsplit('.', 1)
+    cmd = 'docker images -f=reference="%s" -q ' % base_image
+    ps = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    output = ps.communicate()
+    if len(output[0].strip()) > 0:
+        my_id = output[0].strip()
+        if my_id == base_id:
+            pass
+            #print('got correct base_id')
+        else:
+            #print('got WRONG base_id')
+            tlist = getTags(image, test_registry)
+            need_tag = 'base_image%s' % my_id
+            if need_tag in tlist:
+                use_tag = need_tag
+            else:
+                print('**************************************************')
+                print('*  This lab will require a download of           *')
+                print('*  several hundred megabytes.                    *')
+                print('**************************************************')
+                confirm = str(raw_input('Continue? (y/n)')).lower().strip()
+                if confirm != 'y':
+                    print('Exiting lab')
+                    exit(0)
+                else:
+                    print('Please wait for download to complete...')
+                    cmd = 'docker pull %s' % base_image
+                    os.system(cmd)
+                    print('Download has completed.  Wait for lab to start.')
+    return created, user, version, use_tag
+
+def getTags(image, test_registry):
+    cmd =   'curl --silent --header "Accept: application/vnd.docker.distribution.manifest.v2+json"  "https://registry-1.docker.io/v2/%s/tags/list"' % (image)
+    ps = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    output = ps.communicate()
+    if len(output[0].strip()) > 0:
+        j = json.loads(output[0])
+        if 'tags' in j:
+            return j['tags']
+        else:
+            return None
+    else:
+        return None
 
 def getToken(image):
     cmd = 'curl --silent "https://auth.docker.io/token?scope=repository:%s:pull&service=registry.docker.io"' % (image) 
@@ -58,9 +102,12 @@ def getCreated(token, image, digest):
     if len(output[0].strip()) > 0:
         j = json.loads(output[0])
         version = None
+        base = None
         if 'version' in j['container_config']['Labels']:
             version = j['container_config']['Labels']['version'] 
-        return j['created'], j['container_config']['User'], version
+        if 'base' in j['container_config']['Labels']:
+            base = j['container_config']['Labels']['base'] 
+        return j['created'], j['container_config']['User'], version, base
     else:
-        return None, None, None
+        return None, None, None, None
 
