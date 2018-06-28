f645aa218c487e858582cf774f70a73a67299db5
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-06-28 22:53:20 +0000

    steps toward tagging images with base id
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@2213 ed162e55-3153-4343-a18d-e086fa355ed4

---
+++ scripts/labtainer-student/bin/VersionInfo.py
@@ -0,0 +1,24 @@
+def getFrom(dockerfile, registry):
+    with open(dockerfile) as fh:
+        for line in fh:
+            if line.strip().startswith('FROM'):
+                parts = line.strip().split()
+                image_name = parts[1]
+                image_name = image_name.replace("$registry", registry)
+                break
+    return image_name
+
+def getImageId(image):
+    cmd = 'docker images | grep %s' % image
+    ps = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    output = ps.communicate()
+    if len(output[1]) > 0:
+        print(output[1])
+        exit(1)
+    if len(output[0]) > 0:
+        parts = output[0].split()
+        return parts[2]
+    else:
+        print('no image found for %s' % image)
+        exit(1)
+
