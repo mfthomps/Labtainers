a21dfc51e528753beaa0cc53ccf7971d19289d98
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-06-28 23:22:15 +0000

    get only latest base image id
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@2215 ed162e55-3153-4343-a18d-e086fa355ed4

--- scripts/labtainer-student/bin/VersionInfo.py
+++ scripts/labtainer-student/bin/VersionInfo.py
@@ -1,4 +1,5 @@
 def getFrom(dockerfile, registry):
+    image_name = None
     with open(dockerfile) as fh:
         for line in fh:
             if line.strip().startswith('FROM'):
@@ -9,15 +10,15 @@ def getFrom(dockerfile, registry):
     return image_name
 
 def getImageId(image):
-    cmd = 'docker images | grep %s | grep latest' % image
+    #cmd = 'docker images | grep %s' % image
+    cmd = 'docker images -f=reference="%s:latest" -q ' % image
     ps = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
     output = ps.communicate()
     if len(output[1]) > 0:
         print(output[1])
         exit(1)
     if len(output[0]) > 0:
-        parts = output[0].split()
-        return parts[2]
+        return output[0].strip()
     else:
         print('no image found for %s' % image)
         exit(1)
