e10ecada8bbd77ece5528676f4bc3485a7d6ce4a
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-07-05 17:56:21 +0000

    cause existing installations to use the new pull-all.py
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@2241 ed162e55-3153-4343-a18d-e086fa355ed4

--- setup_scripts/pull-all.sh
+++ setup_scripts/pull-all.sh
@@ -1,25 +1,3 @@
 #!/bin/bash
 #
-# pull the baseline labtainer images from the appropriate registry
-# NOTE use of environment variable TEST_REGISTRY
-# Script assumes the pwd is the parent of the labtainer directory
-# Intended to be called from update-labtainer.sh
-#
-if [ "$TEST_REGISTRY" == TRUE ] || [ "$1" == -t ]; then
-    registry=$(grep TEST_REGISTRY ../config/labtainer.config | tr -s ' ' | cut -d ' ' -f 3)
-else
-    registry=$(grep DEFAULT_REGISTRY ../config/labtainer.config | tr -s ' ' | cut -d ' ' -f 3)
-fi
-echo "pull from $registry"
-docker pull $registry/labtainer.base
-docker pull $registry/labtainer.network
-docker pull $registry/labtainer.firefox
-docker pull $registry/labtainer.wireshark
-docker pull $registry/labtainer.java
-docker pull $registry/labtainer.centos
-docker pull $registry/labtainer.lamp
-
-#
-# hacky cleanup functions that are not part of main update (since that is not replaced until after running update...)
-#
-rm -f ../scripts/labtainer-instructor/bin/docgoals.py*
+./pull-all.py
