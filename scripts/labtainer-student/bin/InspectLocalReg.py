1f2c146411be2bfaaf90f500fdceadcd74bbcdfb
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-07-06 18:22:11 +0000

    do not crash if no base image
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@2251 ed162e55-3153-4343-a18d-e086fa355ed4

--- scripts/labtainer-student/bin/InspectLocalReg.py
+++ scripts/labtainer-student/bin/InspectLocalReg.py
@@ -27,33 +27,36 @@ def inspectLocal(image, test_registry, is_rebuild=False):
         return None, None, None, None
     created, user, version, base = getCreated(image, digest, test_registry)
     #print('base is %s' % base)
-    base_image, base_id = base.rsplit('.', 1)
-    my_id = VersionInfo.getImageId(base_image)
-    if my_id == base_id:
-        pass
-        #print('got correct base_id')
-    else:
-        print('got WRONG base_id for base %s used in  %s my: %s  base: %s' % (base_image, image, my_id, base_id))
-        tlist = getTags(image, test_registry)
-        need_tag = 'base_image%s' % my_id
-        if is_rebuild or need_tag in tlist:
-            use_tag = need_tag
-        else:
-            print('**************************************************')
-            print('*  This lab will require a download of           *')
-            print('*  several hundred megabytes.                    *')
-            print('**************************************************')
-            confirm = str(raw_input('Continue? (y/n)')).lower().strip()
-            if confirm != 'y':
-                print('Exiting lab')
-                exit(0)
+    if base is not None:
+       base_image, base_id = base.rsplit('.', 1)
+       my_id = VersionInfo.getImageId(base_image)
+       if my_id == base_id:
+           pass
+           #print('got correct base_id')
+       else:
+            print('got WRONG base_id for base %s used in  %s my: %s  base: %s' % (base_image, image, my_id, base_id))
+            tlist = getTags(image, test_registry)
+            need_tag = 'base_image%s' % my_id
+            if is_rebuild or need_tag in tlist:
+                use_tag = need_tag
             else:
-                print('Please wait for download to complete...')
-                cmd = 'docker pull %s' % base_image
-                os.system(cmd)
-                print('Download has completed.  Wait for lab to start.')
+                print('**************************************************')
+                print('*  This lab will require a download of           *')
+                print('*  several hundred megabytes.                    *')
+                print('**************************************************')
+                confirm = str(raw_input('Continue? (y/n)')).lower().strip()
+                if confirm != 'y':
+                    print('Exiting lab')
+                    exit(0)
+                else:
+                    print('Please wait for download to complete...')
+                    cmd = 'docker pull %s' % base_image
+                    os.system(cmd)
+                    print('Download has completed.  Wait for lab to start.')
 
     return created, user, version, use_tag, base
+
+
     
 def getTags(image, test_registry):
     cmd =   'curl --silent --header "Accept: application/vnd.docker.distribution.manifest.v2+json"  "http://%s/v2/%s/tags/list"' % (test_registry, image)
