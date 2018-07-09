799d7ce356818e59510e55813ac20cdd9f6560ed
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-07-09 22:09:08 +0000

    no longer used
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@2268 ed162e55-3153-4343-a18d-e086fa355ed4

--- labs/nmap-ssh/client/_bin/fixlocal.sh
+++ labs/nmap-ssh/client/_bin/fixlocal.sh
@@ -3,3 +3,7 @@
 #  Script will be run after parameterization has completed, e.g., 
 #  use this to compile source code that has been parameterized.
 #
+sudo systemctl enable telnetbot 
+sudo systemctl start telnetbot 
+sudo systemctl enable webbot 
+sudo systemctl start webbot 
