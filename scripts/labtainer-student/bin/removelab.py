fa8bae628f42ce33fa9f2c7417d8d0a46ec973ed
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-07-10 22:31:50 +0000

    add prestop function, make removelab callable
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@2275 ed162e55-3153-4343-a18d-e086fa355ed4

--- scripts/labtainer-student/bin/removelab.py
+++ scripts/labtainer-student/bin/removelab.py
@@ -2,37 +2,49 @@
 import subprocess
 import sys
 import os
+import argparse
 
-lab = sys.argv[1]
-cmd = 'docker ps -a'
-ps = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-output = ps.communicate()
-lab_container = ' %s.' % lab
-container_list = []
-for line in output[0].splitlines():
-    #print line
-    if lab_container in line:
-        container_list.append(line.split()[0]) 
-if len(container_list) > 0:
-    cmd = 'docker rm %s' % ' '.join(container_list)
-    print cmd
-    os.system(cmd)
+def removeLab(lab):
+    cmd = 'docker ps -a'
+    ps = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    output = ps.communicate()
+    lab_container = ' %s.' % lab
+    container_list = []
+    for line in output[0].splitlines():
+        #print line
+        if lab_container in line:
+            container_list.append(line.split()[0]) 
+    if len(container_list) > 0:
+        cmd = 'docker rm %s' % ' '.join(container_list)
+        print cmd
+        os.system(cmd)
+    
+    
+    lab = sys.argv[1]
+    cmd = 'docker images'
+    ps = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    output = ps.communicate()
+    image_find = '/%s.' % lab
+    image_find2 = '%s.' % lab
+    image_list = []
+    for line in output[0].splitlines():
+        #print line
+        if (image_find in line or line.startswith(image_find2)) and ' <none> ' not in line:
+            parts = line.split()
+            image = '%s:%s' % (parts[0], parts[1])
+            image_list.append(image)
+    if len(image_list) > 0:
+        cmd = 'docker rmi -f %s' % ' '.join(image_list)
+        print cmd
+        os.system(cmd)
+    else:
+        print('No images for %s' % lab)
 
+def main():
+    parser = argparse.ArgumentParser(prog='labtainer', description='Start a Labtainers lab.  Provide no arguments see a list of labs.')
+    parser.add_argument('labname', default='NONE', nargs='?', action='store', help='The lab to delete')
+    args = parser.parse_args()
+    removeLab(args.labname)
 
-lab = sys.argv[1]
-cmd = 'docker images'
-ps = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-output = ps.communicate()
-image_find = '/%s.' % lab
-image_find2 = '%s.' % lab
-image_list = []
-for line in output[0].splitlines():
-    #print line
-    if (image_find in line or line.startswith(image_find2)) and ' <none> ' not in line:
-        parts = line.split()
-        image = '%s:%s' % (parts[0], parts[1])
-        image_list.append(image)
-if len(image_list) > 0:
-    cmd = 'docker rmi -f %s' % ' '.join(image_list)
-    print cmd
-    os.system(cmd)
+if __name__ == '__main__':
+    sys.exit(main())
