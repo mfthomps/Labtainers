74cb4bec5a9c481291fb3a99b616e5a4d5d6ba53
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-07-13 18:15:25 +0000

    iptables lab for ICS, not yet individualized
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@2291 ed162e55-3153-4343-a18d-e086fa355ed4

--- labs/iptables-ics/plc/_bin/fixlocal.sh
+++ labs/iptables-ics/plc/_bin/fixlocal.sh
@@ -3,3 +3,8 @@
 #  Script will be run after parameterization has completed, e.g., 
 #  use this to compile source code that has been parameterized.
 #
+sudo systemctl enable plc
+sudo systemctl start plc
+sudo systemctl enable httpserver.service
+sudo systemctl start httpserver.service
+
