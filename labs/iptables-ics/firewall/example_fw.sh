a31b29a56f7f0b203fc41b0c9ba8ba04633df412
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-07-13 18:15:25 +0000

    iptables lab for ICS, not yet individualized
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@2291 ed162e55-3153-4343-a18d-e086fa355ed4

---
+++ labs/iptables-ics/firewall/example_fw.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+#  This example IPTABLES firewall will only allow SSH traffic
+#  to be forwarded from 172.24.0.2.
+#
+IPTABLES=/sbin/iptables
+
+#start and flush
+$IPTABLES -F
+$IPTABLES -t nat -F
+$IPTABLES -X
+#
+#  By default, do not allow any forwarding or accept any traffic
+#  destined for the firewall.
+#
+$IPTABLES -P FORWARD DROP
+$IPTABLES -P INPUT   DROP
+$IPTABLES -P OUTPUT  DROP
+
+# Allow forwarding of traffic associated with any established session 
+$IPTABLES -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+
+# Allow SSH traffic from 172.24.0.2
+$IPTABLES -A FORWARD -p tcp -s 172.24.0.2 --dport 22 -j ACCEPT
+
+# loopback device (internal traffic)
+iptables -A INPUT -i lo -p all -j ACCEPT
+
