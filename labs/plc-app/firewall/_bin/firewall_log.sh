9b28b990e6091024337ad1aa4223bf72669012d7
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-07-30 17:12:06 +0000

    change proxy to firewall, more robust command line processing
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@2328 ed162e55-3153-4343-a18d-e086fa355ed4

---
+++ labs/plc-app/firewall/_bin/firewall_log.sh
@@ -0,0 +1,1 @@
+tail -f firewall.log
--- labs/plc-app/monitor/_system/usr/bin/heartbeat.py
+++ labs/plc-app/monitor/_system/usr/bin/heartbeat.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python
 import os
 import time
+import subprocess
+import logging
+LOGFILE = "./historian.log"
+print("logging to %s" % LOGFILE)
+logging.basicConfig(filename=LOGFILE, level=logging.DEBUG, 
+                         format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%y-%m-%d %H:%M:%S')
+logging.debug("Starting historian")
+
 cmd = 'manage_plc status'
 while True:
-    os.system(cmd)
+    ps = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    output = ps.communicate()
+    if len(output[0].strip()) > 0:
+         logging.debug('%s' % output[0].strip()) 
     time.sleep(20)
     
