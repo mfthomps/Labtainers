e2866f3d694c8ed4970fb2ee476f763d32e83644
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-08-09 20:48:13 +0000

    include logging of iptables
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@2402 ed162e55-3153-4343-a18d-e086fa355ed4

---
+++ labs/iptables2/firewall/example_fw.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+#  This example IPTABLES firewall will only allow SSH traffic
+#  to be forwarded
+#
+IPTABLES=/sbin/iptables
+
+#start and flush
+$IPTABLES -F
+$IPTABLES -t nat -F
+$IPTABLES -X
+#
+#  By default, do not allow any forwarding or accept any traffic
+#  destined for the firewall.
+#
+$IPTABLES -P FORWARD DROP
+$IPTABLES -P INPUT   DROP
+$IPTABLES -P OUTPUT  DROP
+
+# Allow forwarding of traffic associated with any established session 
+$IPTABLES -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+
+# Allow SSH traffic on port 22
+$IPTABLES -A FORWARD --dport 22 -j ACCEPT
+
+# loopback device (internal traffic)
+iptables -A INPUT -i lo -p all -j ACCEPT
+
+# log IPTABLES filtering actions
+iptables -A FORWARD -j NFLOG -m limit --limit 2/min --nflog-prefix "IPTABLES DROPPED"
+
