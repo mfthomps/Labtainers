3a075d65181be04103f6ba71a120a66aef94cea8
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-08-09 20:48:13 +0000

    include logging of iptables
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@2402 ed162e55-3153-4343-a18d-e086fa355ed4

--- labs/iptables2/firewall/_bin/fixlocal.sh
+++ labs/iptables2/firewall/_bin/fixlocal.sh
@@ -3,3 +3,5 @@
 #  Script will be run after parameterization has completed, e.g., 
 #  use this to compile source code that has been parameterized.
 #
+sudo sed -i s!/var/log/ulog/syslogemu.log!/var/log/iptables.log! /etc/ulogd.conf
+sudo systemctl restart ulogd
