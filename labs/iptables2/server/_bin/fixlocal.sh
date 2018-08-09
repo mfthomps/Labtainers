7d4b412bb52237f864817256cc7ec55da0ff87e1
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-08-09 20:13:50 +0000

    rework lab to include custom service, remove precheck running of nmap
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@2395 ed162e55-3153-4343-a18d-e086fa355ed4

---
+++ labs/iptables2/server/_bin/fixlocal.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+#  Script will be run after parameterization has completed, e.g., 
+#  use this to compile source code that has been parameterized.
+#
+sudo systemctl enable httpserver.service
+sudo systemctl start httpserver.service
+
+sudo systemctl enable wizbang.service
+sudo systemctl start wizbang.service
