350c2748bf8c2b5a09bb79d19cd98d6604b18ef4
(no author) <(no author)@ed162e55-3153-4343-a18d-e086fa355ed4>
2018-08-17 00:07:19 +0000

    remove unused service files, make ftp look like vxworks
    git-svn-id: https://tor.ern.nps.edu/svn/proj/labtainer/trunk@2424 ed162e55-3153-4343-a18d-e086fa355ed4

--- labs/plc-forensics/plc/_bin/fixlocal.sh
+++ labs/plc-forensics/plc/_bin/fixlocal.sh
@@ -4,4 +4,5 @@ cd $HOME
 echo "Guest:guest" | sudo chpasswd
 sudo systemctl enable httpserver.service
 sudo systemctl start httpserver.service
-
+echo "ftpd_banner=VxWorks FTP server (VxWorks VxWorks5.5) ready." | sudo tee -a /etc/vsftpd.conf
+sudo systemctl restart vsftpd.service
