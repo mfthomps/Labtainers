7bd7d9b0699c5e4b5f3baacd3d031e58438430ad
Mike Thompson <mfthomps@nps.edu>
2018-10-18 10:20:22 -0700

    add verydodgy.com to hosts file the correct way

--- labs/macs-hash/macs-hash/_bin/fixlocal.sh
+++ labs/macs-hash/macs-hash/_bin/fixlocal.sh
@@ -3,7 +3,6 @@
 #  Script will be run after parameterization has completed, e.g., 
 #  use this to compile source code that has been parameterized.
 #
-echo "172.17.0.2    verydodgy.com" | sudo tee --append /etc/hosts
 sudo systemctl enable httpserver.service
 sudo systemctl start httpserver.service
 
