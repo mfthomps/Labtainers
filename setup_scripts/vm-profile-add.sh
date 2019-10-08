e7de496e9e9dd9ec1315d413eeb11aba4d3367e9
Mike Thompson <mfthomps@nps.edu>
2019-10-08 16:00:36 -0700

    create xfer directory, add instructions to header

--- setup_scripts/vm-profile-add.sh
+++ setup_scripts/vm-profile-add.sh
@@ -6,6 +6,28 @@
 # This script also creates the .doupdate file and modifieds gnome
 # to shutdown when the virtual powerbutton is pressed.
 #
+# Create new Ubuntu VM 40GB 4 GB RAM; 2 CPUs; VMSVGA; 3d accelleration
+# sudo apt install build-essential dkms linux-headers-$(uname -r)
+# install guest additions
+#sudo usermod -G vboxsf -a $USER
+# add terminal to desktop
+# bidirectional shared clipboard
+# wget https://my.nps.edu/documents/107523844/109121513/labtainer.tar
+# cd labtainer
+# ./install-labtainer.sh
+# reboot
+# setup-scripts/vm-profile-add.sh
+
+#
+# clone (as linked) a smoketest box
+# remove .doupdate
+# echo "frank@beans.com" > $HOME/.local/share/labtainers/email.txt
+# add $HOME/labtainer/trunk/testsets/bin to path in bashrc
+# visudo and change sudo etnry to: ALL=(ALL) NOPASSWD:ALL
+# apt-get install xdotool
+# apt-get install vim
+# setup-scripts/prep-testregistry.sh
+#
 cat >>~/.profile <<EOL
 gnome-terminal --geometry 120x31+150+300 --working-directory=$HOME/labtainer/labtainer-student -e "bash -c \"/bin/cat README; exec bash\"" &
 if [[ -f $HOME/labtainer/.doupdate ]]; then
@@ -23,6 +45,7 @@ gsettings set org.gnome.nm-applet disable-connected-notifications "true"
 gsettings set org.gnome.desktop.session idle-delay 0
 gsettings set org.gnome.desktop.screensaver lock-enabled false
 
+mkdir -p $HOME/labtainer_xfer
 cd $HOME/Desktop
 ln -s $HOME/labtainer/trunk/docs/student/labtainer-student.pdf
 ln -s ~/labtainer_xfer
