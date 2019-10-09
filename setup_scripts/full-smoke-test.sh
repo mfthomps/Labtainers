719d025c85734fa5d0de7202aefa9e2765adb306
Mike Thompson <mfthomps@nps.edu>
2019-10-09 08:14:52 -0700

    time delay before destroying docker, update email

--- setup_scripts/full-smoke-test.sh
+++ setup_scripts/full-smoke-test.sh
@@ -6,11 +6,16 @@
 export TEST_REGISTRY=TRUE
 export PATH="${PATH}:./bin:$HOME/labtainer/trunk/scripts/designer/bin:$HOME/labtainer/trunk/testsets/bin"
 
+if [[ ! -f $HOME/.local/share/labtainers/email.txt ]]; then
+    echo "frank@beans.com" > $HOME/.local/share/labtainers/email.txt
+fi
 now=`date +"%s"`
 exec > /media/sf_SEED/smokelogs/log-$now.log
 exec 2>&1
 
 #Clear out docker.
+echo "will destroy docker in 5 seconds"
+sleep 5
 ./destroy-docker.sh -f
 
 # Update baseline and framework
