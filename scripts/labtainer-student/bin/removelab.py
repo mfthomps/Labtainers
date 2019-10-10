0962bf54bb8624a7291a080d5388c6c1936f5e85
Mike Thompson <mfthomps@nps.edu>
2019-10-10 12:53:00 -0700

    option for gns3

--- scripts/labtainer-student/bin/removelab.py
+++ scripts/labtainer-student/bin/removelab.py
@@ -8,7 +8,10 @@ def removeLab(lab, justContainers):
     cmd = 'docker ps -a'
     ps = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
     output = ps.communicate()
-    lab_container = ' %s.' % lab
+    if lab == 'GNS3':
+        lab_container = '/gns3/init.sh'
+    else:
+        lab_container = ' %s.' % lab
     gns3_container = ' %s_' % lab
     container_list = []
     for line in output[0].splitlines():
@@ -44,7 +47,7 @@ def removeLab(lab, justContainers):
 def main():
     parser = argparse.ArgumentParser(prog='removelab', description='Remove a lab and its images from a Labtainers installation. \
         The next time the lab is run, a fresh (updated) image will be pulled.')
-    parser.add_argument('labname', default='NONE', nargs='?', action='store', help='The lab to delete')
+    parser.add_argument('labname', default='NONE', nargs='?', action='store', help='The lab to delete.  A value of "GNS3" will delete all GNS labs.')
     parser.add_argument('-c', '--containers', action='store_true', help='Delete the containers of this lab, but do not delete the images.')
     args = parser.parse_args()
     removeLab(args.labname, args.containers)
