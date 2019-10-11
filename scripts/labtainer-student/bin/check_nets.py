5bffacc61492a48c83bcaba8d5e3ca7b58945616
Mike Thompson <mfthomps@nps.edu>
2019-10-11 14:26:56 -0700

    report on orphan routes and IP table entries

--- scripts/labtainer-student/bin/check_nets.py
+++ scripts/labtainer-student/bin/check_nets.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 import subprocess
 import shlex
+'''
+sudo iptables -t nat -v -L POSTROUTING -n --line-number
+sudo iptables -t nat --delete POSTROUTING <line number>
+sudo route del -net 192.168.1.0 netmask 255.255.255.0
+'''
+
 def checkContainers():
     cmd = 'docker ps'
     retval = {}
@@ -27,7 +33,8 @@ def getRoutes():
             iface = parts[7]
             ip = parts[0]
             gw = parts[1]
-            retval[iface] = [ip, gw]
+            mask = parts[2]
+            retval[iface] = [ip, gw, mask]
 
     return retval
 
@@ -43,6 +50,18 @@ def getNets():
             retval.append(nid)
     return retval
 
+def getIPTable(ip):
+    retval = None
+    cmd = 'sudo iptables -t nat -v -L POSTROUTING -n --line-number'
+    ps = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    output = ps.communicate()
+    for line in output[0].splitlines(True):
+        if ip in line:
+            retval = line
+            break
+    return retval
+
+
 if __name__ == '__main__':
     if not checkContainers():
         routes = getRoutes()
@@ -50,6 +69,19 @@ if __name__ == '__main__':
         for nid in nets:
             iface = 'br-%s' % nid
             if iface in routes:
-                ip, gw = routes[iface]
+                ip, gw, mask = routes[iface]
                 print('route %s %s for %s may be corrupting labtainers' % (ip, gw, iface))
                 print('try running "docker network delete %s"' % iface)
+        for iface in routes:
+            if iface.startswith('br-'):
+                net_id = iface[3:]
+                if net_id not in nets: 
+                    ip, gw, mask = routes[iface]
+                    print('route %s     %s     %s seems an orphan corrupting docker' % (ip, gw, mask))
+                    print('try: sudo route del -net %s netmask %s' % (ip, mask))
+                    iptable = getIPTable(ip)
+                    if iptable is not None:
+                        print('Also, this IPTABLE entry is maybe a problem: %s' % iptable)
+                        num = iptable.split()[0]
+                        print('Try: sudo iptables -t nat --delete POSTROUTING %s' % num)
+           
