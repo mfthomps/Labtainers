504c896ffe35ad7cde29d6ab27299bdfac89abd4
Mike Thompson <mfthomps@nps.edu>
2019-12-02 15:49:36 -0800

    test lab creation functions

---
+++ testsets/bin/build_lab_test.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#Set email to frank@beans.com for SimLab.py
+sed -i '1d' ~/.local/share/labtainers/email.txt
+echo "frank@beans.com" >> ~/.local/share/labtainers/email.txt
+
+#Reset the lab files
+rm -rf ~/labtainer_xfer/wiggysinglebase
+
+
+cd ../../labs
+rm -fr wiggysinglebase
+mkdir wiggysinglebase 
+cd wiggysinglebase
+new_lab_setup.py
+new_lab_setup.py -a firefox -b firefox
+
+cd wiggysinglebase
+touch hello.py
+echo "#!/usr/bin/env python" >> hello.py
+printf "print('Hello World.')" >> hello.py
+chmod +x hello.py
+
+cd ../instr_config
+echo "hello = wiggysinglebase:hello.py.stdout : CONTAINS : Hello World." >> results.config
+
+cd ..
+
+#Rebuild labs
+cd ../../scripts/labtainer-student
+
+echo "--Rebuild Single Base--"
+rebuild.py wiggysinglebase
+stoplab
+echo "--Smoketest Single Base--"
+#SimLab.py wiggysinglebase
+smoketest.py -l wiggysinglebase
+#stoplab
+
+#cd ../../labs
+#rm -rf wiggy* 
