5c6cb70af419e48d742dbc83c55c8d961508e806
Mike Thompson <mfthomps@nps.edu>
2020-01-03 15:57:43 -0800

    use new softplc container

--- labs/iptables-ics/plc/_bin/fixlocal.sh
+++ labs/iptables-ics/plc/_bin/fixlocal.sh
@@ -3,8 +3,9 @@
 #  Script will be run after parameterization has completed, e.g., 
 #  use this to compile source code that has been parameterized.
 #
-sudo systemctl enable plc
-sudo systemctl start plc
+sudo systemctl enable openplc
+cp /var/tmp/mbconfig.cfg $HOME/OpenPLC_v3/webserver
+cp /var/tmp/openplc.db $HOME/OpenPLC_v3/webserver
+sudo systemctl restart openplc
 sudo systemctl enable httpserver.service
 sudo systemctl start httpserver.service
-
