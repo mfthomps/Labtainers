8d39eb8699c89c3056229e7b43f621bd04ba1bc7
Mike Thompson <mfthomps@nps.edu>
2020-01-08 15:29:38 -0800

    include revision and branch info in readme

--- setup_scripts/update-labtainer.sh
+++ setup_scripts/update-labtainer.sh
@@ -67,3 +67,5 @@ if [[ result -ne 0 ]];then
 EOT
 fi
 grep "^Distribution created:" labtainer/trunk/README.md | awk '{print "Updated to release of: ", $3, $4}'
+grep "^Branch:" labtainer/trunk/README.md | awk '{print "branch: ", $2}'
+grep "^Revision:" labtainer/trunk/README.md | awk '{print "Revision: ", $2}'
