9eb3758b47a7bbe4d4cbd7524dcc037c318dce4e
Mike Thompson <mfthomps@nps.edu>
2020-01-15 17:02:49 -0800

    use vbox server to start VMs, set LABTAINER_BRANCH value

---
+++ testsets/bin/test-ubuntu16.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Start the Ubuntu 16 smoke test with the results of the latest mkall
+#
+vmdir="/media/sf_SEED/test_vms/ubuntu16smoke/"
+if [[ "$1" != "-n" ]]; then 
+    cp testvm-do.sh $vmdir/dothis.sh
+    chmod a+x $vmdir/dothis.sh
+    if [[ "$1" != "-t" ]]; then
+        branch=$(git rev-parse --abbrev-ref HEAD)
+        sed -i "s/REPLACE_THIS/$branch/" $vmdir/dothis.sh
+    else
+        sed -i "s/REPLACE_THIS/premaster/" $vmdir/dothis.sh
+    fi
+else
+    echo "Leaving the dothis.sh file as it was."
+fi
+tlist="labtainer.tar labtainer-developer.tar labtainer-tests.tar"
+for t in $tlist; do
+    cp /media/sf_SEED/$t $vmdir
+done
+
+./vbox-client.py "startvm SmokeTest"
