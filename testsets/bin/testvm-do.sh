32cfa671415f849b86ba0244d3dd6ef32a98ce1f
Mike Thompson <mfthomps@nps.edu>
2020-01-15 17:04:25 -0800

    set LABTAINER_BRANCH value

--- testsets/bin/testvm-do.sh
+++ testsets/bin/testvm-do.sh
@@ -3,12 +3,17 @@
 # Invoked by a testvm .profile within a gnome terminal
 #
 export LABTAINER_DIR=$HOME/labtainer/trunk
-vmtestdir=$(realpath $0)
+# next line altered by sed
+export LABTAINER_BRANCH=REPLACE_THIS
+scriptpath=$(realpath $0)
+vmtestdir=$(dirname "${scriptpath}")
+echo "vmtestdir is $vmtestdir" >> /tmp/dothis.log
 cd $HOME
 tar -xf /media/sf_SEED/labtainer.tar labtainer/trunk/setup_scripts/full-smoke-test.sh
 cd $LABTAINER_DIR/setup_scripts
 ./full-smoke-test.sh 
 result=$?
+echo "Back from full-smoke-test result $result for $vmtestdir" >> /tmp/dothis.log
 if [[ $result == 0 ]]; then
     echo "PASS" > $vmtestdir/result.txt
 else
