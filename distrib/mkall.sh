47a72c632e7d2f55b3fa831e4524f4dc9cdf2732
Mike Thompson <mfthomps@nps.edu>
2020-01-15 17:00:37 -0800

    add quiet switch and error handling

--- distrib/mkall.sh
+++ distrib/mkall.sh
@@ -2,8 +2,25 @@
 #
 # Make distributions of labtainers
 #
-git status -s | grep -E "^ M|^ D|^ A" | less
+if [[ "$1" != "-q" ]]; then
+    git status -s | grep -E "^ M|^ D|^ A" | less
+fi
 ./mkdist.sh $@
+result=$?
+if [[ $result != 0 ]]; then
+    echo "mkdist failed"
+    exit 1
+fi
 ./mk-devel-dist.sh $@
+result=$?
+if [[ $result != 0 ]]; then
+    echo "mk-devel-dist failed"
+    exit 1
+fi
 ./mktest.sh $@
+result=$?
+if [[ $result != 0 ]]; then
+    echo "mktest failed"
+    exit 1
+fi
 echo "Done building Labtainers dist tars"
