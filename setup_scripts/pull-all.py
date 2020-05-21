de0710446b628297e70e3e887acbccfb1da7df9f
mfthomps <mfthomps@nps.edu>
2020-05-21 16:27:52 -0700

    add new base images

--- setup_scripts/pull-all.py
+++ setup_scripts/pull-all.py
@@ -29,7 +29,7 @@ if args.test_registry or test_registry:
 else:
     use_registry = labtainer_config.default_registry
 print('registry is: %s' % use_registry)
-config_list = ['base', 'network', 'firefox', 'wireshark', 'java', 'centos', 'lamp']
+config_list = ['base', 'network', 'firefox', 'wireshark', 'java', 'centos', 'lamp', 'ghidra', 'tap']
 if args.metasploit:
     config_list.append('metasploitable')
     config_list.append('kali')
