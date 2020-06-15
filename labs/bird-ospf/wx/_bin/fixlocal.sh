4c20d9535418e2885e2fd46ee8f24a16faf7d5d3
Mike <mfthomps@nps.edu>
2020-06-15 16:07:49 -0700

    routing ospf intro lab

---
+++ labs/bird-ospf/wx/_bin/fixlocal.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+#  This script will be run after parameterization has completed, e.g., 
+#  use this to compile source code that has been parameterized.
+#  The container user password will be passed as the first argument,
+#  (the user ID is the second parameter)
+#  If this script is to use sudo and the sudoers for the lab
+#  does not permit nopassword, then use:
+#  echo $1 | sudo -S the-command
+#
+#  If you issue commands herein to start services, and those services
+#  have unit files prescribing their being started after the
+#  waitparam.service, then first create the flag directory that
+#  waitparam sleeps on:
+#
+#   PERMLOCKDIR=/var/labtainer/did_param
+#   echo $1 | sudo -S mkdir -p "$PERMLOCKDIR"
+
