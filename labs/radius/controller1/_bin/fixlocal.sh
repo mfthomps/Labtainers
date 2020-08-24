16ff7c35fdd2edd79a1f2e326776d57a994c6e69
Mike <mfthomps@nps.edu>
2020-08-24 10:59:55 -0700

    add some checkwork; expand lab manual

--- labs/radius/controller1/_bin/fixlocal.sh
+++ labs/radius/controller1/_bin/fixlocal.sh
@@ -7,13 +7,16 @@
 #  not not permit nopassword, then use:
 #  echo $1 | sudo -S the-command
 #
+#  Update the pam sshd to use common radius unix
+#
 sudo sed -i 's/@include common-auth/#@include common-auth/' /etc/pam.d/sshd
 sudo sed -i '/@include common-auth/a session    sufficient      /lib/security/pam_radius_auth.so debug conf=/etc/pam_radius_auth.conf' /etc/pam.d/sshd
 sudo sed -i '/@include common-auth/a auth       sufficient      /lib/security/pam_radius_auth.so debug' /etc/pam.d/sshd
 sudo /etc/init.d/xinetd restart
 
+# Put greeting into profile
 me=$(hostname)
 cat >> $HOME/.profile << EOL
 echo "Welcome to the $me shambang PDU controller."
-echo "Type 'help' to terminate your session"
+echo "Type 'help'. Or 'exit' to terminate your session"
 EOL
