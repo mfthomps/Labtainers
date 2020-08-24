92689486a9e055c1976af8a5e0dec3e32223f58d
Mike <mfthomps@nps.edu>
2020-08-24 09:00:12 -0700

    remove debug output

--- setup_scripts/update-add.sh
+++ setup_scripts/update-add.sh
@@ -1,7 +1,5 @@
 hasnew=$(docker images | grep labtainers/labtainer.base)
 if [[ -z $hasnew ]];then
-    echo "nope"
+    #echo "nope"
     $LABTAINER_DIR/setup_scripts/pull-all.py
-else
-    echo "has it $hasnew"
 fi
