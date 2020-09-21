961445d07ddef7940a4f04b98cb4a1ff24d73488
Mike Thompson <mfthomps@nps.edu>
2020-09-21 11:17:39 -0700

    use explicit wget to update labtainers rather than script, avoid image pull for now

--- headless-lite/doterm.sh
+++ headless-lite/doterm.sh
@@ -3,7 +3,6 @@
 #
 gnome-terminal --geometry 120x31+150+100 --working-directory=$HOME/labtainer/labtainer-student -e "bash -c \"/bin/cat README; exec bash\"" &
 if [[ -f $HOME/labtainer/.doupdate ]]; then
-    gnome-terminal --geometry 73x31+100+300 --working-directory=$HOME/labtainer -x ./update-labtainer.sh
-    echo "ran update" >/tmp/update.log
+    gnome-terminal --geometry 73x31+100+300 --working-directory=$HOME/labtainer -e "bash -c  /home/labtainer/.doupdate.sh"
 fi
 
