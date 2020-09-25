b3b4b55beb128179fafd5eb4fd9fb44ad15d9bbb
Mike Thompson <mfthomps@nps.edu>
2020-09-24 17:38:35 -0700

    rename base and headless files, clean up scripts

--- scripts/designer/bin/create_master_headless.sh
+++ scripts/designer/bin/create_master_headless.sh
@@ -4,28 +4,17 @@
 # on any system that has Docker installed, withou having to install Labtainers.
 # Thanks for Olivier Berger for this contribution.
 #
-source ./set_reg.sh
-if [[ "$1" != -f ]]; then
-   echo "This will build the labtainer master controller image (STAGE 1).  "
-   echo "registry is $LABTAINER_REGISTRY"
-   read -p "Continue? (y/n)"
-   if [[ ! $REPLY =~ ^[Yy]$ ]]
-   then
-       echo exiting
-       exit
-   fi
-else
-   echo "registry is $LABTAINER_REGISTRY"
-fi
 here=`pwd`
 cd ../
 mkdir -p workspace_master
-cp -a $LABTAINER_DIR/headless-lite/Dockerfile.labtainer.master.stage.1 workspace_master/Dockerfile.labtainer.headless.stage.1
+rm -fr workspace_master/*
+cp -a $LABTAINER_DIR/headless-lite/Dockerfile.labtainer.master.headless workspace_master/Dockerfile.labtainer.master.headless
 cp -aR workspace/system workspace_master/
 cp -a $LABTAINER_DIR/headless-lite/motd workspace_master
 cp -a $LABTAINER_DIR/headless-lite/docker-entrypoint workspace_master
 cp -a  $LABTAINER_DIR/headless-lite/wait-for-it.sh workspace_master
 cp -a  $LABTAINER_DIR/headless-lite/doterm.sh workspace_master
+cp -a  $LABTAINER_DIR/headless-lite/doupdate.sh workspace_master
 cd workspace_master
 cp -a $LABTAINER_DIR/distrib/labtainer.tar ./
 
@@ -36,11 +25,9 @@ cat <<EOT >bashrc.labtainer.master
    fi
 EOT
 
-cp $LABTAINER_DIR/scripts/labtainer-student/bin/labutils.py ./
-#docker build --no-cache --build-arg DOCKER_GROUP_ID="$(getent group docker | cut -d: -f3)" -f Dockerfile.labtainer.headless -t labtainer.master:latest .
 CACHE="--no-cache"
 if [[ "$1" == -c ]]; then
     CACHE=""
 fi
-docker build $CACHE --build-arg DOCKER_GROUP_ID="$(getent group docker | cut -d: -f3)" -f Dockerfile.labtainer.headless.stage.1 -t labtainer.master:stage.1 .
+docker build $CACHE --build-arg DOCKER_GROUP_ID="$(getent group docker | cut -d: -f3)" -f Dockerfile.labtainer.master.headless -t labtainer.master.headless:latest .
 cd $here
