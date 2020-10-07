248f0607818d0649ac1c189a93d7f79e779c9235
Mike Thompson <mfthomps@nps.edu>
2020-10-07 14:54:59 -0700

    repeatable build of headless tester

---
+++ headless-lite/buildTester.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd $LABTAINER_DIR/distrib
+./mkdist.sh
+./mktest.sh
+cd $LABTAINER_DIR/scripts/designer/bin
+./create_master_headless.sh -d
+./create_headless_tester.sh
+docker push testregistry:5000/labtainer.headless.tester
+cd $LABTAINER_DIR/headless-lite
+cp install-headless.sh /media/sf_SEED/multipass/
+echo "reinstall the server and run install_headless.sh -t"
