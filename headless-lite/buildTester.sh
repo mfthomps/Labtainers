6eed1b45067892701b342ff07fc79b63f64e5694
Mike Thompson <mfthomps@nps.edu>
2020-10-08 10:41:33 -0700

    hint to push repo

--- headless-lite/buildTester.sh
+++ headless-lite/buildTester.sh
@@ -8,4 +8,5 @@ cd $LABTAINER_DIR/scripts/designer/bin
 docker push testregistry:5000/labtainer.headless.tester
 cd $LABTAINER_DIR/headless-lite
 cp install-headless.sh /media/sf_SEED/multipass/
+echo "push any git updates"
 echo "reinstall the server and run install_headless.sh -t"
