886775141fa6292a68195349adbd38a77d3b22a3
Mike <mfthomps@nps.edu>
2020-12-12 12:25:27 -0800

    add flask and Flask-Table

--- setup_scripts/dev-pkg.sh
+++ setup_scripts/dev-pkg.sh
@@ -5,3 +5,4 @@
 sudo apt-get install -y make g++ python3-pip xdotool
 sudo pip3 install py-dateutil
 sudo apt-get install -y texlive-latex-base texlive-latex-extra net-tools
+pip3 install flask Flask-Table
