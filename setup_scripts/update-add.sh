afe17f99f4a1f8b3633af75292972ec3e07029e7
Mike Thompson <mfthomps@nps.edu>
2021-02-01 11:56:42 -0800

    fix syntax

--- setup_scripts/update-add.sh
+++ setup_scripts/update-add.sh
@@ -4,8 +4,8 @@ if [[ -z $hasnew ]];then
     $LABTAINER_DIR/setup_scripts/pull-all.py
 fi
 hascommit=$(grep "^Commit:" labtainer/trunk/README.md)
-hasgit=$(grep "github" labtainer/update-labtainer.sh)
-if [[ -z "$hascommit" ] || [ -z "$hasgit" ]]; then
+hasgit=$(grep "github.*releases" update-labtainer.sh)
+if [ -z "$hascommit" ] || [ -z "$hasgit" ]; then
     cd labtainer
     wget --quiet https://github.com/mfthomps/Labtainers/releases/latest/download/labtainer.tar -O labtainer.tar
     sync
