deacb293847df2e13762d262a1f601211e0c0c11
Mike Thompson <mfthomps@nps.edu>
2021-02-01 11:33:29 -0800

    confirm the update-labtainers was updated, e.g., tar actually pulled and expanded

--- setup_scripts/update-add.sh
+++ setup_scripts/update-add.sh
@@ -3,9 +3,9 @@ if [[ -z $hasnew ]];then
     #echo "nope"
     $LABTAINER_DIR/setup_scripts/pull-all.py
 fi
-#hascommit=$(grep "^Commit:" labtainer/trunk/README.md)
-hascommit=$(grep "github" labtainer/update-labtainer.sh)
-if [[ -z "$hascommit" ]]; then
+hascommit=$(grep "^Commit:" labtainer/trunk/README.md)
+hasgit=$(grep "github" labtainer/update-labtainer.sh)
+if [[ -z "$hascommit" ] || [ -z "$hasgit" ]]; then
     cd labtainer
     wget --quiet https://github.com/mfthomps/Labtainers/releases/latest/download/labtainer.tar -O labtainer.tar
     sync
