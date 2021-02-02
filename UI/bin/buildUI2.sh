c1956eb40c84ea3897987f1b03d139f3c3a2a223
Mike Thompson <mfthomps@nps.edu>
2021-02-02 10:30:22 -0800

    option to not run the UI

--- UI/bin/buildUI2.sh
+++ UI/bin/buildUI2.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-
+#
+# Rebuild the Labtianers Lab Editor UI
+# Use -n to supress running the UI after rebuild.
+#
 javac  ../MainUI/src/main/java/labtainers/goalsui/*.java ../MainUI/src/main/java/labtainers/resultsui/*.java ../MainUI/src/main/java/labtainers/paramsui/*.java ../MainUI/src/main/java/labtainers/mainui/*.java -d . -Xlint:unchecked || exit
 jar cmf mainUI.mf ./MainUI.jar labtainers/mainui/*.class labtainers/goalsui/*.class labtainers/resultsui/*.class labtainers/paramsui/*.class ../MainUI/src/main/resources/* || exit
-java -jar MainUI.jar
-#java MainWindow
+if [[ "$1" != "-n" ]]; then
+    java -jar MainUI.jar
+fi
