eb8a17430e33875a9388194406caf783d2b39414
Mike <mfthomps@nps.edu>
2021-02-05 08:57:53 -0800

    adjust timestamps along with trim

--- labs/wireshark-intro/wireshark-intro/_bin/fixlocal.sh
+++ labs/wireshark-intro/wireshark-intro/_bin/fixlocal.sh
@@ -4,5 +4,5 @@
 
 cd $HOME
 # Do editcap to new file to avoid potential corruption
-editcap -F pcap -r telnet.pcap new.telnet.pcap EDITCAP_START-999999
+editcap -F pcap -t EDITCAP_START -r telnet.pcap new.telnet.pcap EDITCAP_START-999999
 mv new.telnet.pcap telnet.pcap
