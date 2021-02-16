3a94566251b52f546b8264e23742bb68fcf0cfb8
Mike Thompson <mfthomps@nps.edu>
2021-02-15 19:31:59 -0800

    move to ubuntu20 breakage

--- labs/ldap/server1/_system/var/tmp/configure-ldap-client.sh
+++ labs/ldap/server1/_system/var/tmp/configure-ldap-client.sh
@@ -6,8 +6,12 @@ dpkg-reconfigure -feditor ldap-auth-config
 sed -i 's/use_authtok//'  /etc/pam.d/common-password
 
 # enable use of ldap for authentication
-sudo sed -i 's/compat/compat ldap/' /etc/nsswitch.conf
+sudo sed -i 's/^passwd:.*/passwd:    compat ldap/' /etc/nsswitch.conf
+sudo sed -i 's/^group:.*/group:    compat ldap/' /etc/nsswitch.conf
+sudo sed -i 's/^shadow:.*/shadow:    compat ldap/' /etc/nsswitch.conf
+
 
 sudo sed -i '/and here are more per-package modules/a session required        pam_mkhomedir.so umask=0022 skel=/etc/skel' /etc/pam.d/common-session
+sudo sed -i '/pam_systemd.so/d' /etc/pam.d/common-session
 
 systemctl restart nscd
