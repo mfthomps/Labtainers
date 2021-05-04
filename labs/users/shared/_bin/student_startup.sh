27bdede8c281bea58ea68c9194a77679cddcdfd0
Mike <mfthomps@nps.edu>
2021-05-04 09:51:48 -0700

    users lab

---
+++ labs/users/shared/_bin/student_startup.sh
@@ -0,0 +1,14 @@
+#
+# If root, loop forever in login. 
+#
+id | grep root >>/dev/null
+result=$?
+if [[ $result -eq 0 ]]; then
+    rm -f /run/nologin
+    #echo "is root"
+    while [ 1 ]; do
+        trap login SIGINT
+        /sbin/login
+    done
+fi
+
