18dbcb5b2cd0760224e0625d1fd4faa86f8bbe32
Mike Thompson <mfthomps@nps.edu>
2021-05-25 09:51:52 -0700

    delete unused lab

--- labs/usbtest/host_data/usb_create.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-labdir=`dirname "$0"`
-BACK_STORE=$labdir/usb.img
-echo "backstore is $BACK_STORE"
-modprobe g_mass_storage file=$BACK_STORE idVendor=0x1d6b idProduct=0x0104 iManufacturer=Myself iProduct=VirtualBlockDevice iSerialNumber=123
