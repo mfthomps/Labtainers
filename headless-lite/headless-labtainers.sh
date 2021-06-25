44f2c26ae03c7bf5fbd0a54d705e1d4cab308469
Mike <mfthomps@nps.edu>
2021-06-25 13:02:39 -0700

    more posixish for windows compat

--- headless-lite/headless-labtainers.sh
+++ headless-lite/headless-labtainers.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-function check_docker {
+check_docker() {
 /usr/local/bin/docker ps
 result=$?
-if [[ ! $result == 0 ]]; then
+if [ ! $result == 0 ]; then
     echo "Docker Desktop not installed or not running" >>/tmp/lab-preinstall.log
 
     case "$OSTYPE" in
@@ -24,7 +24,7 @@ fi
 }
 
 
-function do_up {
+do_up() {
    echo "When you see two 'done's below, open a browser and goto"
    echo "   http://localhost:6901/vnc.html?password="
    echo "No password is needed, just click 'submit' if prompted."
@@ -33,7 +33,7 @@ function do_up {
    echo "Your results are in ~/headless-labtainers/labtainer_xfer"
 }
 
-function fix_it {
+fix_it() {
 case "$OSTYPE" in
   solaris*) echo "SOLARIS" ;;
   darwin*)  echo "fix up OSX" >> /tmp/headless.log
@@ -64,28 +64,28 @@ export LABTAINER_UPDATE=""
 export LABTAINER_DEV=""
 export LABTAINER_TEST=""
 export TEST_REGISTRY=""
-while [[ -n "$1" ]]; do
-    if [[ "$1" == -h ]]; then
+while [ -n "$1" ]; do
+    if [ "$1" == -h ]; then
         echo "-d to use your local yml file"
         echo "-n to supress updates on the container, e.g. if you created your own labtainer.tar"
         exit 0
-    elif [[ "$1" == -n ]]; then
+    elif [ "$1" == -n ]; then
         export LABTAINER_UPDATE="FALSE"
         shift
-    elif [[ "$1" == -d ]]; then
+    elif [ "$1" == -d ]; then
         LABTAINER_DEV="TRUE"
         shift
-    elif [[ "$1" == -t ]]; then
+    elif [ "$1" == -t ]; then
         LABTAINER_TEST="TRUE"
         shift
     fi
 done
 
 
-if [[ "$LABTAINER_TEST" == "TRUE" ]];then
+if [ "$LABTAINER_TEST" == "TRUE" ];then
    export TEST_REGISTRY=TRUE
 fi
-if [[ -d ./mystuff ]]; then
+if [ -d ./mystuff ]; then
     echo "Running Headless Labtainers."
     check_docker
     fix_it
@@ -99,10 +99,10 @@ else
     mkdir -p mystuff
     mkdir -p labtainer_xfer
     mkdir -p labtainers
-    if [[ "$LABTAINER_DEV" == "TRUE" ]];then
+    if [ "$LABTAINER_DEV" == "TRUE" ];then
         echo "Using local yml"
         cp $LABTAINER_DIR/headless-lite/docker-compose.yml .
-    elif [[ "$LABTAINER_TEST" == "TRUE" ]];then
+    elif [ "$LABTAINER_TEST" == "TRUE" ];then
         echo "Using labtainer.headless.tester"
         curl https://raw.githubusercontent.com/mfthomps/Labtainers/master/headless-lite/docker-compose.yml > docker-compose.yml 
         sed -i s%labtainers/labtainer.master.headless%testregistry:5000/labtainer.headless.tester% docker-compose.yml
@@ -121,7 +121,7 @@ else
     fix_it
     do_up
     HEADLESS_DIR=`pwd`
-    echo "In the future, open a terminal, cd to 
+    echo "In the future, open a terminal, cd to"
     echo " $HEADLESS_DIR and run:"
     echo "./headless-labtainers.sh"
 fi
