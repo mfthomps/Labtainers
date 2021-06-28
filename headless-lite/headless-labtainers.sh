b5ddc6482d039805a0e1c6915558e2a451fb3899
Mike Thompson <mfthomps@nps.edu>
2021-06-28 14:05:46 -0700

    newlines in echo, change to printf

--- headless-lite/headless-labtainers.sh
+++ headless-lite/headless-labtainers.sh
@@ -25,14 +25,14 @@ fi
 
 
 do_up() {
-   printf "\n\nStarting Labtainers..."
-   printf "When you see two 'done's below, open a browser and goto"
-   printf "   http://localhost:6901/vnc.html?password="
-   printf ""
-   printf "No password is needed, just click 'submit' if prompted."
-   printf "Use ctrl-C to stop Headless Labtainers."
+   printf "\n\nStarting Labtainers...\n"
+   printf "When you see two 'done's below, open a browser and goto\n"
+   printf "   http://localhost:6901/vnc.html?password=\n"
+   printf "\n"
+   printf "No password is needed, just click 'submit' if prompted.\n"
+   printf "Use ctrl-C to stop Headless Labtainers.\n"
    docker-compose up >> /tmp/headless.log
-   printf "\nYour results are in ~/headless-labtainers/labtainer_xfer"
+   printf "\nYour results are in ~/headless-labtainers/labtainer_xfer\n"
 }
 
 fix_it() {
