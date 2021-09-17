f28c34b650b6553ac638eda7c15b657207e55f2f
Mike Thompson <mfthomps@nps.edu>
2021-09-17 08:33:50 -0700

    option to not run

--- MakepackUI/bin/buildUI2.sh
+++ MakepackUI/bin/buildUI2.sh
@@ -5,4 +5,6 @@
 #/src/main/java/newpackage
 javac -classpath json-simple-1.1.1.jar ../src/main/java/newpackage/NewJFrame.java -d . -Xlint:unchecked || exit
 jar cmf makepackui.mf ./makepackui.jar newpackage/*.class ../src/main/resources/* || exit
-java -jar makepackui.jar & 
+if [[ "$1" != "-n" ]]; then
+    java -jar makepackui.jar & 
+fi
