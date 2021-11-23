61e01a717704b93fdbb44d9354cf76c1dab3e4d2
Mike Thompson <mfthomps@nps.edu>
2021-11-23 08:33:47 -0800

    Fix issue #60, DISPLAY variable handling

--- scripts/labtainer-student/bin/labutils.py
+++ scripts/labtainer-student/bin/labutils.py
@@ -1278,7 +1278,12 @@ def DoStartOne(labname, name, container, start_config, labtainer_config, lab_pat
         logger.debug("DoStart for %s AllContainersCreated result (%s)" % (container.name, haveContainer))
 
         display = os.getenv('DISPLAY')
-        display_num = int(display[1:])
+        if ':' in display:
+            display = display.split(':')[1]
+            display_num = int(float(display))
+        else:
+            logger.error('could not get display number from %s' % os.getenv('DISPLAY'))
+            return 
         # Set need_seeds=False first
         need_seeds=False
         # IsContainerCreated return False if container does not exists
