956febb5e69ee2e4e333af76eb88221e1ae6ef77
Mike Thompson <mfthomps@nps.edu>
2022-01-10 14:23:13 -0800

    use github latest release for update.

--- headless-lite/doupdate.sh
+++ headless-lite/doupdate.sh
@@ -1,7 +1,6 @@
 cd $LABTAINER_DIR/..
-wget -vv -4 https://nps.box.com/shared/static/afz87ok8ezr0vtyo2qtlqbfmc28zk08j.tar -O labtainer.tar
+wget --quiet https://github.com/mfthomps/Labtainers/releases/latest/download/labtainer.tar -O labtainer.tar
 echo "doing update of labtainer" >/tmp/update.log
-#wget -vv -4  https://nps.edu/documents/107523844/117289221/labtainer.tar -O labtainer.tar
 sync
 cd ..
 tar xf labtainer/labtainer.tar --keep-newer-files --warning=none
