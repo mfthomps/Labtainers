824dbad1278ab9c845ff790278457c8d54a69191
Mike Thompson <mfthomps@nps.edu>
2022-01-14 10:06:53 -0800

    use azure for ssh keys

--- azure/create_vm.sh
+++ azure/create_vm.sh
@@ -12,10 +12,6 @@ fi
 user_id=$1
 vm_name=$user_id-labtainervm
 rm -f ~/.ssh/id_labtainers*
-ssh-keygen -b 2048 -t rsa -f ~/.ssh/id_labtainers -q -N ""
-key=$(cat ~/.ssh/id_labtainers.pub)
-echo "key generated"
-sed  "s|REPLACE_WITH_KEY|$key|" cloud_init.template > cloud_init.txt 
 ./resourcecheck.sh
 echo "Creating Azure VM $vm_name for $user_id"
 az vm create \
@@ -24,6 +20,7 @@ az vm create \
  --admin-username labtainer \
  --image UbuntuLTS \
  --ssh-key-value "~/.ssh/id_labtainers.pub" \
+ --generate-ssh-keys \
  --custom-data cloud_init.txt > $user_id.json
 
 ./waitdone.sh $user_id
