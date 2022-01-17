6a2bdc2ef4e9b120d17553eada6ba2b512e7bbd4
Mike Thompson <mfthom@gmail.com>
2022-01-17 08:51:56 -0800

    Update resourcecheck.sh

--- azure/resourcecheck.sh
+++ azure/resourcecheck.sh
@@ -2,7 +2,7 @@
 result=$(az group list | grep labtainerResources)
 if [ -z "${result}" ]; then
     echo "Creating Labtainer resource group."
-    az group create -l westus2 -n labtainerResources --output none
+    az group create -l westus -n labtainerResources --output none
 else
     echo "Labtainer resource group exists."
 fi
