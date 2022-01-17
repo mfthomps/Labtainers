e0b54fd93fd9c53937397549770ae8018868dfa7
Mike Thompson <mfthom@gmail.com>
2022-01-17 10:18:33 -0800

    Update restart.sh

--- azure/restart.sh
+++ azure/restart.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-./checktunnel.sh || exit 1
+if [ "$#" -ne 1 ]; then
+    echo "restart.sh <user ID>"
+    exit
+fi
+user=$1
+./checktunnel.sh $1 || exit 1
 echo "Point your browser to http://localhost:6901"
