62540cf02cfa05c164bf5638fc5a786ccf1cf7b8
Mike <mfthomps@nps.edu>
2022-01-24 11:26:21 -0800

    powerscript fixes

--- azure/update_user.ps1
+++ azure/update_user.ps1
@@ -5,8 +5,9 @@ If ($args.Count -ne 1){
 $ErrorActionPreference = "Stop"
 $user_id=$args[0]
 $vm_name=$user_id+"-labtainervm"
-if (-Not test-path $HOME/.ssh/id_labtainers){
-    ssh-keygen -b 2048 -t rsa -f ~/.ssh/id_labtainers -q -N ""
+if (-not(test-path $HOME/.ssh/id_labtainers)){
+    ssh-keygen -b 2048 -t rsa -f $HOME/.ssh/id_labtainers -q -N '""'
 }
 $key = Get-Content $HOME/.ssh/id_labtainers.pub -Raw
-az vm user update -u labtainer --ssh-key-value "$key" -n $vm_name -g labtainerResources --output none
+echo "key is $key"
+az vm user update -u labtainer -n $vm_name -g labtainerResources --output none --ssh-key-value "$key"
