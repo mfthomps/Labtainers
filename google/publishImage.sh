811a815952c549f3b785a77dd84e4f52a4cbf97c
Mike Thompson <mfthomps@nps.edu>
2022-01-24 16:47:57 -0800

    describe steps for adding image

--- google/publishImage.sh
+++ google/publishImage.sh
@@ -1,3 +1,15 @@
+# First use create_base.sh to create an vm instance named base-labtainervm
+#
+# The use 
+#    gclooud compute instances describe base-labtainervm
+# to find the disk name, it is a url.
+#
+# Then 
+#     gcloud compute images delete labtainervm
+#    gcloud compute images create labtainervm --source-disk=https://www.googleapis.com/compute/v1/projects/labtainers/zones/us-west1-a/disks/base-labtainervm
+
+#
+#
 gcloud compute images add-iam-policy-binding labtainervm \
     --member='allAuthenticatedUsers' \
     --role='roles/compute.imageUser'
