3b3599a74ce7fff5a4bc824f87e4c565efcdd1f1
Mike Thompson <mfthomps@nps.edu>
2022-02-22 09:10:41 -0800

    Modify readme to reflect labtianer user, rather than student

--- headless-lite/doterm.sh
+++ headless-lite/doterm.sh
@@ -9,7 +9,10 @@ if [[ result -ne 0 ]];then
    echo 'source $LABTAINER_DIR/setup_scripts/lab-completion.bash' >> $target
 fi
 source $LABTAINER_DIR/setup_scripts/lab-completion.bash
-
+#
+# fix link in README file
+#
+sed -i '0,%labtainer%{s%student/labtainer%labtainer/labtainer%}' $HOME/labtainer/labtainer-student/README
 /usr/bin/waitForX.sh 
 sleep 2
 gnome-terminal --geometry 120x31+150+100 --working-directory=$HOME/labtainer/labtainer-student -- bash -c "/bin/cat README; exec bash" &
