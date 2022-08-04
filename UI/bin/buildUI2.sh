04665b8d1c7ca2b0e741cfe16852a76b1edb72cc
Mike Thompson <mfthomps@nps.edu>
2022-08-04 14:31:28 -0700

    force build using older JDK 11

--- UI/bin/buildUI2.sh
+++ UI/bin/buildUI2.sh
@@ -3,8 +3,8 @@
 # Rebuild the Labtianers Lab Editor UI
 # Use -n to supress running the UI after rebuild.
 #
-javac  ../MainUI/src/main/java/labtainers/goalsui/*.java ../MainUI/src/main/java/labtainers/resultsui/*.java ../MainUI/src/main/java/labtainers/paramsui/*.java ../MainUI/src/main/java/labtainers/mainui/*.java -d . -Xlint:unchecked || exit
+/usr/bin/javac  ../MainUI/src/main/java/labtainers/goalsui/*.java ../MainUI/src/main/java/labtainers/resultsui/*.java ../MainUI/src/main/java/labtainers/paramsui/*.java ../MainUI/src/main/java/labtainers/mainui/*.java -d . -Xlint:unchecked || exit
 jar cmf mainUI.mf ./MainUI.jar labtainers/mainui/*.class labtainers/goalsui/*.class labtainers/resultsui/*.class labtainers/paramsui/*.class ../MainUI/src/main/resources/* || exit
 if [[ "$1" != "-n" ]]; then
-    java -jar MainUI.jar
+    /usr/bin/java -jar MainUI.jar
 fi
