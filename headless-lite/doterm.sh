3aa887d1f956fc5b8e6ebae314747291ccc00d8d
Mike Thompson <mfthomps@nps.edu>
2022-10-11 12:29:22 -0700

    gnome-terminal runs in background anyway, was causing crash on double-click in GCL?

--- headless-lite/doterm.sh
+++ headless-lite/doterm.sh
@@ -15,7 +15,7 @@ source $LABTAINER_DIR/setup_scripts/lab-completion.bash
 sed -i '0,%labtainer%{s%student/labtainer%labtainer/labtainer%}' $HOME/labtainer/labtainer-student/README
 /usr/bin/waitForX.sh 
 sleep 2
-gnome-terminal --geometry 120x31+150+100 --working-directory=$HOME/labtainer/labtainer-student -- bash -c "/bin/cat README; exec bash" &
+gnome-terminal --geometry 120x31+150+100 --working-directory=$HOME/labtainer/labtainer-student -- bash -c "/bin/cat README; exec bash" 
 if [[ -f $HOME/labtainer/.doupdate ]] && [[ "$LABTAINER_UPDATE" != 'FALSE' ]]; then
     xterm -e  /home/labtainer/.doupdate.sh
     rm $HOME/labtainer/.doupdate
