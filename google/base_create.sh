23f6f1b7a3365b2753c4258a4b27342284c24d43
Mike Thompson <mfthomps@nps.edu>
2022-12-12 09:28:59 -0800

    update base image, error handing creating base.

--- google/base_create.sh
+++ google/base_create.sh
@@ -9,7 +9,8 @@ cp cloud_init.template cloud_init.txt
 gcloud compute instances create $vm_name --image-family=ubuntu-1804-lts \
    --image-project=ubuntu-os-cloud \
    --metadata-from-file=user-data=cloud_init.txt
-gcloud compute ssh labtainer@$vm_name --command='echo "VM created, wait for reboot"'
+sleep 2
+gcloud compute ssh labtainer@$vm_name --command='echo "VM created, wait for reboot"' || exit
 cp ~/.ssh/google_compute_engine ~/.ssh/id_labtainers
 cp ~/.ssh/google_compute_engine.pub ~/.ssh/id_labtainers.pub
 ./waitdone.sh $user_id
--- google/publishImage.sh
+++ google/publishImage.sh
@@ -2,18 +2,19 @@
 # First:
 #    delete_vm.sh base
 #    base_create.sh base
+#    stop_vm.sh base
 #
 # Then use 
 #    gcloud compute instances describe base-labtainervm | grep disks
 # to find the disk name, it is a url.
 #
 # Then 
-#     (NO, cycle through images!)    gcloud compute images delete labtainervm2
-#    gcloud compute images create labtainervm2 --source-disk=https://www.googleapis.com/compute/v1/projects/labtainers/zones/us-west1-a/disks/base-labtainervm
+#    Cycle through image names.  Most recently used should be in the gcloud binding command.
+#    gcloud compute images create labtainervm5 --source-disk=https://www.googleapis.com/compute/v1/projects/labtainers/zones/us-west1-a/disks/base-labtainervm
 #
 # Only then run this script
 #
 #
-gcloud compute images add-iam-policy-binding labtainervm4 \
+gcloud compute images add-iam-policy-binding labtainervm5 \
     --member='allAuthenticatedUsers' \
     --role='roles/compute.imageUser'
