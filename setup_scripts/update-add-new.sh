ccc115f0fd78f41cb7c1d42a61d639f381345eb9
mike <mfthomps@nps.edu>
2024-07-04 07:54:31 -0700

    testing

--- setup_scripts/update-add-new.sh
+++ setup_scripts/update-add-new.sh
@@ -1,6 +1,7 @@
 #
 # All non-Ubuntu 18 update logic should go here
 #
+#
 $LABTAINER_DIR/setup_scripts/pull-all.py $test_flag 
 grep "^Distribution created:" labtainer/trunk/README.md | awk '{print "Updated to release of: ", $3, $4}'
 grep "^Branch:" labtainer/trunk/README.md | awk '{print "branch: ", $2}'
